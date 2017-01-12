4acc1a61e74ca0616f814cf0b6f4d37faeff965f
sibille <sibille@ilitia.com>
2017-01-12 14:49:13 +0100

    Init _Build

---
+++ _Build/CreateGitHubRelease.ps1
@@ -0,0 +1,46 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$versionNumber,
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$reposOwner,
+  [Parameter(Mandatory=$True,Position=3)]
+  [string]$reposName,
+  [Parameter(Mandatory=$True,Position=4)]
+  [string]$branch,
+  [Parameter(Mandatory=$True,Position=5)]
+  [string]$apiKey
+)
+
+Write-Host "Info..."
+Write-Host "Version Number" $versionNumber
+Write-Host "Repos Owner" $reposOwner
+Write-Host "Repos Name" $reposName
+Write-Host "Branch" $branch
+
+
+$body = @{
+  tag_name = $versionNumber;
+  target_commitish= $branch;
+  name= $versionNumber;
+  body= "Release version " + $versionNumber;
+}
+
+$releaseParams = @{ 
+   Uri = "https://api.github.com/repos/"+ $owner + "/" + $repos + "/releases";     
+   Method = 'POST'; 
+   Headers = @{ 
+     Authorization = 'Basic ' + [Convert]::ToBase64String( 
+      [Text.Encoding]::ASCII.GetBytes($apiKey + ":x-oauth-basic")); 
+    } 
+    ContentType = 'application/json'; 
+    Body = (ConvertTo-Json $body -Compress) 
+  } 
+ 
+Write-Host "Creating release in GitHub for version" $versionNumber
+
+Invoke-RestMethod @releaseParams  
+
+Write-Host 'Finished!'
+
+
