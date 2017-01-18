303b83fd3e203127c8e4547c108449c53c466f3b
Raúl Alarcón <ralarcon@microsoft.com>
2017-01-18 12:51:31 +0100

    Consider DEV15 to run integration tests

--- Code/test/Templates.Test/RestoreAndBuild.bat
+++ Code/test/Templates.Test/RestoreAndBuild.bat
@@ -1,4 +1,25 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
-..\..\..\..\..\..\_tools\nuget.exe restore -OutputDirectory c:\packages
+#Setup MsBuild context by brute force, restore packages and build the solution
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017" (
+	GOTO DEV15
+)
+ELSE (
+	GOTO DEV14
+)
 
-msbuild "%~1" /t:Rebuild /p:Configuration=%3;Platform=%2;AppxPackageSigningEnabled=false;NuGetPackagesDirectory=C:\packages
+:DEV15
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsMSBuildCmd.bat"
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+	msbuild "%~1" /t:Restore;Rebuild /p:Configuration=%3;Platform=%2;AppxPackageSigningEnabled=false
+GOTO END
+
+:DEV14
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
+	..\..\..\..\..\..\_tools\nuget.exe restore
+	msbuild "%~1" /t:Rebuild /p:Configuration=%3;Platform=%2;AppxPackageSigningEnabled=false
+GOTO END
+
+
+:END
+
+
