381e2fa6edc97518146d8aa264c2a4a1fbb4d8fb
Martin Vega <mvegaca@outlook.com>
2017-01-20 09:12:58 +0100

    SplitView app template updates

--- Templates/MVVMLightSplitView/App_Name/Home/HomePage.xaml.cs
+++ Templates/MVVMLightSplitView/App_Name/Home/HomePage.xaml.cs
@@ -13,12 +13,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App_Name.Home
 {
     /// <summary>
-    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class HomePage : Page
     {
--- Templates/MVVMLightSplitView/App_Name/Shell/ShellPage.xaml.cs
+++ Templates/MVVMLightSplitView/App_Name/Shell/ShellPage.xaml.cs
@@ -3,8 +3,13 @@ using App_Name.Services;
 using Microsoft.Practices.ServiceLocation;
 using Windows.UI.Xaml.Controls;
 
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
 namespace App_Name.Shell
 {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
     public sealed partial class ShellPage : Page
     {
         public ShellPage()
