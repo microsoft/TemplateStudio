e72a9347c005849a48773df70334c4d15ca261dd
Martin Vega <mvegaca@outlook.com>
2017-01-20 09:13:21 +0100

    Add SplitViewPage item template

---
+++ Templates/MVVMLightSplitViewPage/App_Page/App_PageViewModel.cs
@@ -0,0 +1,11 @@
+using GalaSoft.MvvmLight;
+
+namespace Page_NS.App_Page
+{
+    public class App_PageViewModel : ViewModelBase
+    {
+        public App_PageViewModel()
+        {
+        }        
+    }
+}
