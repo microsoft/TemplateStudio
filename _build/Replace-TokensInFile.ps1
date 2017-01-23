b1a08d3e5c2404ab50823b3c4e3b49a22908cc2b
Raúl Alarcón <ralarcon@microsoft.com>
2017-01-23 12:34:47 +0100

    Update to optionally copy replaced file as config target

--- _build/Replace-TokensInFile.ps1
+++ _build/Replace-TokensInFile.ps1
@@ -1,13 +1,16 @@
 param
 (
 	[Parameter(Mandatory=$true)]
-	[string]$File,
+	[string]$TokenizedFile,
 
 	[Parameter(Mandatory=$true)]
 	[array]$Tokens,
 
 	[Parameter(Mandatory=$true)]
 	[array]$Replacements,
+
+	[Parameter(Mandatory=$false)]	
+	[array]$TargetConfigFile, #If Defined, the TargetConfigFile will be replaced by the TokenizedFile after replacements occurs.
 
 	[Parameter(Mandatory=$false)]
 	[array]$TokenPattern = "###TOKEN###"
@@ -20,18 +23,25 @@ if($Tokens.Count -ne $Replacements.Count){
 	throw "The elements count does not match between Tokens and Replacements."
 }
 else{
-	if(Test-Path $File){
-		$filecontent = Get-Content($File)
-		attrib $file -r
+	if(Test-Path $TokenizedFile){
+		$TokenizedFilecontent = Get-Content($TokenizedFile)
+		attrib $TokenizedFile -r
 		for ($i=0; $i -le $Tokens.Count-1; $i++) {
 			$token = $TokenPattern -replace "TOKEN", $Tokens[$i]
 			Write-Output "Replacing token $token..."
-			$filecontent = $filecontent -replace $token,  $Replacements[$i]
+			$TokenizedFilecontent = $TokenizedFilecontent -replace $token,  $Replacements[$i]
    		}
-		Out-File -InputObject $filecontent -FilePath $File -Encoding utf8
+		Out-File -InputObject $TokenizedFilecontent -FilePath $TokenizedFile -Encoding utf8
 		Write-Output "Done."
+
+		if($TargetConfigFile){
+			if((Test-Path $TargetConfigFile) -eq $false){
+				Write-Output "Target config file does not exists. It will be created."
+			}
+			Copy-Item -Path $TokenizedFilecontent -Destination $TargetConfigFile -Force -ErrorAction SilentlyContinue
+		}
 	}
 	else{
-		throw "File not found '$File'"
+		throw "File not found '$TokenizedFile'"
 	}
 }
