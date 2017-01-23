1272a5d378e2960e78cd6314358a9fc945d71546
Martin Vega <mvegaca@outlook.com>
2017-01-23 14:12:49 +0100

    Add MVVMLightSplitViewProject template

---
+++ templates/MVVMLightSplitViewProject/MVVMLightSplitViewProject/Shell/ViewModelLocator.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using MVVMLightSplitViewProject.Shell;
+
+namespace MVVMLightSplitViewProject
+{
+    public partial class ViewModelLocator
+    {
+        public ShellViewModel ShellViewModel => ServiceLocator.Current.GetInstance<ShellViewModel>();
+
+        public void RegisterShell() => SimpleIoc.Default.Register<ShellViewModel>();
+    }
+}
+
