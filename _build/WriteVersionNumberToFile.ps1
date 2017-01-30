c8e6cb32b9b22c89432bf18bee75fda0c995b272
sibille <sibille@ilitia.com>
2017-01-30 13:41:37 +0100

    Extract version number from build number

--- _build/WriteVersionNumberToFile.ps1
+++ _build/WriteVersionNumberToFile.ps1
@@ -1,16 +1,28 @@
 ﻿[CmdletBinding()]
 Param(
   [Parameter(Mandatory=$True,Position=1)]
-  [string]$versionNumber,
+  [string]$buildNumber,
   [Parameter(Mandatory=$True,Position=2)]
   [string]$destinationDirectory,
   [Parameter(Mandatory=$True,Position=3)]
   [string]$destinationFile
 )
+
 Write-Host "Info..."
 Write-Host "Version Number" $versionNumber
 Write-Host "Destination Directory" $destinationDirectory
 Write-Host "Destination File" $destinationFile
+
+$TfsBuildVersionRegEx = "_v(\d+)\.(\S+)\.(\d+)"
+
+if($buildNumber -match $TfsBuildVersionRegEx){
+
+	$versionNumber = $matches[0].Replace("_","")
+    Write-Host "Version Number" $versionNumber
+}
+else{
+	throw "Build format does not match the expected pattern (buildName_version)"
+}
 
 Write-Host "Writing" $versionNumber "to" $destinationPath
 
