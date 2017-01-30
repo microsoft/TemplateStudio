7b793ff3f2b93a8b21fac930392c357715748394
sibille <sibille@ilitia.com>
2017-01-30 13:30:50 +0100

    Update gen version and added script to write version number

---
+++ _build/WriteVersionNumberToFile.ps1
@@ -0,0 +1,19 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$versionNumber,
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$destinationDirectory,
+  [Parameter(Mandatory=$True,Position=3)]
+  [string]$destinationFile
+)
+Write-Host "Info..."
+Write-Host "Version Number" $versionNumber
+Write-Host "Destination Directory" $destinationDirectory
+Write-Host "Destination File" $destinationFile
+
+Write-Host "Writing" $versionNumber "to" $destinationPath
+
+New-Item $destinationDirectory\$destinationFile -type file -force -value $versionNumber
+
+
