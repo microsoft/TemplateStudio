dbeab52b8b7c1627534fd9679798bdda1ee4da16
sibille <sibille@ilitia.com>
2017-02-14 15:02:31 +0100

    Add toast notification sample code

--- templates/BackgroundTaskFeature/BackgroundTaskFeature/BackgroundTaskFeature.cs
+++ templates/BackgroundTaskFeature/BackgroundTaskFeature/BackgroundTaskFeature.cs
@@ -1,4 +1,6 @@
 ﻿using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
 
 namespace BackgroundTaskFeature
 {
@@ -11,12 +13,51 @@ namespace BackgroundTaskFeature
             //TODO UWPTemplates: Insert the code that should be executed in the background task here. 
             //Remember to use a BackgroundTaskDeferral if you are executing asynchgronous methods.
             //Documentation: https://msdn.microsoft.com/windows/uwp/launch-resume/support-your-app-with-background-tasks
+
+            //This is an example of sending a toast notification from a background task
+            //Documentation: https://docs.microsoft.com/windows/uwp/controls-and-patterns/tiles-badges-notifications
+            var toast = ConstructToastNotification();
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+
         }
 
         private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
         {
            //TODO UWPTemplates: Insert code to handle the cancelation request here. 
            //Documentation: https://docs.microsoft.com/windows/uwp/launch-resume/handle-a-cancelled-background-task
+        }
+
+        private ToastNotification ConstructToastNotification()
+        {
+            string xml = 
+            $@"<toast activationType='foreground'>
+                    <visual>
+                        <binding template='ToastGeneric'>
+                            <text>Action - text</text>
+                            <text>Make sure left button on the toast has the text ""ok"" on it, and the right button has the text ""cancel"" on it.</text>
+                        </binding>
+                    </visual>
+                    <actions>
+                        <action
+                            content='ok'
+                            activationType='foreground'
+                            arguments='ok'/>
+                        <action
+                            content='cancel'
+                            activationType='foreground'
+                            arguments='cancel'/>
+                    </actions>
+                </toast>";
+
+            // Parse to XML
+            XmlDocument toastXml = new XmlDocument();
+            toastXml.LoadXml(xml);
+
+            // Generate toast
+            var toast = new ToastNotification(toastXml);
+
+            return toast;
         }
     }
 }
