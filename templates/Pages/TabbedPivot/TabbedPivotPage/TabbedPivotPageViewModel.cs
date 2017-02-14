2f7ebd485e65812c612dab8dab4fe5ebccc1d2bc
Martin Vega <mvegaca@outlook.com>
2017-02-14 12:00:41 +0100

    Add TabbedPivotPage template

---
+++ templates/Pages/TabbedPivot/TabbedPivotPage/TabbedPivotPageViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+//PostActionAnchor: MVVM NAMESPACE
+
+namespace ItemNamespace.TabbedPivotPage
+{
+    public class TabbedPivotPageViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        public TabbedPivotPageViewModel()
+        {
+        }        
+    }
+}
