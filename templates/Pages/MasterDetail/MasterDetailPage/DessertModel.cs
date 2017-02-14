cc1f8904f0d1264563c4ae969df68f3646e248cf
Martin Vega <mvegaca@outlook.com>
2017-02-14 10:43:16 +0100

    Add SplitView template

---
+++ templates/Pages/MasterDetail/MasterDetailPage/DessertModel.cs
@@ -0,0 +1,12 @@
+namespace ItemNamespace.MasterDetailPage
+{
+    public class DessertModel
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Picture { get; set; }
+        public string Subtitle { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+}
