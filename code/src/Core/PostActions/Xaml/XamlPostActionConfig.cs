971da101d08edaeb300e0afc925889cd1d6b120c
javitosanchez <javier.sanchez.m@live.com>
2017-02-17 14:50:55 +0100

    Xaml post action

---
+++ code/src/Core/PostActions/Xaml/XamlPostActionConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Templates.Core.PostActions.Xaml
+{
+    public class XamlPostActionConfig
+    {
+        public Element[] elements { get; set; }
+    }
+
+    public class Element
+    {
+        public string path { get; set; }
+        public string content { get; set; }
+    }
+}
---
+++ code/test/Core.Test/PostActions/XamlPostActionTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Templates.Core.PostActions;
+using Microsoft.Templates.Core.PostActions.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Microsoft.Templates.Core.Test.PostActions
+{
+    public class XamlPostActionTest
+    {
+        private const string Xaml = @"<Application
+                                        x:Class=""BlankProject.App""
+                                        xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                                        xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+                                        RequestedTheme=""Light"">
+
+                                        <Application.Resources>
+                                            <ResourceDictionary>
+                                            </ResourceDictionary>
+                                        </Application.Resources>
+                                    </Application>";
+
+        private const string EspectedXaml = @"<Application
+                                                x:Class=""BlankProject.App""
+                                                xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                                                xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+                                                RequestedTheme=""Light""
+                                                xmlns:local1=""using:TestProject1""
+                                                xmlns:local2=""using:TestProject2"">
+
+                                                <Application.Resources>
+                                                    <ResourceDictionary>
+                                                        <local1:ViewModelLocator x:Key=""Locator1"" />
+                                                        <local2:ViewModelLocator x:Key=""Locator2"" />
+                                                        <local1:ViewModelLocator2 x:Key=""Locator3"" />
+                                                    </ResourceDictionary>
+                                                </Application.Resources>
+                                            </Application>";
+
+        [Fact]
+        public void Execute()
+        {
+            var config = new XamlPostActionConfig
+            {
+                elements = new Element[]
+                {
+                    new Element
+                    {
+                        path = "/Application/Application.Resources/ResourceDictionary",
+                        content = "<local1:ViewModelLocator xmlns:local1=\"using:TestProject1\" xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" x:Key=\"Locator1\" />"
+                    },
+                    new Element
+                    {
+                        path = "/Application/Application.Resources/ResourceDictionary",
+                        content = "<local2:ViewModelLocator xmlns:local2=\"using:TestProject2\" xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" x:Key=\"Locator2\" />"
+                    },
+                    new Element
+                    {
+                        path = "/Application/Application.Resources/ResourceDictionary",
+                        content = "<local1:ViewModelLocator2 xmlns:local1=\"using:TestProject1\" xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" x:Key=\"Locator3\" />"
+                    },
+                }
+            };
+            var target = new XamlPostAction();
+
+            var result = target.Execute(config, Xaml);
+
+            Assert.NotNull(result);
+            Assert.Equal(XElement.Parse(EspectedXaml).ToString(), XElement.Parse(result).ToString());
+        }
+    }
+}
