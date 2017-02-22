c7a5e993c930e4256a34c0dfbc2532cb1588c33d
Martin Vega <mvegaca@outlook.com>
2017-02-22 12:19:27 +0100

    Finish Live Tile Service

--- templates/Frameworks/Basic/Templates/Project/Services/NavigationService.cs
+++ templates/Frameworks/Basic/Templates/Project/Services/NavigationService.cs
@@ -5,35 +5,23 @@ using Windows.UI.Xaml.Media.Animation;
 
 namespace RootNamespace.Services
 {
-    public class NavigationService
+    public static class NavigationService
     {
-        private static readonly NavigationService _instance = new NavigationService();
-        public static NavigationService Instance
-        {
-            get => _instance;
-        }
+        private static Frame _frame = Window.Current.Content as Frame;
+        public static void SetNavigationFrame(Frame frame) => _frame = frame;
 
-        private Frame _frame;
+        public static bool CanGoBack => _frame.CanGoBack;
+        public static bool CanGoForward => _frame.CanGoForward;
 
-        private NavigationService()
-        {
-            _frame = Window.Current.Content as Frame;
-        }        
+        public static void GoBack() => _frame.GoBack();
+        public static void GoForward() => _frame.GoForward();
 
-        public void SetNavigationFrame(Frame frame) => _frame = frame;
+        public static bool Navigate<T>() where T : Page => _frame.Navigate(typeof(T));
+        public static bool Navigate<T>(object parameter) where T : Page => _frame.Navigate(typeof(T), parameter);
+        public static bool Navigate<T>(object parameter, NavigationTransitionInfo infoOverride) where T : Page => _frame.Navigate(typeof(T), parameter, infoOverride);
 
-        public bool CanGoBack => _frame.CanGoBack;
-        public bool CanGoForward => _frame.CanGoForward;
-
-        public void GoBack() => _frame.GoBack();
-        public void GoForward() => _frame.GoForward();
-
-        public bool Navigate<T>() where T : Page => _frame.Navigate(typeof(T));
-        public bool Navigate<T>(object parameter) where T : Page => _frame.Navigate(typeof(T), parameter);
-        public bool Navigate<T>(object parameter, NavigationTransitionInfo infoOverride) where T : Page => _frame.Navigate(typeof(T), parameter, infoOverride);
-
-        public bool Navigate(Type pageType) => _frame.Navigate(pageType);
-        public bool Navigate(Type pageType, object parameter) => _frame.Navigate(pageType, parameter);
-        public bool Navigate(Type pageType, object parameter, NavigationTransitionInfo infoOverride) => _frame.Navigate(pageType, parameter, infoOverride);
+        public static bool Navigate(Type pageType) => _frame.Navigate(pageType);
+        public static bool Navigate(Type pageType, object parameter) => _frame.Navigate(pageType, parameter);
+        public static bool Navigate(Type pageType, object parameter, NavigationTransitionInfo infoOverride) => _frame.Navigate(pageType, parameter, infoOverride);
     }
 }
