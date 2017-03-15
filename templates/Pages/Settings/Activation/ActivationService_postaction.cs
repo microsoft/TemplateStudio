9e8676eee08dc8d33dd477f38adeaae36bffe297
javitosanchez <javier.sanchez.m@live.com>
2017-03-15 19:02:02 +0100

    Settings init in ActivationService

---
+++ templates/Pages/Settings/Activation/ActivationService_postaction.cs
@@ -0,0 +1,4 @@
+private async Task InitializeAsync()
+{
+    Services.ThemeSelectorService.SetRequestedTheme();
+}
--- templates/Pages/Settings/App_postaction.xaml.cs
+++
@@ -1,6 +0,0 @@
-﻿protected override async void OnLaunched(LaunchActivatedEventArgs e)
-{
-    //^^
-
-    Services.ThemeSelectorService.SetRequestedTheme();
-}
