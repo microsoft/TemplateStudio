7abe6f0a7d4e661f4b66379aab4b7244c2749723
javitosanchez <javier.sanchez.m@live.com>
2017-03-15 18:50:10 +0100

    Code review bugs result for MVVM

--- templates/Frameworks/Basic/Templates/Project/_Mvvm/RelayCommand.cs
+++ templates/Frameworks/Basic/Templates/Project/_Mvvm/RelayCommand.cs
@@ -5,49 +5,39 @@ namespace RootNamespace.Mvvm
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action execute;
-        private readonly Func<bool> canExecute;
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
 
         public event EventHandler CanExecuteChanged;
         public RelayCommand(Action execute) : this(execute, null) { }
 
         public RelayCommand(Action execute, Func<bool> canExecute)
         {
-            if(execute == null)
-            {
-                throw new ArgumentNullException("execute");
-            }
-
-            this.execute = execute;
-            this.canExecute = canExecute;
+            this._execute = execute ?? throw new ArgumentNullException("execute");
+            this._canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter) => this.canExecute == null || this.canExecute();
-        public void Execute(object parameter) => this.execute();
-        public void OnCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+        public void Execute(object parameter) => _execute();
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 
     public class RelayCommand<T> : ICommand
     {
-        private readonly Action<T> execute;
-        private readonly Func<T, bool> canExecute;
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
 
         public event EventHandler CanExecuteChanged;
         public RelayCommand(Action<T> execute) : this(execute, null) { }
 
         public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
         {
-            if(execute == null)
-            {
-                throw new ArgumentNullException("execute");
-            }
-
-            this.execute = execute;
-            this.canExecute = canExecute;
+            this._execute = execute ?? throw new ArgumentNullException("execute");
+            this._canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter) => this.canExecute == null || this.canExecute((T)parameter);
-        public void Execute(object parameter) => this.execute((T)parameter);
-        public void OnCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute((T)parameter);
+        public void Execute(object parameter) => _execute((T)parameter);
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
