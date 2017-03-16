ef7c8eb63c0af7f3fd1f9fb852508a31a735f606
javitosanchez <javier.sanchez.m@live.com>
2017-03-16 18:53:08 +0100

    Templates repository structure refactor

---
+++ code/src/Core/MetadataInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Templates.Core
+{
+    public class MetadataInfo
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+    }
+}
