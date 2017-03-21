cdf2e06598e94e0e8baf672040f322339bb5840b
Raúl Alarcón <ralarcon@microsoft.com>
2017-03-21 15:01:32 +0100

    Add Strong Versioning

--- _build/Extension-SetIdentityAndVersion.ps1
+++ _build/Extension-SetIdentityAndVersion.ps1
@@ -10,7 +10,10 @@ Param(
   [string]$vsixDisplayName,
 
   [Parameter(Mandatory=$True,Position=4)]
-  [string]$buildNumber
+  [string]$buildNumber,
+
+  [Parameter(Mandatory=$False,Position=3)]
+  [string]$publicKeyToken = "e4ef4cc7a47ae0c5" #TestKey.snk
 )
 
 $VersionRegex = "(\d+)\.(\d+)\.(\d+)\.(\d+)"
@@ -44,24 +47,49 @@ else{
   throw "Identity is mandatory."
 }
 
-## APPLY VERSION TO ASSEMBLY FILES
+## APPLY VERSION TO ASSEMBLY FILES (AssemblyVersion and AssemblyFileVersion)
 Write-Host "Applying version to AssemblyInfo Files in matching the path pattern '$codePathPattern'" 
 $files = Get-ChildItem -include "*AssemblyInfo.cs" -Recurse |  Where-Object{ $_.FullName -notmatch "\\Templates\\" }
 if($files)
 {
     Write-Host "Will apply $versionNumber to $($files.count) files."
 
-    $assemblyFileVersionRegEx = "AssemblyFileVersion\(""(\d+)\.(\d+)\.(\d+)\.(\d+)""\)" 
-    $assemblyFileVersionReplacement = "AssemblyFileVersion(""$versionNumber"")"
+    $assemblyVersionRegEx = "\(""$VersionRegex""\)" 
+    $assemblyVersionReplacement = "(""$versionNumber"")"
 
     foreach ($file in $files) {
         $filecontent = Get-Content($file)
         attrib $file -r
-        $filecontent -replace $assemblyFileVersionRegEx, $assemblyFileVersionReplacement | Out-File $file utf8
+        $filecontent -replace $assemblyVersionRegEx, $assemblyVersionReplacement | Out-File $file utf8
         Write-Host "$file - version applied"
     }
 }
 else
 {
     Write-Warning "No files found to apply version."
+}
+
+## APPLY VERSION TO PROJECT TEMPLATE WIZARD REFERENCE
+if($publicKeyToken){
+  Write-Host "Setting Wizard Extension configuration in Project Template"
+  $projectTemplate = Get-ChildItem -include "*.vstemplate" -recurse |  Where-Object{ $_.FullName -notmatch "\\Templates\\" -and $_.FullName -match "\\ProjectTemplates\\"}
+  if($projectTemplate){
+    [xml]$projectTemplateContent = Get-Content $projectTemplate
+
+    $newPublicKeyToken = "PublicKeyToken=$publicKeyToken"
+    $wizardAssemblyStrongName = $projectTemplateContent.VSTemplate.WizardExtension.Assembly -replace $VersionRegEx, $versionNumber 
+    $wizardAssemblyStrongName = $wizardExtensioAssembly -replace "PublicKeyToken=.*</Assembly>", "$newPublicKeyToken</Assembly>"
+
+    $projectTemplateContent.VSTemplate.WizardExtension.Assembly = $wizardAssemblyStrongName
+    
+    $projectTemplateContent.Save($projectTemplate)
+
+    Write-Host "$projectTemplate - Wizard Assembly Strong Name updated ($wizardAssemblyStrongName)"
+  }
+  else{
+    throw "No Project Template manifest file found!"
+  }
+}
+else{
+  throw "Public key token not set."
 }
