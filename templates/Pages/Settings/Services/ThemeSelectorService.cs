43a54dfa773c7f8186cfd7023edde2edd63835aa
Martin Vega <mvegaca@outlook.com>
2017-03-22 10:06:42 +0100

    Code review of ThemeSelectorService

--- templates/Pages/Settings/Services/ThemeSelectorService.cs
+++ templates/Pages/Settings/Services/ThemeSelectorService.cs
@@ -5,18 +5,12 @@ using RootNamespace.Extensions;
 
 namespace RootNamespace.Services
 {
-    public enum Theme
-    {
-        Light,
-        Dark
-    }
-
     public static class ThemeSelectorService
     {
         private const string SettingsKey = "RequestedTheme";
 
-        public static bool IsLightThemeEnabled => Theme == Theme.Light;
-        public static Theme Theme { get; set; }
+        public static bool IsLightThemeEnabled => Theme == ElementTheme.Light;
+        public static ElementTheme Theme { get; set; }
 
         static ThemeSelectorService()
         {
@@ -25,46 +19,48 @@ namespace RootNamespace.Services
 
         public static void SwitchTheme()
         {
-            if (Theme == Theme.Dark)
+            if (Theme == ElementTheme.Dark)
             {
-                SetTheme(Theme.Light);
+                SetTheme(ElementTheme.Light);
             }
             else
             {
-                SetTheme(Theme.Dark);
+                SetTheme(ElementTheme.Dark);
             }
         }
-        public static void SetTheme(Theme theme)
+        
+        public static void SetTheme(ElementTheme theme)
         {
             Theme = theme;
             SetRequestedTheme();
             SaveThemeInSettings(Theme);
         }
+
         public static void SetRequestedTheme()
         {
-            var elementTheme = Theme == Theme.Dark ? ElementTheme.Dark : ElementTheme.Light;
             var frameworkElement = Window.Current.Content as FrameworkElement;
             if (frameworkElement != null)
             {
-                frameworkElement.RequestedTheme = elementTheme;
+                frameworkElement.RequestedTheme = Theme;
             }
         }
 
-        private static Theme LoadThemeFromSettings()
+        private static ElementTheme LoadThemeFromSettings()
         {
-            Theme cacheTheme = Theme.Light;
+            ElementTheme cacheTheme = ElementTheme.Light;
             string themeName = ApplicationData.Current.LocalSettings.Read<string>(SettingsKey);
             if (String.IsNullOrEmpty(themeName))
             {
-                cacheTheme = Application.Current.RequestedTheme == ApplicationTheme.Dark ? Theme.Dark : Theme.Light;
+                cacheTheme = Application.Current.RequestedTheme == ApplicationTheme.Dark ? ElementTheme.Dark : ElementTheme.Light;
             }
             else
             {
-                Enum.TryParse<Theme>(themeName, out cacheTheme);
+                Enum.TryParse<ElementTheme>(themeName, out cacheTheme);
             }
             return cacheTheme;
         }
-        private static void SaveThemeInSettings(Theme theme)
+
+        private static void SaveThemeInSettings(ElementTheme theme)
         {
             ApplicationData.Current.LocalSettings.Save<string>(SettingsKey, theme.ToString());
         }
