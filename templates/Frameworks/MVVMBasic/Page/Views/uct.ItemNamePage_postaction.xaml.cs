95a316cebb6d4b01f42f60fa818760c2f5f4c293
javitosanchez <javier.sanchez.m@live.com>
2017-04-05 19:11:45 +0200

    composition templates for mvvmbasic

--- templates/Frameworks/MVVMBasic/Page/Views/uct.ItemNamePage_postaction.xaml.cs
+++
@@ -1,8 +0,0 @@
-using ItemNamespace.ViewModels;
-namespace ItemNamespace.Views
-{
-    public sealed partial class uct.ItemNamePage : Page
-    {    
-        public uct.ItemNameViewModel ViewModel { get; } = new uct.ItemNameViewModel();
-    }
-}
