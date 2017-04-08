c5dd4a6e6e2d3ef074e17fe82cb95b0f0838b1fc
crutkas <Clint@rutkas.com>
2017-04-08 11:12:03 -0700

    Bug fix for #155

--- templates/Pages/MasterDetail.CodeBehind/Models/SampleModel.cs
+++ templates/Pages/MasterDetail.CodeBehind/Models/SampleModel.cs
@@ -5,8 +5,12 @@ namespace ItemNamespace.Models
     public class SampleModel
     {
         public string Title { get; set; }
-        public Symbol Symbol { get; set; }
-        public char SymbolAsChar => (char)Symbol;
         public string Description { get; set; }
+        public Symbol Symbol { get; set; }
+
+        public char SymbolAsChar
+        {
+            get { return (char)Symbol; }
+        }
     }
 }
