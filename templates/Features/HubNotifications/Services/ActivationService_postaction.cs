eb1caa0dde4e42bbe3628aa8b0776675f3e55cc9
Martin Vega <mvegaca@outlook.com>
2017-04-24 16:40:34 +0200

    Fix PostAction

--- templates/Features/HubNotifications/Services/ActivationService_postaction.cs
+++ templates/Features/HubNotifications/Services/ActivationService_postaction.cs
@@ -8,15 +8,15 @@ namespace Param_ItemNamespace.Services
         private async Task StartupAsync()
         {
             //TODO UWPTemplates: To use the HubNotificationService especific data related with your Azure Notification Hubs is required.
-            //  1. Go to the HubNotificationsService class, in the InitializeAsync() method, provide the Hub Name and DefaultListenSharedAccessSignature.
+            //  1. Go to the HubNotificationsFeatureService class, in the InitializeAsync() method, provide the Hub Name and DefaultListenSharedAccessSignature.
             //  2. Uncomment the following line (an exception is thrown if it is executed before the previous information is provided).
 
-            //Singleton<HubNotificationsService>.Instance.InitializeAsync();
+            //Singleton<HubNotificationsFeatureService>.Instance.InitializeAsync();
         }
 
         private IEnumerable<ActivationHandler> GetActivationHandlers()
         {
-            yield return Singleton<HubNotificationsService>.Instance;
+            yield return Singleton<HubNotificationsFeatureService>.Instance;
         }
     }
 }
