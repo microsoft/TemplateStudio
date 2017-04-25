4536a3b9e36164a90a59bc8173de2ecefce35723
javitosanchez <javier.sanchez.m@live.com>
2017-04-25 12:34:12 +0200

    Licenses in summary

---
+++ code/src/Core/Templates/TemplateLicense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Templates.Core
+{
+    public class TemplateLicense
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+    }
+}
--- code/src/UI/ViewModels/ProjectTemplatesViewModel.cs
+++ code/src/UI/ViewModels/ProjectTemplatesViewModel.cs
@@ -35,7 +35,6 @@ namespace Microsoft.Templates.UI.ViewModels
 
         public ObservableCollection<SummaryItemViewModel> SummaryPages { get; } = new ObservableCollection<SummaryItemViewModel>();
         public ObservableCollection<SummaryItemViewModel> SummaryFeatures { get; } = new ObservableCollection<SummaryItemViewModel>();
-        public ObservableCollection<SummaryLicenceViewModel> SummaryLicences { get; } = new ObservableCollection<SummaryLicenceViewModel>();
 
         public List<(string Name, ITemplateInfo Template)> SavedTemplates { get; } = new List<(string Name, ITemplateInfo Template)>();
 
@@ -87,6 +86,7 @@ namespace Microsoft.Templates.UI.ViewModels
             if (SavedTemplates == null || SavedTemplates.Count == 0)
             {
                 AddFromLayout(ContextProjectType.Name, ContextFramework.Name);
+                MainViewModel.Current.RebuildLicenses();
             }
 
             await Task.CompletedTask;
@@ -116,6 +116,13 @@ namespace Microsoft.Templates.UI.ViewModels
         }
 
         private void OnAddItem((string Name, ITemplateInfo Template) item, bool isRemoveEnabled = true)
+        {
+            AddItem(item, isRemoveEnabled);
+
+            MainViewModel.Current.RebuildLicenses();
+        }
+
+        private void AddItem((string Name, ITemplateInfo Template) item, bool isRemoveEnabled = true)
         {
             SaveNewTemplate(item);
             var newTemplates = GenComposer.GetNewDependencies(item.Template, SavedTemplates.Select(s => s.Template).ToList());
@@ -151,7 +158,6 @@ namespace Microsoft.Templates.UI.ViewModels
                     IsRemoveEnabled = isRemoveEnabled
                 });
             }
-            GenComposer.AddMissingLicences(item.Template, SummaryLicences);
             OnPropertyChanged("GetUsedTemplatesIdentitiesFunc");            
         }
 
@@ -174,6 +180,8 @@ namespace Microsoft.Templates.UI.ViewModels
             }
             SavedTemplates.Remove(SavedTemplates.First(st => st.Name == item.ItemName));
             OnPropertyChanged("GetUsedTemplatesIdentitiesFunc");
+
+            MainViewModel.Current.RebuildLicenses();
         }
     }
 }
