01eb918be961f99e4e3171cdfd10755bb5581d2a
Martin Vega <mvegaca@outlook.com>
2017-04-28 11:14:36 +0200

    Hide author from templates made by Microsoft

---
+++ code/src/UI/Converters/MicrosoftTemplatesAuthorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Microsoft.Templates.UI.Converters
+{
+    public class MicrosoftTemplatesAuthorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value.ToString().ToLower() == "microsoft")
+            {
+                return Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
