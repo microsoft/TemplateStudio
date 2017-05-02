9aa6f974224bd498e616cdf87efb039799d6c273
Matt Lacey <matt@mrlacey.co.uk>
2017-05-02 02:19:08 +0100

    Abstract commonality in MetadataInfo & TemplateInfo into share base
    Issue #244

--- code/src/UI/ViewModels/TemplateInfoViewModel.cs
+++ code/src/UI/ViewModels/TemplateInfoViewModel.cs
@@ -15,20 +15,12 @@ using System.Linq;
 
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.Templates.Core;
-using Microsoft.Templates.Core.Mvvm;
 using System.Collections.ObjectModel;
 
 namespace Microsoft.Templates.UI.ViewModels
 {
-    public class TemplateInfoViewModel : Observable
+    public class TemplateInfoViewModel : CommonInfoViewModel
     {
-        private string _name;
-        public string Name
-        {
-            get => _name;
-            set => SetProperty(ref _name, value);
-        }
-
         private string _templateName;
         public string TemplateName
         {
@@ -36,68 +28,32 @@ namespace Microsoft.Templates.UI.ViewModels
             set => SetProperty(ref _templateName, value);
         }
 
-
-        private string _author;
-        public string Author
-        {
-            get => _author;
-            set => SetProperty(ref _author, value);
-        }
-
-        private string _summary;
-        public string Summary
-        {
-            get { return _summary; }
-            set { SetProperty(ref _summary, value); }
-        }
-
-        private string _description;
-        public string Description
-        {
-            get { return _description; }
-            set { SetProperty(ref _description, value); }
-        }
-
-        private string _icon;
-        public string Icon
-        {
-            get { return _icon; }
-            set { SetProperty(ref _icon, value); }
-        }
-
         private string _version;
         public string Version
         {
-            get { return _version; }
-            set { SetProperty(ref _version, value); }
+            get => _version;
+            set => SetProperty(ref _version, value);
         }
 
         private int _order;
         public int Order
         {
-            get { return _order; }
-            set { SetProperty(ref _order, value); }
+            get => _order;
+            set => SetProperty(ref _order, value);
         }
 
         private bool _multipleInstances;
         public bool MultipleInstances
         {
-            get { return _multipleInstances; }
-            set { SetProperty(ref _multipleInstances, value); }
+            get => _multipleInstances;
+            set => SetProperty(ref _multipleInstances, value);
         }
-
-        private IEnumerable<TemplateLicense> _licenseTerms;
-        public IEnumerable<TemplateLicense> LicenseTerms
-        {
-            get { return _licenseTerms; }
-            set { SetProperty(ref _licenseTerms, value); }
-        }        
 
         private string _group;
         public string Group
         {
-            get { return _group; }
-            set { SetProperty(ref _group, value); }
+            get => _group;
+            set => SetProperty(ref _group, value);
         }
 
         private bool _isEnabled;
@@ -110,17 +66,17 @@ namespace Microsoft.Templates.UI.ViewModels
         private string _dependencies;
         public string Dependencies
         {
-            get { return _dependencies; }
-            set { SetProperty(ref _dependencies, value); }
+            get => _dependencies;
+            set => SetProperty(ref _dependencies, value);
         }
 
-        public ObservableCollection<DependencyInfoViewModel> DependencyItems { get; } = new ObservableCollection<DependencyInfoViewModel>();        
+        public ObservableCollection<DependencyInfoViewModel> DependencyItems { get; } = new ObservableCollection<DependencyInfoViewModel>();
 
         private TemplateType _templateType;
         public TemplateType TemplateType
         {
-            get { return _templateType; }
-            set { SetProperty(ref _templateType, value); }
+            get => _templateType;
+            set => SetProperty(ref _templateType, value);
         }
 
         public ITemplateInfo Template { get; set; }
@@ -157,4 +113,3 @@ namespace Microsoft.Templates.UI.ViewModels
         }
     }
 }
- 
