8a2923a1476c33ead03a4e99187cbad863b194a6
Martin Vega <mvegaca@outlook.com>
2017-05-03 11:15:13 +0200

    Code refactor

---
+++ code/src/UI/Controls/TemplateInfoControl/TemplateInfoControl.Members.cs
@@ -0,0 +1,109 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using Microsoft.Templates.Core.Mvvm;
+using Microsoft.Templates.UI.ViewModels;
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Microsoft.Templates.UI.Controls
+{
+    public partial class TemplateInfoControl
+    {
+        #region TemplateInfo
+        public TemplateInfoViewModel TemplateInfo
+        {
+            get { return (TemplateInfoViewModel)GetValue(TemplateInfoProperty); }
+            set { SetValue(TemplateInfoProperty, value); }
+        }
+        public static readonly DependencyProperty TemplateInfoProperty = DependencyProperty.Register("TemplateInfo", typeof(TemplateInfoViewModel), typeof(TemplateInfoControl), new PropertyMetadata(null));
+        #endregion
+
+        #region NoEditingContentVisibility
+        public Visibility NoEditingContentVisibility
+        {
+            get { return (Visibility)GetValue(NoEditingContentVisibilityProperty); }
+            set { SetValue(NoEditingContentVisibilityProperty, value); }
+        }
+        public static readonly DependencyProperty NoEditingContentVisibilityProperty = DependencyProperty.Register("NoEditingContentVisibility", typeof(Visibility), typeof(TemplateInfoControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region EditingContentVisibility
+        public Visibility EditingContentVisibility
+        {
+            get { return (Visibility)GetValue(EditingContentVisibilityProperty); }
+            set { SetValue(EditingContentVisibilityProperty, value); }
+        }
+        public static readonly DependencyProperty EditingContentVisibilityProperty = DependencyProperty.Register("EditingContentVisibility", typeof(Visibility), typeof(TemplateInfoControl), new PropertyMetadata(Visibility.Collapsed));
+        #endregion
+
+        #region AddingVisibility
+        public Visibility AddingVisibility
+        {
+            get { return (Visibility)GetValue(AddingVisibilityProperty); }
+            set { SetValue(AddingVisibilityProperty, value); }
+        }
+        public static readonly DependencyProperty AddingVisibilityProperty = DependencyProperty.Register("AddingVisibility", typeof(Visibility), typeof(TemplateInfoControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region ErrorMessage
+        public string ErrorMessage
+        {
+            get { return (string)GetValue(ErrorMessageProperty); }
+            set { SetValue(ErrorMessageProperty, value); }
+        }
+        public static readonly DependencyProperty ErrorMessageProperty = DependencyProperty.Register("ErrorMessage", typeof(string), typeof(TemplateInfoControl), new PropertyMetadata(String.Empty));
+        #endregion
+
+        #region NewTemplateName
+        public string NewTemplateName
+        {
+            get { return (string)GetValue(NewTemplateNameProperty); }
+            set { SetValue(NewTemplateNameProperty, value); }
+        }
+        public static readonly DependencyProperty NewTemplateNameProperty = DependencyProperty.Register("NewTemplateName", typeof(string), typeof(TemplateInfoControl), new PropertyMetadata(String.Empty, OnNewTemplateNameChanged));
+        #endregion
+
+        #region IsValidName
+        public bool IsValidName
+        {
+            get { return (bool)GetValue(IsValidNameProperty); }
+            set { SetValue(IsValidNameProperty, value); }
+        }
+        public static readonly DependencyProperty IsValidNameProperty = DependencyProperty.Register("IsValidName", typeof(bool), typeof(TemplateInfoControl), new PropertyMetadata(true));
+        #endregion
+
+        #region TitleForeground
+        public SolidColorBrush TitleForeground
+        {
+            get => (SolidColorBrush)GetValue(TitleForegroundProperty);
+            set => SetValue(TitleForegroundProperty, value);
+        }
+        public static readonly DependencyProperty TitleForegroundProperty = DependencyProperty.Register("TitleForeground", typeof(SolidColorBrush), typeof(TemplateInfoControl), new PropertyMetadata(null));
+        #endregion
+
+        private ICommand _addItemCommand;
+        public ICommand AddItemCommand => _addItemCommand ?? (_addItemCommand = new RelayCommand(OnAddItem));
+
+        private ICommand _saveItemCommand;
+        public ICommand SaveItemCommand => _saveItemCommand ?? (_saveItemCommand = new RelayCommand(OnSaveItem));
+
+        private ICommand _showItemInfoCommand;
+        public ICommand ShowItemInfoCommand => _showItemInfoCommand ?? (_showItemInfoCommand = new RelayCommand(OnShowItemInfo));
+
+        private ICommand _closeEditionCommand;
+        public ICommand CloseEditionCommand => _closeEditionCommand ?? (_closeEditionCommand = new RelayCommand(SwichVisibilities));
+    }
+}
---
+++ code/src/UI/Controls/TemplateInfoControl/TemplateInfoControl.xaml.cs
@@ -0,0 +1,137 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using Microsoft.Templates.Core;
+using Microsoft.Templates.UI.Resources;
+using Microsoft.Templates.UI.ViewModels;
+
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Microsoft.Templates.UI.Controls
+{
+    public partial class TemplateInfoControl : UserControl
+    {
+        public TemplateInfoControl()
+        {
+            InitializeComponent();
+            TitleForeground = FindResource("UIBlack") as SolidColorBrush;
+            MainViewModel.Current.ProjectTemplates.UpdateTemplateAvailable += (sender, args) => CheckAddingStatus();
+            Loaded += (sender, args) => CheckAddingStatus();
+        }
+
+        private void OnAddItem()
+        {
+            var names = MainViewModel.Current.ProjectTemplates.GetUsedNamesFunc.Invoke();
+
+            if (TemplateInfo.CanChooseItemName)
+            {
+                NewTemplateName = Naming.Infer(names, TemplateInfo.Template.GetDefaultName());
+                SwichVisibilities();
+
+                templateName.Focus();
+                templateName.SelectAll();
+            }
+            else
+            {
+                NewTemplateName = TemplateInfo.Template.GetDefaultName();
+                MainViewModel.Current.ProjectTemplates.AddCommand.Execute((NewTemplateName, TemplateInfo.Template));
+
+                CheckAddingStatus();
+            }
+        }
+
+        private void OnSaveItem()
+        {
+            if (IsValidName)
+            {
+                MainViewModel.Current.ProjectTemplates.AddCommand.Execute((NewTemplateName, TemplateInfo.Template));
+
+                SwichVisibilities();
+                CheckAddingStatus();
+            }
+        }
+
+        private void CheckAddingStatus()
+        {
+            if (TemplateInfo.MultipleInstances == false && IsAlreadyDefined)
+            {
+                AddingVisibility = Visibility.Collapsed;
+                TitleForeground = FindResource("UIMiddleLightGray") as SolidColorBrush;
+            }
+            else
+            {
+                AddingVisibility = Visibility.Visible;
+                TitleForeground = FindResource("UIBlack") as SolidColorBrush;
+            }
+        }
+
+        private void OnShowItemInfo()
+        {
+            MainViewModel.Current.ProjectTemplates.ShowInfoCommand.Execute(TemplateInfo);
+        }
+
+        private bool IsAlreadyDefined => MainViewModel.Current.ProjectTemplates.GetUsedTemplatesIdentitiesFunc.Invoke().Any(name => name == TemplateInfo.Template.Identity);
+
+        private void SwichVisibilities()
+        {
+            if (EditingContentVisibility == Visibility.Collapsed)
+            {
+                EditingContentVisibility = Visibility.Visible;
+                NoEditingContentVisibility = Visibility.Collapsed;
+            }
+            else
+            {
+                EditingContentVisibility = Visibility.Collapsed;
+                NoEditingContentVisibility = Visibility.Visible;
+            }
+        }
+
+        private void HandleValidation(Core.ValidationResult validationResult)
+        {
+            IsValidName = validationResult.IsValid;
+            ErrorMessage = String.Empty;
+
+            if (!IsValidName)
+            {
+                ErrorMessage = StringRes.ResourceManager.GetString($"ValidationError_{validationResult.ErrorType}");
+
+                if (string.IsNullOrWhiteSpace(ErrorMessage))
+                {
+                    ErrorMessage = "UndefinedError";
+                }
+
+                throw new Exception(ErrorMessage);
+            }
+        }
+
+        public void Validate(string name)
+        {
+            var names = MainViewModel.Current.ProjectTemplates.GetUsedNamesFunc.Invoke();
+            var validationResult = Core.Naming.Validate(names, name);
+
+            HandleValidation(validationResult);
+        }
+
+        private static void OnNewTemplateNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as TemplateInfoControl;
+            if (control.TemplateInfo.CanChooseItemName)
+            {
+                control.Validate(e.NewValue as string);
+            }
+        }
+    }
+}
