9d1ffaf17f3054425eddef10c071bb8efa72631b
Matt Lacey <matt@mrlacey.co.uk>
2017-05-16 16:02:38 -0700

    Add basic grid page
    #128

---
+++ templates/Pages/Grid/Views/GridViewPage.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Param_ItemNamespace.Views
+{
+    public sealed partial class GridViewPage : Page
+    {
+        public GridViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
