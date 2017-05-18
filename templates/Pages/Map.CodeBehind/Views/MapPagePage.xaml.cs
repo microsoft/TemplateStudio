f45bfba1da743ce48885261f879964f411f00fa3
Matt Lacey <matt@mrlacey.co.uk>
2017-05-18 23:50:04 +0100

    Allow arbitrary insertion of "fragments" into generated code
    For issue #138
    - Allows insertion of pieces of code within a file.
    - Path to the fragment file is relative to the template root folder
    - Sample shows deduping of properties in the Maps View/ViewModel
    - File naming and location of fragments files intended as a guide
    - Intentionally done at the end of the generation process and does not
    support merging of anything within a fragment to avoid letting the
    composition process getting too complicated.
    - Intentionally do not support nested merging to try and keep things
    simple
    - No tests added as not sure the best way to add something specific.
    Existing test prove this works or generated apps wouldn't compile
    - Should be easy to extend for XAML as well if we want to add this.

--- templates/Pages/Map.CodeBehind/Views/MapPagePage.xaml.cs
+++ templates/Pages/Map.CodeBehind/Views/MapPagePage.xaml.cs
@@ -12,31 +12,7 @@ namespace Param_ItemNamespace.Views
 {
     public sealed partial class MapPagePage : Page, System.ComponentModel.INotifyPropertyChanged
     {
-        // TODO UWPTemplates: Set your preferred default zoom level
-        private const double defaultZoomLevel = 17;
-
-        private readonly LocationService locationService;
-
-        // TODO UWPTemplates: Set your preferred default location if a geolock can't be found.
-        private readonly BasicGeoposition defaultPosition = new BasicGeoposition()
-        {
-            Latitude = 47.609425,
-            Longitude = -122.3417
-        };
-
-        private double _zoomLevel;
-        public double ZoomLevel
-        {
-            get { return _zoomLevel; }
-            set { Set(ref _zoomLevel, value); }
-        }
-
-        private Geopoint _center;
-        public Geopoint Center
-        {
-            get { return _center; }
-            set { Set(ref _center, value); }
-        }
+// FRAGMENT _fragments\Page_Map_Properties.fragment.cs
 
         public MapPagePage()
         {
