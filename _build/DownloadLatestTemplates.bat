7bec5f306e2cc6a62a032b41aa3c5d5766e09843
Raúl Alarcón <ralarcon@microsoft.com>
2017-05-19 01:26:31 +0200

    Reorg params

--- _build/DownloadLatestTemplates.bat
+++ _build/DownloadLatestTemplates.bat
@@ -1,3 +1,3 @@
 IF EXIST "%2" (DEL %2 /S)
 
-%ProgramFiles(x86)%\Microsoft SDKs\Azure\AzCopy\AzCopy.exe /Source:%2 /SourceKey:%3 /Dest:%4 /Y
+%ProgramFiles(x86)%\Microsoft SDKs\Azure\AzCopy\AzCopy.exe /Source:%1 /SourceKey:%3 /Dest:%2 /Y
