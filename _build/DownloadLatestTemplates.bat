ce94b6cfd1eead615e8ae93d14372a3b81962c66
Raúl Alarcón <ralarcon@microsoft.com>
2017-05-19 01:31:06 +0200

    Avoid whitespaces

--- _build/DownloadLatestTemplates.bat
+++ _build/DownloadLatestTemplates.bat
@@ -1,3 +1,3 @@
 IF EXIST "%2" (DEL %2 /S)
 
-%ProgramFiles(x86)%\Microsoft SDKs\Azure\AzCopy\AzCopy.exe /Source:%1 /SourceKey:%3 /Dest:%2 /Y
+"%ProgramFiles(x86)%\Microsoft SDKs\Azure\AzCopy\AzCopy.exe" /Source:%1 /SourceKey:%3 /Dest:%2 /Y
