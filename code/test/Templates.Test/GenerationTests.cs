cb669a69c66272ae8f9b22f521c47499c5ac5f28
Martin Vega <mvegaca@outlook.com>
2017-05-23 16:39:37 +0200

    Add style resources dictionaries

--- code/test/Templates.Test/GenerationTests.cs
+++ code/test/Templates.Test/GenerationTests.cs
@@ -151,7 +151,7 @@ namespace Microsoft.Templates.Test
             Assert.True(result.exitCode.Equals(0), $"Solution {targetProjectTemplate.Name} was not built successfully. {Environment.NewLine}Errors found: {GetErrorLines(result.outputFile)}.{Environment.NewLine}Please see {Path.GetFullPath(result.outputFile)} for more details.");
 
             //Clean
-            Directory.Delete(outputPath, true);
+            //Directory.Delete(outputPath, true);
         }
 
         [Theory, MemberData("GetProjectTemplates"), Trait("Type", "ProjectGeneration")]
