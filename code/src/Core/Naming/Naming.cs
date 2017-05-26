df01dcf83f2cd5b6c35c33b01335b32db7df31eb
sibille <sibille@ilitia.com>
2017-05-26 08:50:16 +0200

    Added header and cleanup usings

--- code/src/Core/Naming/Naming.cs
+++ code/src/Core/Naming/Naming.cs
@@ -10,8 +10,6 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
-using Microsoft.TemplateEngine.Abstractions;
-using Microsoft.Templates.Core.Gen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
