3d7cf839b70c682a693ba0146ba2693008245caf
Martin Vega <mvegaca@outlook.com>
2017-06-20 11:37:03 +0200

    Remove alow click condition

--- code/src/UI/ViewModels/MainViewModel.cs
+++ code/src/UI/ViewModels/MainViewModel.cs
@@ -284,7 +284,6 @@ namespace Microsoft.Templates.UI.ViewModels
             {
                 ItemsSource = items,
                 Style = MainView.FindResource("SummaryListViewStyle") as Style,
-                Tag = "AllowClick",
                 ItemTemplate = MainView.FindResource("ProjectTemplatesSummaryItemTemplate") as DataTemplate
             };
             if (allowDragAndDrop)
