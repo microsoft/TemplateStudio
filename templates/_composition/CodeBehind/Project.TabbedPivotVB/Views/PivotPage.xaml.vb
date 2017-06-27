9bcaf90db760a24d5db95cf55304bab4a88ca1bd
Matt Lacey <matt@mrlacey.co.uk>
2017-06-27 15:28:55 +0100

    Added generation of blank Pivot&Tabs with CodeBehind

---
+++ templates/_composition/CodeBehind/Project.TabbedPivotVB/Views/PivotPage.xaml.vb
@@ -0,0 +1,14 @@
+Imports Windows.UI.Xaml.Controls
+Imports Windows.UI.Xaml.Navigation
+
+Namespace wts.ItemName.Views
+    Public NotInheritable Partial Class PivotPage
+        Inherits Page
+        Public Sub New()
+            ' We use NavigationCacheMode.Required to keep track the selected item on navigation. For further information see the following links.
+            ' https://msdn.microsoft.com/en-us/library/windows/apps/xaml/windows.ui.xaml.controls.page.navigationcachemode.aspx
+            ' https://msdn.microsoft.com/en-us/library/windows/apps/xaml/Hh771188.aspx
+            NavigationCacheMode = NavigationCacheMode.Required
+        End Sub
+    End Class
+End Namespace
