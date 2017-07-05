2c675245688b982aa48d36aa95a2ae55f4d0d192
Raúl Alarcón <ralarcon@microsoft.com>
2017-07-05 09:37:31 +0200

    Add shortcuts

--- code/src/Installer.2017/Commands/RelayCommandPackage.cs
+++ code/src/Installer.2017/Commands/RelayCommandPackage.cs
@@ -28,23 +28,6 @@ using Microsoft.Templates.UI.VisualStudio;
 
 namespace Microsoft.Templates.Extension.Commands
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [ProvideService((typeof(ISGenContextBootstrapService)), IsAsyncQueryable = true)]
     [ProvideAutoLoad("{f1536ef8-92ec-443c-9ed7-fdadf150da82}", PackageAutoLoadFlags.BackgroundLoad)]
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
