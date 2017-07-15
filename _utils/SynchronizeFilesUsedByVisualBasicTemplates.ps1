c17ebe3f2aa83c655793f3aa86c3078e520ba0ee
Matt Lacey <matt@mrlacey.co.uk>
2017-07-15 22:38:53 +0100

    Add localized files for blank page desccription

--- _utils/SynchronizeFilesUsedByVisualBasicTemplates.ps1
+++ _utils/SynchronizeFilesUsedByVisualBasicTemplates.ps1
@@ -2,4 +2,4 @@
 # XAML files should be the same for both C# & VB so this means changes can be made for C# and then easily copied for the VB templates.
 # This is necessary because the way template folders are copied and filtered means that, for example, the VB templates can't point to files in the C# folders.
 
-Get-ChildItem ..\templates\* -Recurse -include *.xaml, *.resw, *.md, *.png | where { $_.FullName -Match "VB\\" } | % { $cs = $_.FullName -replace "VB\\", "\\"; Copy-Item $cs $_.FullName }
+Get-ChildItem ..\templates\* -Recurse -include *.xaml, *.resw, *.md, *.png, *.description.json | where { $_.FullName -Match "VB\\" } | % { $cs = $_.FullName -replace "VB\\", "\\"; Copy-Item $cs $_.FullName }
