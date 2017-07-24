d50a36ef9615e97b9be413cf58a96ccb572b2dca
Lucas Tai-MacArthur <t-lutaim@microsoft.com>
2017-07-24 12:50:24 -0700

    Master/Detail CB template uses connected animation
    Master items now contain each order's unique icon
    Detail page now has a distinct header showing name and icon
    
    On selection in wide mode, these attributes from the master list fly
    across to form the basis of the detail page's header while the
    supporting information changes.

--- templates/Pages/MasterDetail.CodeBehind/Views/MasterDetailDetailControl.xaml.cs
+++ templates/Pages/MasterDetail.CodeBehind/Views/MasterDetailDetailControl.xaml.cs
@@ -1,6 +1,8 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 using Param_ItemNamespace.Models;
+
 
 namespace Param_ItemNamespace.Views
 {
@@ -9,10 +11,21 @@ namespace Param_ItemNamespace.Views
         public Order MasterMenuItem
         {
             get { return GetValue(MasterMenuItemProperty) as Order; }
-            set { SetValue(MasterMenuItemProperty, value); }
+            set
+            {
+                SetValue(MasterMenuItemProperty, value);
+                ConnectedAnimationService.GetForCurrentView().DefaultDuration = System.TimeSpan.FromSeconds(.25);
+                ConnectedAnimation iconAnim = ConnectedAnimationService.GetForCurrentView().GetAnimation("companyIcon");
+                ConnectedAnimation titleAnim = ConnectedAnimationService.GetForCurrentView().GetAnimation("companyTitle");
+                if (!(iconAnim == null) && !(titleAnim == null))
+                {
+                    iconAnim.TryStart(destIcon);
+                    titleAnim.TryStart(destTitle);
+                }
+            }
         }
 
-        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(Order), typeof(MasterDetailDetailControl), new PropertyMetadata(null));
+        public static DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem",typeof(Order),typeof(MasterDetailDetailControl),new PropertyMetadata(null));
 
         public MasterDetailDetailControl()
         {
--- templates/Pages/MasterDetail.CodeBehind/Views/MasterDetailPage.xaml.cs
+++ templates/Pages/MasterDetail.CodeBehind/Views/MasterDetailPage.xaml.cs
@@ -1,6 +1,10 @@
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 using Param_ItemNamespace.Models;
 using Param_ItemNamespace.Services;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Linq;
@@ -43,6 +47,29 @@ namespace Param_ItemNamespace.Views
             var item = e?.ClickedItem as Order;
             if (item != null)
             {
+                if (WindowStates.CurrentState.Name != "NarrowState")
+                {
+                	foreach (var fonticon in GetAllVisualChildrenOfType<FontIcon>(MasterListView))
+                    {
+                        var element = (UIElement)fonticon;
+                        bool foundIcon = (item.HashIdentIcon == (string)fonticon.Tag);
+                        if (foundIcon)
+                        {
+                            ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("companyIcon", element);
+                        }
+                    }
+
+                    foreach (var textblock in GetAllVisualChildrenOfType<TextBlock>(MasterListView))
+                    {
+                        var element = (UIElement)textblock;
+                        bool foundTitle = (item.HashIdentTitle == (string)textblock.Tag);
+                        if (foundTitle)
+                        {
+                            ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("companyTitle", element);
+                        }
+                    }
+                }
+
                 if (WindowStates.CurrentState == NarrowState)
                 {
                     NavigationService.Navigate<Views.MasterDetailDetailPage>(item);
@@ -50,6 +77,27 @@ namespace Param_ItemNamespace.Views
                 else
                 {
                     Selected = item;
+                }
+            }
+        }
+
+        private IEnumerable<T> GetAllVisualChildrenOfType<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    yield return (T)child;
+                else
+                {
+                    for (int j = 0; j < VisualTreeHelper.GetChildrenCount(child); j++)
+                    {
+                        IEnumerable<T> childrenOfChild = GetAllVisualChildrenOfType<T>(child);
+                        foreach (T subChild in childrenOfChild)
+                        {
+                            yield return subChild;
+                        }
+                    }
                 }
             }
         }
