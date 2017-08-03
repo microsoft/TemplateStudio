026aa67e87490ca698c512252a63f2aecef3fd36
Guillaume DEMICHELI <30379@supinfo.com>
2017-08-04 01:25:42 +0200

    Add _shared postaction

---
+++ templates/_composition/_shared/Page.Camera.AddInitialization_TabbedPivot/Views/wts.ItemNamePage_postaction.xaml.cs
@@ -0,0 +1,29 @@
+﻿//{[{
+using Windows.UI.Xaml;
+//}]}
+namespace Param_ItemNamespace.Views
+{
+    public sealed partial class wts.ItemNamePage : Page
+    {
+        public wts.ItemNamePage()
+        {
+            InitializeComponent();
+            //{[{
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+            //}]}
+        }
+
+        //{[{
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await Camera.InitializeAsync();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            Camera.Cleanup();
+        }
+        //}]}
+    }
+}
