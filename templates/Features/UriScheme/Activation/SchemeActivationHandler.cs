ce457e5a78dd83ee13672280131f583dac3f95e8
Matt Lacey <matt@mrlacey.co.uk>
2017-08-16 13:53:11 +0100

    Successfully creates CodeBehind version

--- templates/Features/UriScheme/Activation/SchemeActivationHandler.cs
+++ templates/Features/UriScheme/Activation/SchemeActivationHandler.cs
@@ -10,25 +10,36 @@ namespace Param_ItemNamespace.Activation
     // TODO WTS: Change the image in Assets/Logo.png to one for display if the OS asks the user which app to launch.
     internal class SchemeActivationHandler : ActivationHandler<ProtocolActivatedEventArgs>
     {
-        // This handler expects URIs of the format 'wtsapp:sample?secret={value}'
+        // By default, this handler expects URIs of the format 'wtsapp:sample?secret={value}'
         protected override async Task HandleInternalAsync(ProtocolActivatedEventArgs args)
         {
-            // The following will extract the secret value and pass it to the page. Alternatively, you could pass all or some of the Uri.
-            var decoder = new Windows.Foundation.WwwFormUrlDecoder(args.Uri.Query);
-
-            var secret = "<<I-HAVE-NO-SECRETS>>";
-
-            try 
+            if (args.Uri.AbsolutePath.ToLowerInvariant().Equals("sample"))
             {
-                decoder.GetFirstValueByName("secret");
+                var secret = "<<I-HAVE-NO-SECRETS>>";
+
+                try
+                {
+                    if (args.Uri.Query != null)
+                    {
+                        // The following will extract the secret value and pass it to the page. Alternatively, you could pass all or some of the Uri.
+                        var decoder = new Windows.Foundation.WwwFormUrlDecoder(args.Uri.Query);
+
+                        decoder.GetFirstValueByName("secret");
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    // This will happen if the URI doens't contain a param called 'secret'
+                }
+
+                // It's also possible to have logic here to navigate to different pages. e.g. if you have logic based on the URI used to launch
+                NavigationService.Navigate(typeof(Views.URISchemePage), secret);
             }
-            catch (ArgumentException)
+            else
             {
-                // This will happen if the URI Query doesn't contain a param called 'secret'
+                // If not navigating to a specific page based on the URI, navigate to the home page
+                NavigationService.Navigate((typeof(Views.Param_HomeNamePage)));
             }
- 
-            // It's also possible to have logic here to navigate to different pages. e.g. if you have logic based on the specific URI used to launch
-            NavigationService.Navigate(typeof(Views.UriSchemeExamplePage), secret);
 
             await Task.CompletedTask;
         }
