1334dfde87661f02a619934e2fba9131e0a81363
Martin Vega <mvegaca@outlook.com>
2017-08-24 18:50:41 +0200

    Create InformationButton control

---
+++ code/src/UI/Controls/InformationButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Microsoft.Templates.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for InformationButton.xaml
+    /// </summary>
+    public partial class InformationButton : UserControl
+    {
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(InformationButton), new PropertyMetadata(null));
+
+        public InformationButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
