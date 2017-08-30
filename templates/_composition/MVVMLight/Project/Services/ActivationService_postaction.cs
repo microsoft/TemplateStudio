53319a94748e4938a62bc59cc4752339f465618c
Raúl Alarcón <ralarcon@microsoft.com>
2017-08-30 15:45:01 +0200

    Remove white spaces

--- templates/_composition/MVVMLight/Project/Services/ActivationService_postaction.cs
+++ templates/_composition/MVVMLight/Project/Services/ActivationService_postaction.cs
@@ -6,7 +6,7 @@
 //{[{
 
         private ViewModels.ViewModelLocator Locator => Application.Current.Resources["Locator"] as ViewModels.ViewModelLocator;
-        
+
         private NavigationServiceEx NavigationService => Locator.NavigationService;
 //}]}
     }
