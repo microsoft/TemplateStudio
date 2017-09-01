25b2a23ad5fa6cb8bac17b820c70fec85875a2fd
Raúl Alarcón <ralarcon@microsoft.com>
2017-09-01 11:58:06 +0200

    Comment already in the ViewModel

--- templates/Pages/Settings/Views/SettingsPagePage.xaml.cs
+++ templates/Pages/Settings/Views/SettingsPagePage.xaml.cs
@@ -5,7 +5,6 @@ namespace Param_ItemNamespace.Views
 {
     public sealed partial class SettingsPagePage : Page
     {
-        //// TODO WTS: Add other settings as necessary. For help see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/pages/settings.md
         //// TODO WTS: Change the URL for your privacy policy in the Resource File, currently set to https://YourPrivacyUrlGoesHere
 
         public SettingsPagePage()
