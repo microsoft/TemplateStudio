94e0afbfbdc0361e8fbb0ea72f8e86b6a4d77d9c
Nigel Sampson <nigel.sampson@compiledexperience.com>
2017-09-03 23:09:50 +1200

    Tabbed Pivot Page

---
+++ templates/Pages/TabbedPivot.CaliburnMicro/ViewModels/TabbedPivotPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Caliburn.Micro;
+
+namespace Param_ItemNamespace.ViewModels
+{
+    public class TabbedPivotPageViewModel : Conductor<Screen>.Collection.OneActive
+    {
+        public TabbedPivotPageViewModel()
+        {
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            // WTS Add view models to the Items collection to display them in the Tabs
+        }
+    }
+}
