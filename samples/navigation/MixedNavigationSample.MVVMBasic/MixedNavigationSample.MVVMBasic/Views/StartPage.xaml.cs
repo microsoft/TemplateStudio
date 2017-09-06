623b007b03b2fa72972e09eb57af7f3ec44c0c44
Martin Vega <mvegaca@outlook.com>
2017-09-06 15:54:22 +0200

    Add MixedNavigationSample.MVVMBasic

---
+++ samples/navigation/MixedNavigationSample.MVVMBasic/MixedNavigationSample.MVVMBasic/Views/StartPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using MixedNavigationSample.MVVMBasic.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace MixedNavigationSample.MVVMBasic.Views
+{
+    public sealed partial class StartPage : Page
+    {
+        public StartViewModel ViewModel { get; } = new StartViewModel();
+
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
