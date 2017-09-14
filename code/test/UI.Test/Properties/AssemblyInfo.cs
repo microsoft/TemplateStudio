471ac2839fd86f6210595d147cccb630cd2fe57d
Martin Vega <mvegaca@outlook.com>
2017-09-14 16:49:56 +0200

    Add Test for UI project

---
+++ code/test/UI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef7baa17-a68c-49aa-aa3a-1fe38bea0306")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ code/test/UI.Test/TemplatesFixture.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Templates.Core;
+using Microsoft.Templates.Core.Gen;
+using Microsoft.Templates.Core.Locations;
+using Microsoft.Templates.Fakes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Templates.Core.Diagnostics;
+using Microsoft.Templates.Core.PostActions.Catalog.Merge;
+
+namespace UI.Test
+{
+    public class TemplatesFixture : IContextProvider
+    {
+        public TemplatesRepository Repository { get; private set; }
+
+        public string ProjectName => "Test";
+
+        public string OutputPath => throw new NotImplementedException();
+
+        public string ProjectPath => throw new NotImplementedException();
+
+        public List<string> ProjectItems => throw new NotImplementedException();
+
+        public List<string> FilesToOpen => throw new NotImplementedException();
+
+        public List<FailedMergePostAction> FailedMergePostActions => throw new NotImplementedException();
+
+        public Dictionary<string, List<MergeInfo>> MergeFilesFromProject => throw new NotImplementedException();
+
+        public Dictionary<ProjectMetricsEnum, double> ProjectMetrics => throw new NotImplementedException();
+
+        public void InitializeFixture(string language)
+        {
+            var source = new LocalTemplatesSource();
+
+            GenContext.Bootstrap(source, new FakeGenShell(language), language);
+
+            GenContext.ToolBox.Repo.SynchronizeAsync().Wait();
+
+            Repository = GenContext.ToolBox.Repo;
+            GenContext.Current = this;
+        }
+    }
+}
