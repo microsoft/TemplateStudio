85cab60c6f2897248d790f1ad3449557c0d2c1b3
Nukepayload2 <1939357182@qq.com>
2017-09-18 16:55:18 +0800

    Translated "First use prompt".

---
+++ templates/Features/FirstUsePromptVB/Services/FirstRunDisplayService.vb
@@ -0,0 +1,19 @@
+﻿Imports Param_ItemNamespace.Views
+Imports Param_ItemNamespace.Helpers
+Imports System.Threading.Tasks
+Imports Windows.ApplicationModel
+
+Namespace Services
+    Public Class FirstRunDisplayService
+        Friend Shared Function ShowIfAppropriateAsync() As Task
+            Dim hasShownFirstRun As Boolean = False
+            hasShownFirstRun = Await Windows.Storage.ApplicationData.Current.LocalSettings.ReadAsync(Of Boolean)(NameOf(hasShownFirstRun))
+
+			If Not hasShownFirstRun Then
+                Await Windows.Storage.ApplicationData.Current.LocalSettings.SaveAsync(NameOf(hasShownFirstRun), True)
+                Dim dialog = New FirstRunDialog()
+                Await dialog.ShowAsync()
+            End If
+        End Function
+    End Class
+End Namespace
