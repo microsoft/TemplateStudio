b5fbb80e7d6427daed6ba12b0ea4486d1a688542
Nukepayload2 <1939357182@qq.com>
2017-09-18 17:20:33 +0800

    Translated "StoreNotifications"

---
+++ templates/Features/StoreNotificationsVB/Services/ActivationService_postaction.vb
@@ -0,0 +1,25 @@
+﻿'{**
+' These code blocks include the StoreNotificationsFeatureService Instance in the method `GetActivationHandlers()`
+' and initializes it in the method `InitializeAsync()` in the ActivationService of your project.
+'**}
+'{[{
+Imports Param_RootNamespace.Helpers
+'}]}
+Namespace Services
+    Friend Class ActivationService
+        Private Function InitializeAsync() As Task
+            '{[{
+            Await Singleton(Of StoreNotificationsFeatureService).Instance.InitializeAsync()
+            '}]}
+        End Function
+
+        Private Iterator Function GetActivationHandlers() As IEnumerable(Of ActivationHandler)
+            '{[{
+            Yield Singleton(Of StoreNotificationsFeatureService).Instance
+            '}]}
+            '{--{
+            Return
+            '}--}
+        End Function
+    End Class
+End Namespace
