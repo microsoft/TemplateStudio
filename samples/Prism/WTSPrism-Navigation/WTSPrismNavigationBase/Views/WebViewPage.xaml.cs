670cb6112ae292442f728323fed2f249b303dc7b
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2017-09-21 13:50:45 -0500

    Add comment to why the WebViewService is initialized in the UI and not like a typical service

--- samples/Prism/WTSPrism-Navigation/WTSPrismNavigationBase/Views/WebViewPage.xaml.cs
+++ samples/Prism/WTSPrism-Navigation/WTSPrismNavigationBase/Views/WebViewPage.xaml.cs
@@ -14,6 +14,12 @@ namespace WTSPrismNavigationBase.Views
         public WebViewPage()
         {
             InitializeComponent();
+
+            // This is an unusual way to initialize a service to a ViewModel, but since this service
+            // requires a reference to the WebView this is one way to provide the required reference.
+            // In this case the WebViewService isn't a traditional Service but more of a shim to provide to better
+            // separation of View and ViewModel and unit testing of a ViewModel that uses the WebViewService since the
+            // WebViewService implements the IWebViewService interface that allows for mocking of the service.
             ViewModel.WebViewService = new WebViewService(this.webView);
         }
     }
--- samples/Prism/WTSPrism-Pivot/Views/WebViewPage.xaml.cs
+++ samples/Prism/WTSPrism-Pivot/Views/WebViewPage.xaml.cs
@@ -6,11 +6,20 @@ namespace WTSPrism.Views
 {
     public sealed partial class WebViewPage : Page
     {
-        private WebViewPageViewModel ViewModel => DataContext as WebViewPageViewModel;
+        private WebViewPageViewModel ViewModel
+        {
+            get { return DataContext as WebViewPageViewModel; }
+        }
 
         public WebViewPage()
         {
             InitializeComponent();
+
+            // This is an unusual way to initialize a service to a ViewModel, but since this service
+            // requires a reference to the WebView this is one way to provide the required reference.
+            // In this case the WebViewService isn't a traditional Service but more of a shim to provide to better
+            // separation of View and ViewModel and unit testing of a ViewModel that uses the WebViewService since the
+            // WebViewService implements the IWebViewService interface that allows for mocking of the service.
             ViewModel.WebViewService = new WebViewService(this.webView);
         }
     }
