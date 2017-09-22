fbc98a7626031a7beb0237a2345383b8abe2de82
Matt Lacey <matt@mrlacey.co.uk>
2017-09-22 15:35:15 +0100

    Add Feedback Hub link to CodeBehind projects
    For #196

---
+++ templates/Features/FeedbackHub.CodeBehind/Views/$SEARCH2$.xaml.cs
@@ -0,0 +1,30 @@
+﻿//{**
+// This code block adds the code to launch the Feedback Hub from the settings page
+//**}
+namespace Param_ItemNamespace.Views
+{
+    public sealed partial class SettingsPagePage : Page, INotifyPropertyChanged
+    {
+        private void Initialize()
+        {
+            //^^
+            //{[{
+
+            if (Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported())
+            {
+                FeedbackLink.Visibility = Visibility.Visible;
+            }
+            //}]}
+        }
+
+        //^^
+        //{[{
+        private async void FeedbackLink_Click(object sender, RoutedEventArgs e)
+        {
+            // This launcher is part of the Store Services SDK https://docs.microsoft.com/en-us/windows/uwp/monetize/microsoft-store-services-sdk
+            var launcher = Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault();
+            await launcher.LaunchAsync();
+        }
+        //}]}
+    }
+}
