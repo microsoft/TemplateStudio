a39c3a71bd35d5619287893666147903a8c82c55
Matt Lacey <matt@mrlacey.co.uk>
2017-09-22 18:34:02 +0100

    Add Feedback Hub link to Caliburn Micro projects
    For #196

---
+++ templates/Features/FeedbackHub.CaliburnMicro/ViewModels/$SEARCH2$.cs
@@ -0,0 +1,20 @@
+﻿//{**
+// This code block adds the code to launch the Feedback Hub from the settings page
+//**}
+namespace Param_ItemNamespace.ViewModels
+{
+    public class SettingsPageViewModel : Observable
+    {
+        //{[{
+        public Visibility FeedbackLinkVisibility => Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported() ? Visibility.Visible : Visibility.Collapsed;
+
+        public async void LaunchFeedbackHub()
+        {
+            // This launcher is part of the Store Services SDK https://docs.microsoft.com/en-us/windows/uwp/monetize/microsoft-store-services-sdk
+            var launcher = Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault();
+            await launcher.LaunchAsync();
+        }
+
+        //}]}
+    }
+}
