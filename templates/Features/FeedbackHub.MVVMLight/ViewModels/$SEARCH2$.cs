fe4b34f217a2720521130fb7392fdf7795097ea1
Matt Lacey <matt@mrlacey.co.uk>
2017-09-22 16:55:45 +0100

    Add Feedback Hub link to MVVM Light projects
    For #196

---
+++ templates/Features/FeedbackHub.MVVMLight/ViewModels/$SEARCH2$.cs
@@ -0,0 +1,33 @@
+﻿//{**
+// This code block adds the code to launch the Feedback Hub from the settings page
+//**}
+namespace Param_ItemNamespace.ViewModels
+{
+    public class SettingsPageViewModel : ViewModelBase
+    {
+        //{[{
+        public Visibility FeedbackLinkVisibility => Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported() ? Visibility.Visible : Visibility.Collapsed;
+
+        private ICommand _launchFeedbackHubCommand;
+
+        public ICommand LaunchFeedbackHubCommand
+        {
+            get
+            {
+                if (_launchFeedbackHubCommand == null)
+                {
+                    _launchFeedbackHubCommand = new RelayCommand(
+                        async () =>
+                        {
+                            // This launcher is part of the Store Services SDK https://docs.microsoft.com/en-us/windows/uwp/monetize/microsoft-store-services-sdk
+                            var launcher = Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault();
+                            await launcher.LaunchAsync();
+                        });
+                }
+
+                return _launchFeedbackHubCommand;
+            }
+        }
+        //}]}
+    }
+}
