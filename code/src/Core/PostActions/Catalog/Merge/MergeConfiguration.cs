cd2388a7a2133219d12c3a2f8e370be325d437f4
sibille <sibille@ilitia.com>
2017-09-29 13:16:37 +0200

    init mergeresourcedictionary postaction

--- code/src/Core/PostActions/Catalog/Merge/MergeConfiguration.cs
+++ code/src/Core/PostActions/Catalog/Merge/MergeConfiguration.cs
@@ -12,6 +12,19 @@ namespace Microsoft.Templates.Core.PostActions.Catalog.Merge
 {
     public class MergeConfiguration
     {
+        public const string Suffix = "postaction";
+        public const string NewSuffix = "failedpostaction";
+
+        public const string PostactionRegex = @"(\$\S*)?(_" + Suffix + "|_g" + Suffix + @")\.";
+        public const string FailedPostactionRegex = @"(\$\S*)?(_" + NewSuffix + "|_g" + NewSuffix + @")(\d)?\.";
+
+        public const string Extension = "_" + Suffix + ".";
+        public const string GlobalExtension = "$*_g" + Suffix + ".";
+
+        public const string ResourceDictionaryMatch = @"<ResourceDictionary
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">";
+
         public string FilePath { get; private set; }
 
         public bool FailOnError { get; private set; }
---
+++ code/src/Core/PostActions/Catalog/Merge/MergeResourceDictionaryPostAction.cs
@@ -0,0 +1,131 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Templates.Core.Gen;
+using Microsoft.Templates.Core.Resources;
+
+namespace Microsoft.Templates.Core.PostActions.Catalog.Merge
+{
+    public class MergeResourceDictionaryPostAction : MergePostAction
+    {
+        public MergeResourceDictionaryPostAction(MergeConfiguration config) : base(config)
+        {
+        }
+
+        public override void Execute()
+        {
+            string originalFilePath = GetFilePath();
+            if (!File.Exists(originalFilePath))
+            {
+                File.Copy(_config.FilePath, originalFilePath);
+                GenContext.Current.ProjectItems.Add(originalFilePath);
+
+                // TODO: Create postaction file for App.xaml
+            }
+            else
+            {
+                var mergeRoot = XElement.Load(_config.FilePath);
+                var sourceRoot = XElement.Load(originalFilePath);
+
+                foreach (var node in GetNodesToMerge(mergeRoot))
+                {
+                    var sourceNode = sourceRoot.Elements().FirstOrDefault(e => GetKey(e) == GetKey(node));
+                    if (sourceNode == null)
+                    {
+                        AddNodeToSource(sourceRoot, node);
+                    }
+                    else
+                    {
+                        if (!CompareNodes(node, sourceNode))
+                        {
+                            var errorMessage = string.Format(StringRes.FailedMergePostActionKeyAlreadyDefined, GetKey(node), GetRelativePath(originalFilePath));
+                            if (_config.FailOnError)
+                            {
+                                // TODO: Throw exception
+                                throw new InvalidDataException(errorMessage);
+                            }
+                            else
+                            {
+                                AddFailedMergePostActions(originalFilePath, MergeFailureType.KeyAlreadyDefined, errorMessage);
+                                File.Delete(_config.FilePath);
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.OmitXmlDeclaration = true;
+                settings.Indent = true;
+                settings.IndentChars = "    ";
+                using (XmlWriter writer = XmlTextWriter.Create(originalFilePath, settings))
+                {
+                    sourceRoot.Save(writer);
+                }
+            }
+
+            File.Delete(_config.FilePath);
+        }
+
+        private static void AddNodeToSource(XElement sourceRoot, XElement node)
+        {
+            if (node.PreviousNode != null && node.PreviousNode.NodeType == XmlNodeType.Comment)
+            {
+                sourceRoot.Add(node.PreviousNode);
+            }
+
+            sourceRoot.Add(node);
+        }
+
+        private string GetKey(XElement node)
+        {
+            XNamespace ns = node.GetNamespaceOfPrefix("x");
+            return node.Attribute(ns + "Key").Value;
+        }
+
+        private IEnumerable<XElement> GetNodesToMerge(XElement rootNode)
+        {
+            return rootNode.Descendants().Where(e => e.Attributes().Any(a => a.Name.LocalName == "Key"));
+        }
+
+        private bool CompareNodes(XElement mergeNode, XElement sourceNode)
+        {
+            if (mergeNode.Value != sourceNode.Value)
+            {
+                return false;
+            }
+            else
+            {
+                if (mergeNode.Elements().Count() != sourceNode.Elements().Count())
+                {
+                    return false;
+                }
+                else
+                {
+                    foreach (var mergeChildNode in mergeNode.Elements())
+                    {
+                        var sourceChildNode = sourceNode.Elements().FirstOrDefault(e => e.Attribute("Property").Value == mergeChildNode.Attribute("Property").Value);
+
+                        if (sourceChildNode == null || sourceChildNode.Attribute("Value").Value != mergeChildNode.Attribute("Value").Value)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private string GetFilePath()
+        {
+            return Regex.Replace(_config.FilePath, MergeConfiguration.PostactionRegex, ".");
+        }
+    }
+}
