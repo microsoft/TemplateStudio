c68e109eb6557bf2df6eb62138bd21e364f0d3e7
Nukepayload2 <1939357182@qq.com>
2017-09-30 13:34:26 +0800

    Ported UriScheme feature. Verified Blank and CodeBehind.

---
+++ templates/_composition/MVVMLight/Feature.UriSchemeExamplePageVB/ViewModels/wts.ItemNameExampleViewModel.vb
@@ -0,0 +1,19 @@
+﻿Imports GalaSoft.MvvmLight
+
+Namespace ViewModels
+    ' TODO WTS: This class exists purely as part of the example of how to launch a specific page in response to a protocol launch and pass it a value. It is expected that you will delete this class once you have changed the handling of a protocol launch to meet your needs and redirected to another of your pages.
+    Public Class wts.ItemNameExampleViewModel
+        Inherits ViewModelBase
+        ' This property is just for displaying the passed in value
+        Private _secret As String
+
+        Public Property Secret As String
+            Get
+                Return _secret
+            End Get
+            Set
+                [Set](_secret, Value)
+            End Set
+        End Property
+    End Class
+End Namespace
