51d885fdd9a7bcd0571f41781bb4eb2682806db4
ralarcon <ralarcon@microsoft.com>
2017-10-03 15:36:56 +0200

    Set NETCore.UniversalPlatformWindows to 5.4

--- code/test/Templates.Test/RestoreAndBuild.bat
+++ code/test/Templates.Test/RestoreAndBuild.bat
@@ -7,9 +7,6 @@ ELSE (
 )
 
 :DEV15
-	IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\Preview\Community" (
-		call "%ProgramFiles(x86)%\Microsoft Visual Studio\Preview\Community\Common7\Tools\VsMSBuildCmd.bat"
-	)
 	IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community" (
 		call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
 	)
