ce8e6e9d9fc61f5470b368531b8ddfacba702883
Martin Vega <mvegaca@outlook.com>
2017-10-05 12:37:20 +0200

    Add sample out of the box

---
+++ samples/notifications/ToastNotificationSample/ToastNotificationSample/Services/ToastNotificationsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using ToastNotificationSample.Activation;
+
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Notifications;
+
+namespace ToastNotificationSample.Services
+{
+    internal partial class ToastNotificationsService : ActivationHandler<ToastNotificationActivatedEventArgs>
+    {
+        public void ShowToastNotification(ToastNotification toastNotification)
+        {
+            ToastNotificationManager.CreateToastNotifier().Show(toastNotification);
+        }
+
+        protected override async Task HandleInternalAsync(ToastNotificationActivatedEventArgs args)
+        {
+            //// TODO WTS: Handle activation from toast notification,
+            //// for more info on handling activation see
+            //// Documentation: https://blogs.msdn.microsoft.com/tiles_and_toasts/2015/07/08/quickstart-sending-a-local-toast-notification-and-handling-activations-from-it-windows-10/
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ samples/notifications/ToastNotificationSample/ToastNotificationSample/Views/ActivatedFromToastPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using ToastNotificationSample.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace ToastNotificationSample.Views
+{
+    public sealed partial class ActivatedFromToastPage : Page
+    {
+        public ActivatedFromToastViewModel ViewModel { get; } = new ActivatedFromToastViewModel();
+
+        public ActivatedFromToastPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
