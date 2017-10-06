4266ca0af2da154d0b6a346ced97006a094e1ae9
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2017-10-06 10:22:09 -0500

    Remap default Page->ViewModel naming to match generated filename

--- templates/_composition/Prism/Project.Blank.AddNavigation/App_postaction.xaml.cs
+++ templates/_composition/Prism/Project.Blank.AddNavigation/App_postaction.xaml.cs
@@ -1,15 +1,34 @@
-﻿namespace Param_RootNamespace
+﻿//{[{
+using Prism.Mvvm;
+using System.Globalization;
+//}]}namespace Param_RootNamespace
 {
     public sealed partial class App : PrismUnityApplication
     {
 //^^
 //{[{
-    
         protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
         {
             NavigationService.Navigate(PageTokens.Param_HomeNamePage, null);
             return Task.FromResult(true);
         }
 //}]}
+
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            Container.RegisterInstance<IResourceLoader>(new ResourceLoaderAdapter(new ResourceLoader()));
+//{[{
+            // We are remapping the default ViewNamePage->ViewNamePageViewModel naming to ViewNamePage->ViewNameViewModel to 
+            // gain better code reuse with other frameworks and pages within Windows Template Studio
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "Param_RootNamespace.ViewModels.{0}ViewModel, Param_RootNamespace", viewType.Name.Replace("Page",String.Empty));
+                var viewModelType = Type.GetType(viewModelTypeName);
+                return viewModelType;
+            });
+//}]}
+            return base.OnInitializeAsync(args);
+        }
+
     }
 }
