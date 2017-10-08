3992021590089c6773f07841cef7274084df070b
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2017-10-08 15:06:38 -0500

    Adding Master/Detail pages for Prism

---
+++ templates/Pages/MasterDetail.Prism/ViewModels/MasterDetailDetailViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Prism.Commands;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Param_ItemNamespace.Models;
+using Param_ItemNamespace.Services;
+
+namespace Param_ItemNamespace.ViewModels
+{
+    public class MasterDetailDetailViewModel : ViewModelBase
+    {
+        const string NarrowStateName = "NarrowState";
+        const string WideStateName = "WideState";
+
+        private readonly INavigationService navigationService;
+        private readonly ISampleDataService sampleDataService;
+
+        public ICommand StateChangedCommand { get; }
+
+        private SampleOrder item;
+        public SampleOrder Item
+        {
+            get { return item; }
+            set { SetProperty(ref item, value); }
+        }
+
+        public MasterDetailDetailViewModel(INavigationService navigationService, ISampleDataService sampleDataService)
+        {
+            this.navigationService = navigationService;
+            this.sampleDataService = sampleDataService;
+            StateChangedCommand = new DelegateCommand<VisualStateChangedEventArgs>(OnStateChanged);
+        }
+
+        public async override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            long orderId = (long)e.Parameter;
+            var data = await sampleDataService.GetSampleModelDataAsync();
+            Item = data.FirstOrDefault(x => x.OrderId == orderId);
+        }
+
+        private void OnStateChanged(VisualStateChangedEventArgs args)
+        {
+            if (args.OldState.Name == NarrowStateName && args.NewState.Name == WideStateName)
+            {
+                navigationService.GoBack();
+            }
+        }
+    }
+}
---
+++ templates/Pages/MasterDetail.Prism/ViewModels/MasterDetailViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Prism.Commands;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Param_ItemNamespace.Models;
+using Param_ItemNamespace.Services;
+
+namespace Param_ItemNamespace.ViewModels
+{
+    public class MasterDetailViewModel : ViewModelBase
+    {
+        private readonly INavigationService navigationService;
+        private readonly ISampleDataService sampleDataService;
+
+        const string NarrowStateName = "NarrowState";
+        const string WideStateName = "WideState";
+
+        private VisualState currentState;
+
+        public MasterDetailViewModel(INavigationService navigationService, ISampleDataService sampleDataService)
+        {
+            this.navigationService = navigationService;
+            this.sampleDataService = sampleDataService;
+            ItemClickCommand = new DelegateCommand<ItemClickEventArgs>(OnItemClick);
+            StateChangedCommand = new DelegateCommand<VisualStateChangedEventArgs>(OnStateChanged);
+            LoadedCommand = new DelegateCommand<VisualState>(OnLoaded);
+        }
+
+        private SampleOrder selected;
+        public SampleOrder Selected
+        {
+            get => selected;
+            set => SetProperty(ref selected, value);
+        }
+
+        public ICommand ItemClickCommand { get; }
+        public ICommand StateChangedCommand { get; }
+        public ICommand LoadedCommand { get; }
+
+        public ObservableCollection<SampleOrder> SampleItems { get; } = new ObservableCollection<SampleOrder>();
+
+        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            await LoadDataAsync(currentState);
+        }
+
+        public async Task LoadDataAsync(VisualState currentState)
+        {
+            this.currentState = currentState;
+            SampleItems.Clear();
+
+            var data = await sampleDataService.GetSampleModelDataAsync();
+
+            foreach (var item in data)
+            {
+                SampleItems.Add(item);
+            }
+            Selected = SampleItems.First();
+        }
+
+        private void OnLoaded(VisualState state)
+        {
+            currentState = state;
+        }
+
+        private void OnStateChanged(VisualStateChangedEventArgs args)
+        {
+            currentState = args.NewState;
+        }
+
+        private void OnItemClick(ItemClickEventArgs args)
+        {
+            if (args?.ClickedItem is SampleOrder item)
+            {
+                if (currentState?.Name == NarrowStateName)
+                {
+                    navigationService.Navigate(PageTokens.MasterDetailDetailPage, item.OrderId);
+                }
+                else
+                {
+                    Selected = item;
+                }
+            }
+        }
+    }
+}
