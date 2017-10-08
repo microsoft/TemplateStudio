96ab8e5900205e4f5f1e4165f87c90bee35e92af
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2017-10-08 11:27:39 -0500

    Add Grid page for Prism

---
+++ templates/Pages/Grid.Prism/ViewModels/GridViewViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Windows.Mvvm;
+using Param_ItemNamespace.Models;
+using Param_ItemNamespace.Services;
+
+namespace Param_ItemNamespace.ViewModels
+{
+    public class GridViewViewModel : ViewModelBase
+    {
+        private readonly ISampleDataService sampleDataService;
+
+        public GridViewViewModel(ISampleDataService sampleDataService)
+        {
+            this.sampleDataService = sampleDataService;
+        }
+
+        public ObservableCollection<SampleOrder> Source => sampleDataService.GetGridSampleData();
+    }
+}
