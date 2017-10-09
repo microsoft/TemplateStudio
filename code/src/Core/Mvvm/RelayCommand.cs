e875ccd20e1d8086b1f71e745221e14bef701523
David Gómez Cano <davidg@ILITIA.COM>
2017-10-09 10:00:58 +0200

    Supress SA1402 rule in files with generic and non-generic classes.

--- code/src/Core/Mvvm/RelayCommand.cs
+++ code/src/Core/Mvvm/RelayCommand.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Input;
 
 namespace Microsoft.Templates.Core.Mvvm
@@ -33,6 +34,44 @@ namespace Microsoft.Templates.Core.Mvvm
         public void Execute(object parameter)
         {
             _execute();
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    [SuppressMessage(
+        "StyleCop.CSharp.MaintainabilityRules",
+        "SA1402:File may only contain a single class",
+        Justification = "For simplicity we're allowing generic and non-generic versions in one file.")]
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
         }
 
         public void OnCanExecuteChanged()
--- code/src/Core/PostActions/PostAction.cs
+++ code/src/Core/PostActions/PostAction.cs
@@ -2,10 +2,27 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.Templates.Core.PostActions
 {
     public abstract class PostAction
     {
         public abstract void Execute();
+    }
+
+    [SuppressMessage(
+       "StyleCop.CSharp.MaintainabilityRules",
+       "SA1402:File may only contain a single class",
+       Justification = "For simplicity we're allowing generic and non-generic versions in one file.")]
+
+    public abstract class PostAction<TConfig> : PostAction
+    {
+        protected readonly TConfig _config;
+
+        public PostAction(TConfig config)
+        {
+            _config = config;
+        }
     }
 }
