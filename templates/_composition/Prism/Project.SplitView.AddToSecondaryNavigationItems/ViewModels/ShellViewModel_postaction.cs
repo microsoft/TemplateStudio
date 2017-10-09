da0579488a646f3d7d7ddb4416e17aee5aa51afd
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2017-10-09 18:05:03 -0500

    Navigation Pane project support for Prism!

---
+++ templates/_composition/Prism/Project.SplitView.AddToSecondaryNavigationItems/ViewModels/ShellViewModel_postaction.cs
@@ -0,0 +1,19 @@
+﻿//{**
+//This code block adds the wts.ItemNameViewModel to the _secondaryItems of the ShellViewModel.
+//**}
+//{[{
+using Param_ItemNamespace.Helpers;
+//}]}
+namespace Param_ItemNamespace.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        private void PopulateNavItems()
+        {
+            //^^
+            //{[{
+            SecondaryItems.Add(new ShellNavigationItem("Shell_wts.ItemName".GetLocalized(), Symbol.Document, "wts.ItemName"));
+            //}]}
+        }
+    }
+}
