8ad063897515e8c9631afc568c45f6ac350285e8
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2017-10-11 22:57:53 -0500

    Background Tasks for Prism

---
+++ templates/_composition/Prism/Feature.BackgroundTask/App_postaction.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+//^^
+//{[{
+using Param_RootNamespace.Services;
+//}]}
+
+namespace Param_RootNamespace
+{
+    public sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+//{[{
+            Container.RegisterInstance<IBackgroundTaskService>(new BackgroundTaskService());
+//}]}
+        }
+
+        protected override Task OnActivateApplicationAsync(IActivatedEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+//{[{
+        protected override void OnBackgroundActivated(BackgroundActivatedEventArgs args)
+        {
+            base.OnBackgroundActivated(args);
+            Container.Resolve<IBackgroundTaskService>().Start(args.TaskInstance);
+        }
+//}]}
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+//{[{
+            Container.Resolve<IBackgroundTaskService>().RegisterBackgroundTasks();
+//}]}
+            return base.OnInitializeAsync(args);
+        }
+    }
+}
---
+++ templates/_composition/Prism/Feature.BackgroundTask/Services/IBackgroundTaskService.cs
@@ -0,0 +1,10 @@
+using Windows.ApplicationModel.Background;
+
+namespace Param_ItemNamespace.Services
+{
+    internal interface IBackgroundTaskService
+    {
+        void RegisterBackgroundTasks();
+        void Start(IBackgroundTaskInstance taskInstance);
+    }
+}
