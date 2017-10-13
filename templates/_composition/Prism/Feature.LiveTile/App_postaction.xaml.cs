20e27a9b5f425eefe3aaa3508cd75d13ba24823b
Jeremy Woo-Sam <jeremyws@microsoft.com>
2017-10-12 19:15:33 -0700

    Added LIveTile Feature

---
+++ templates/_composition/Prism/Feature.LiveTile/App_postaction.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+//^^
+//{[{
+using Param_RootNamespace.Services;
+//}]}
+
+namespace Param_RootNamespace
+{
+    public sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+//{[{
+            Container.RegisterInstance<ILiveTileFeatureService>(new LiveTileFeatureService());
+//}]}
+        }
+
+        private async Task LaunchApplication(string page, object launchParam)
+        {
+            NavigationService.Navigate(page, launchParam);            
+            Window.Current.Activate();
+//{[{
+            Container.Resolve<ILiveTileFeatureService>().SampleUpdate();
+//}]}
+            await Task.CompletedTask;
+        }
+
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+//{[{
+            Container.Resolve<ILiveTileService>().EnableQueueAsync();
+//}]}
+            return base.OnInitializeAsync(args);
+        }
+    }
+}
