26dc16f5980d95419d2af5fb50953f1a4d4f1903
David Gómez Cano <davidg@ILITIA.COM>
2017-10-16 12:30:30 +0200

    Add camera template to Caliburn.Micro project type.

---
+++ templates/Pages/Camera.CaliburnMicro/ViewModels/CameraViewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Param_ItemNamespace.EventHandlers;
+using Caliburn.Micro;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Param_ItemNamespace.ViewModels
+{
+    public class CameraViewViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        private BitmapImage _photo;
+
+        public BitmapImage Photo
+        {
+            get { return _photo; }
+            set { Set(ref _photo, value); }
+        }
+
+        public void OnPhotoTaken(CameraControlEventArgs args)
+        {
+            if (!string.IsNullOrEmpty(args.Photo))
+            {
+                Photo = new BitmapImage(new Uri(args.Photo));
+            }
+        }
+    }
+}
