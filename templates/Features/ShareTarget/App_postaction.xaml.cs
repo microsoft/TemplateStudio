fc413ea7fa7b85d2e4239e0a6104795b79c8b458
Martin Vega <mvegaca@outlook.com>
2017-10-20 13:30:43 +0200

    Add ShareTarget for MVVM Basic

---
+++ templates/Features/ShareTarget/App_postaction.xaml.cs
@@ -0,0 +1,28 @@
+﻿//{[{
+using Windows.UI.Xaml.Controls;
+//}]}
+//{**
+// This code block adds the OnShareTargetActivated event handler to the App class of your project.
+//**}
+namespace Param_RootNamespace
+{
+    public sealed partial class App : Application
+    {
+//^^
+//{[{
+
+        protected override void OnShareTargetActivated(ShareTargetActivatedEventArgs args)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+
+            rootFrame.Navigate(typeof(Views.ShareTargetFeatureExamplePage), args.ShareOperation);
+            Window.Current.Activate();
+        }
+//}]}
+    }
+}
---
+++ templates/Features/ShareTarget/Helpers/ShareOperationExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.Storage;
+
+namespace Param_ItemNamespace.Helpers
+{
+    public static class ShareOperationExtensions
+    {
+        public static async Task<IReadOnlyList<IStorageItem>> GetStorageItemsAsync(this ShareOperation shareOperation)
+        {
+            return await GetOperationDataAsync<IReadOnlyList<IStorageItem>>(shareOperation, StandardDataFormats.StorageItems);
+        }
+
+        public static async Task<Uri> GetWebLinkAsync(this ShareOperation shareOperation)
+        {
+            return await GetOperationDataAsync<Uri>(shareOperation, StandardDataFormats.WebLink) as Uri;
+        }
+
+        private static async Task<T> GetOperationDataAsync<T>(this ShareOperation shareOperation, string dataFormat)
+            where T : class
+        {
+            try
+            {
+                if (dataFormat == StandardDataFormats.WebLink)
+                {
+                    return await shareOperation.Data.GetWebLinkAsync() as T;
+                }
+
+                if (dataFormat == StandardDataFormats.StorageItems)
+                {
+                    return await shareOperation.Data.GetStorageItemsAsync() as T;
+                }
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+
+            return default(T);
+        }
+    }
+}
