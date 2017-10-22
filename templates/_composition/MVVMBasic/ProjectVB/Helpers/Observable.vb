764378e40f9ed08b8e0202180f72c375b21b9300
Matt Lacey <matt@mrlacey.co.uk>
2017-10-22 20:46:36 +0100

    Get more tests passing

--- templates/_composition/MVVMBasic/ProjectVB/Helpers/Observable.vb
+++ templates/_composition/MVVMBasic/ProjectVB/Helpers/Observable.vb
@@ -4,12 +4,12 @@
 
         Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
  
-        Friend Sub [Set](Of T)(ByRef storage As T, value As T, <CallerMemberName> Optional propertyName As String = Nothing)
-            If Equals(storage, value) Then
+        Friend Sub [Set](Of T)(ByRef storage As T, newValue As T, <CallerMemberName> Optional propertyName As String = Nothing)
+            If Equals(storage, newValue) Then
                 return
             End If
  
-            storage = value
+            storage = newValue
             OnPropertyChanged(propertyName)
         End Sub
  
