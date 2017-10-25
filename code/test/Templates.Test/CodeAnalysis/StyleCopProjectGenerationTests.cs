b1b1702d2d9e2c5090e1de74b8abb108208f4ac8
Matt Lacey <matt@mrlacey.co.uk>
2017-10-25 17:37:52 +0100

    fix styleccop issue on last commit

--- code/test/Templates.Test/CodeAnalysis/StyleCopProjectGenerationTests.cs
+++ code/test/Templates.Test/CodeAnalysis/StyleCopProjectGenerationTests.cs
@@ -17,7 +17,7 @@ using Microsoft.VisualStudio.Threading;
 
 namespace Microsoft.Templates.Test
 {
-  //  [Collection("StyleCopCollection")]
+   // [Collection("StyleCopCollection")]
    // [Trait("ExecutionSet", "Minimum")]
    // [Trait("ExecutionSet", "BuildStyleCop")]
     public class StyleCopProjectGenerationTests : BaseTestContextProvider
@@ -34,9 +34,9 @@ namespace Microsoft.Templates.Test
             await _fixture.InitializeFixtureAsync(this);
         }
 
-   //     [Theory]
-   //     [MemberData("GetProjectTemplatesForStyleCopAsync")]
-   //     [Trait("Type", "CodeStyle")]
+       // [Theory]
+       // [MemberData("GetProjectTemplatesForStyleCopAsync")]
+       // [Trait("Type", "CodeStyle")]
         public async Task GenerateAllPagesAndFeaturesAndCheckWithStyleCopAsync(string projectType, string framework)
         {
             await SetUpFixtureForTestingAsync();
