929c87fe879d8d7ee6a31f05081a6af689bcf404
Matt Lacey <matt@mrlacey.co.uk>
2017-10-27 14:03:31 +0100

    Port of Camera Page to VB
    for #1335

---
+++ templates/_composition/_shared/Page.Camera.AddResourceVB/EventHandlers/CameraControlEventArgs.vb
@@ -0,0 +1,20 @@
+﻿Namespace EventHandlers
+    Public Class CameraControlEventArgs
+        Inherits EventArgs
+
+        Private _Photo As String
+
+        Public Property Photo As String
+            Get
+                Return _Photo
+            End Get
+            Set
+                _Photo = Value
+            End Set
+        End Property
+
+        Public Sub New(photo__1 As String)
+            Photo = photo__1
+        End Sub
+    End Class
+End Namespace
