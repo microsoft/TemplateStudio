9b66915d60e0a0a75dc85de15cc4a823c7f61401
Martin Vega <mvegaca@outlook.com>
2017-10-31 17:05:58 +0100

    Add WebToLink PoC

---
+++ poc/WebToAppLink/WebToAppLink/Activation/WebToAppActivationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+using WebToAppLink.Services;
+using WebToAppLink.Views;
+
+namespace WebToAppLink.Activation
+{
+    internal class WebToAppActivationHandler : ActivationHandler<ProtocolActivatedEventArgs>
+    {
+        protected override async Task HandleInternalAsync(ProtocolActivatedEventArgs args)
+        {
+            switch (args.Uri.AbsolutePath)
+            {
+                case "/":
+                    NavigationService.Navigate<MainPage>();
+                    break;
+                case "/windows/apps":
+                    NavigationService.Navigate<WindowsAppsPage>();
+                    break;
+            }            
+            await Task.CompletedTask;
+        }
+
+        protected override bool CanHandleInternal(ProtocolActivatedEventArgs args)
+        {
+            return args.Uri.Host == "developer.microsoft.com";
+        }
+    }
+}
