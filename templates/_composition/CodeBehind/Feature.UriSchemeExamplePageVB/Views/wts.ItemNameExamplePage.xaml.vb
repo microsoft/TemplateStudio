32202a920a55c7f21031bf96f98e903797f75220
Martin Vega <mvegaca@outlook.com>
2017-11-06 15:52:46 +0100

    ShareTarget updates

--- templates/_composition/CodeBehind/Feature.UriSchemeExamplePageVB/Views/wts.ItemNameExamplePage.xaml.vb
+++ templates/_composition/CodeBehind/Feature.UriSchemeExamplePageVB/Views/wts.ItemNameExamplePage.xaml.vb
@@ -4,7 +4,10 @@ Imports Windows.UI.Xaml.Controls
 Imports Windows.UI.Xaml.Navigation
 
 Namespace Views
-    ' TODO WTS: This page exists purely as an example of how to launch a specific page in response to a protocol launch and pass it a value. It is expected that you will delete this page once you have changed the handling of a protocol launch to meet your needs and redirected to another of your pages.
+    ' TODO WTS: This page exists purely as an example of how to launch a specific page
+    ' in response to a protocol launch and pass it a value. It is expected that you will
+    ' delete this page once you have changed the handling of a protocol launch to meet your
+    ' needs and redirected to another of your pages.
     Partial Public NotInheritable Class wts.ItemNameExamplePage
         Inherits Page
         Implements INotifyPropertyChanged
