7112ca6d5598cedb73ad5df036b26f1c18337e97
David Gómez Cano <davidg@ILITIA.COM>
2017-11-14 16:39:46 +0100

    First version of Xamarin project template.

---
+++ templates/Projects/Xamarin/wts.DefaultProject/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+using wts.DefaultProject.Views;
+
+namespace wts.DefaultProject
+{
+    public partial class App : Application
+	{
+        public static NavigationPage NavPage = null;
+
+        public App ()
+		{
+			InitializeComponent();
+
+            //FIRST PAGE IS REQUIRED TO START THE APP
+            NavPage = new NavigationPage(new FirstPage());
+
+            //DEFAULT
+            MainPage = NavPage;
+
+            //MASTER DETAIL
+            //MainPage = new Views.Navigation.MasterDetailPage()
+            //{
+            //    Detail = NavPage
+            //};
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
