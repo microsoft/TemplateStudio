7aafd8d22b91d6a67b4a01a730cd96d21165a8a8
David Gómez Cano <davidg@ILITIA.COM>
2017-11-16 11:20:12 +0100

    Fix postaction

--- templates/_composition/_shared/Project.Blank.AddNavigationXamarin/App_postaction.xaml.cs
+++ templates/_composition/_shared/Project.Blank.AddNavigationXamarin/App_postaction.xaml.cs
@@ -10,20 +10,5 @@
             MainPage = new NavigationPage(new Param_HomeNamePage());
             //}]}            
         }
-
-        protected override void OnStart ()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep ()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume ()
-        {
-            // Handle when your app resumes
-        }
     }
 }
