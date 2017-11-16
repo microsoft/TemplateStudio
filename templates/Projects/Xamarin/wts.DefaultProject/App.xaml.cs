a50e687d3c8d9d923537babacbfd99fd142448ab
David Gómez Cano <davidg@ILITIA.COM>
2017-11-16 11:16:32 +0100

    Add navigation postaction to xamarin project

--- templates/Projects/Xamarin/wts.DefaultProject/App.xaml.cs
+++ templates/Projects/Xamarin/wts.DefaultProject/App.xaml.cs
@@ -5,23 +5,9 @@ namespace wts.DefaultProject
 {
     public partial class App : Application
 	{
-        public static NavigationPage NavPage = null;
-
         public App ()
 		{
 			InitializeComponent();
-
-            //FIRST PAGE IS REQUIRED TO START THE APP
-            NavPage = new NavigationPage(new FirstPage());
-
-            //DEFAULT
-            MainPage = NavPage;
-
-            //MASTER DETAIL
-            //MainPage = new Views.Navigation.MasterDetailPage()
-            //{
-            //    Detail = NavPage
-            //};
         }
 
 		protected override void OnStart ()
