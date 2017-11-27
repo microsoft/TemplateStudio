ccfe633a35acd567270be3c31d83e73b5ce39c4f
David Gómez <v-davgom@microsoft.com>
2017-11-27 15:40:02 +0100

    Create master detail navigation template to Code-Behind framework

---
+++ templates/Xamarin/_composition/CodeBehind/Page.MasterDetail.RegisterPage/Param_ProjectName/Views/Navigation/MasterDetailPageMaster_postaction.xaml.cs
@@ -0,0 +1,19 @@
+﻿//^^
+//{[{
+using System.Collections.ObjectModel;
+using Param_RootNamespace.Models;
+//}]}
+
+namespace Param_RootNamespace.Views.Navigation
+{
+    public partial class MasterDetailPageMaster : ContentPage
+    {
+        public ObservableCollection<MasterDetailPageMenuItem> PrimaryMenuItems { get; private set; } = new ObservableCollection<MasterDetailPageMenuItem>
+        {
+            //^^
+            //{[{
+                new MasterDetailPageMenuItem { Title = "wts.ItemName", TargetType = typeof(wts.ItemNamePage), IconSource = "blank.png"},
+            //}]}
+        };
+    }
+}
