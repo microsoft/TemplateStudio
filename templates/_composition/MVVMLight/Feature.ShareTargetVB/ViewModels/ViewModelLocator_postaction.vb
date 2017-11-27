af61ecec35c28a7fd6cc8dbdf29aa982ae442cf8
Matt Lacey <matt@mrlacey.co.uk>
2017-11-27 22:05:55 +0000

    Add VB versions of ShareTarget templates
    For 1342

---
+++ templates/_composition/MVVMLight/Feature.ShareTargetVB/ViewModels/ViewModelLocator_postaction.vb
@@ -0,0 +1,25 @@
+﻿Namespace ViewModels
+    Public Class ViewModelLocator
+        Public Sub New()
+            ServiceLocator.SetLocatorProvider(Function() SimpleIoc.[Default])
+            '{[{
+            If SimpleIoc.[Default].IsRegistered(Of NavigationServiceEx)() Then
+                Return
+            End If
+            '}]}
+            SimpleIoc.[Default].Register(Function() New NavigationServiceEx())
+            '^^
+            '{[{
+            Register(Of wts.ItemNameViewModel, wts.ItemNamePage)()
+                '}]}
+        End Sub
+
+        '{[{
+        Public ReadOnly Property wts.ItemNameViewModel() As wts.ItemNameViewModel
+            Get
+                Return ServiceLocator.Current.GetInstance(Of wts.ItemNameViewModel)()
+            End Get
+        End Property
+        '}]}
+    End Class
+End Namespace
