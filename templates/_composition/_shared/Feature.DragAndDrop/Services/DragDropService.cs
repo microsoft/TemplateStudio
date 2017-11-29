b878e488764c9b9a9bcbe62b46402261f7d58133
David Gómez <v-davgom@microsoft.com>
2017-11-29 12:40:28 +0100

    Add drag and drop feature for Code-Behind framework.

--- templates/_composition/_shared/Feature.DragAndDrop/Services/DragDropService.cs
+++ templates/_composition/_shared/Feature.DragAndDrop/Services/DragDropService.cs
@@ -1,8 +1,9 @@
-﻿using Param_RootNamespace.Models;
-using Windows.ApplicationModel.DataTransfer;
+﻿using Windows.ApplicationModel.DataTransfer;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
+
+using Param_RootNamespace.Models;
 
 namespace Param_ItemNamespace.Services
 {
@@ -61,50 +62,11 @@ namespace Param_ItemNamespace.Services
         }
 
         private static void ConfigureUIElement(UIElement element, DropConfiguration configuration)
-        {
-            element.DragEnter += (sender, args) =>
-            {
-                //Operation is copy by default
-                args.AcceptedOperation = DataPackageOperation.Copy;
-
-                var data = new DragDropData { AcceptedOperation = args.AcceptedOperation, DataView = args.DataView };
-                configuration.DragEnterCommand?.Execute(data);
-                args.AcceptedOperation = data.AcceptedOperation;
-            };
-
-            element.DragOver += (sender, args) =>
-            {
-                var data = new DragDropData { AcceptedOperation = args.AcceptedOperation, DataView = args.DataView };
-                configuration.DragOverCommand?.Execute(data);
-                args.AcceptedOperation = data.AcceptedOperation;
-            };
-
-            element.DragLeave += (sender, args) =>
-            {
-                var data = new DragDropData { AcceptedOperation = args.AcceptedOperation, DataView = args.DataView };
-                configuration.DragLeaveCommand?.Execute(data);
-            };
-
-            element.Drop += async (sender, args) =>
-            {
-                await configuration.ProcessComandsAsync(args.DataView);
-            };
-
+	    {
         }
 
         private static void ConfigureListView(ListViewBase listview, ListViewDropConfiguration configuration)
-        {
-            listview.DragItemsStarting += (sender, args) =>
-            {
-                var data = new DragDropStartingData { Data = args.Data, Items = args.Items };
-                configuration.DragItemsStartingCommand?.Execute(data);
-            };
-
-            listview.DragItemsCompleted += (sender, args) =>
-            {
-                var data = new DragDropCompletedData {DropResult = args.DropResult, Items = args.Items };
-                configuration.DragItemsCompletedCommand?.Execute(data);
-            };
+        {		
         }
 
         private static void OnVisualConfigurationPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
