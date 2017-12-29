d00a137660b17cd1ebe679aa14f85b414a5a9e69
Matt Lacey <matt@mrlacey.co.uk>
2017-12-29 16:22:45 +0000

    Include links to VB versions of docs in comments
    #1337

--- templates/Projects/DefaultVB/Activation/ActivationHandler.vb
+++ templates/Projects/DefaultVB/Activation/ActivationHandler.vb
@@ -1,5 +1,5 @@
 ﻿Namespace Activation
-    ' For more information on application activation see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/activation.md
+    ' For more information on application activation see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/activation.vb.md
     Friend MustInherit Class ActivationHandler
         Public MustOverride Function CanHandle(args As Object) As Boolean
         Public MustOverride Function HandleAsync(args As Object) As Task
