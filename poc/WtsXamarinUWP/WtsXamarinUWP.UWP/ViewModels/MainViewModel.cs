2e1c369fc3928335b2069a14df6303b0219bc581
David Gómez <v-davgom@microsoft.com>
2018-01-09 10:15:00 +0100

    Add UWP native project

---
+++ poc/WtsXamarinUWP/WtsXamarinUWP.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using WtsXamarinUWP.UWP.Helpers;
+
+namespace WtsXamarinUWP.UWP.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ poc/WtsXamarinUWP/WtsXamarinUWP.UWP/Views/BlankPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Windows.UI.Xaml.Controls;
+
+using WtsXamarinUWP.UWP.ViewModels;
+
+namespace WtsXamarinUWP.UWP.Views
+{
+    public sealed partial class BlankPage : Page
+    {
+        public BlankViewModel ViewModel { get; } = new BlankViewModel();
+
+        public BlankPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
