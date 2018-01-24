c26c2f84fbba68bd5271d57900515bb596309d28
Martin Vega <mvegaca@outlook.com>
2018-01-24 08:42:12 +0100

    Add CodeViewer to V2

--- code/src/UI/V2ViewModels/Common/DataItems/NewItemFileViewModel.cs
+++ code/src/UI/V2ViewModels/Common/DataItems/NewItemFileViewModel.cs
@@ -2,7 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.IO;
 using System.Windows.Media;
+using Microsoft.Templates.Core.Gen;
 using Microsoft.Templates.Core.PostActions.Catalog.Merge;
 using Microsoft.Templates.UI.V2Services;
 
@@ -16,61 +19,64 @@ namespace Microsoft.Templates.UI.V2ViewModels.Common
 
         public string Description { get; private set; } // TODO: mvegaca Load from resource
 
-        public string Subject { get; private set; }
+        public string Subject { get; }
 
         public string Icon { get; private set; }
 
         public Brush CircleColor { get; private set; }
 
-        public FileExtension FileExtension { get; private set; }
+        public FileExtension FileExtension { get; }
 
-        private NewItemFileViewModel(FileStatus fileStatus)
+        public Func<string, string> UpdateTextAction { get; }
+
+        public string TempFile { get; }
+
+        public string ProjectFile { get; }
+
+        public string FailedPostaction { get; private set; }
+
+        private NewItemFileViewModel(FileStatus fileStatus, string subject, Func<string, string> updateTextAction = null)
         {
             FileStatus = fileStatus;
             Icon = GetIcon();
             CircleColor = GetCircleColor();
+            UpdateTextAction = updateTextAction;
+            Subject = subject;
+            TempFile = Path.Combine(GenContext.Current.OutputPath, subject);
+            ProjectFile = Path.Combine(GenContext.Current.ProjectPath, subject);
         }
 
         public static NewItemFileViewModel NewFile(NewItemGenerationFileInfo file)
         {
-            return new NewItemFileViewModel(FileStatus.NewFile)
-            {
-                Subject = file.Name
-            };
+            return new NewItemFileViewModel(FileStatus.NewFile, file.Name);
         }
 
         public static NewItemFileViewModel ModifiedFile(NewItemGenerationFileInfo file)
         {
-            return new NewItemFileViewModel(FileStatus.ModifiedFile)
-            {
-                Subject = file.Name
-            };
+            return new NewItemFileViewModel(FileStatus.ModifiedFile, file.Name);
         }
 
         public static NewItemFileViewModel ConflictingFile(NewItemGenerationFileInfo file)
         {
-            return new NewItemFileViewModel(FileStatus.ConflictingFile)
-            {
-                Subject = file.Name
-            };
+            return new NewItemFileViewModel(FileStatus.ConflictingFile, file.Name);
         }
 
         public static NewItemFileViewModel UnchangedFile(NewItemGenerationFileInfo file)
         {
-            return new NewItemFileViewModel(FileStatus.UnchangedFile)
-            {
-                Subject = file.Name
-            };
+            return new NewItemFileViewModel(FileStatus.UnchangedFile, file.Name);
         }
 
         public static NewItemFileViewModel ConflictingStylesFile(FailedMergePostAction file)
         {
-            return new NewItemFileViewModel(FileStatus.ConflictingStylesFile);
+            return new NewItemFileViewModel(FileStatus.ConflictingStylesFile, file.FileName, AsUserFriendlyPostAction)
+            {
+                FailedPostaction = Path.Combine(GenContext.Current.OutputPath, $"{file.FileName.Replace(".xaml", string.Empty)}_failedpostaction.xaml")
+            };
         }
 
         public static NewItemFileViewModel WarningFile(FailedMergePostAction file)
         {
-            return new NewItemFileViewModel(FileStatus.WarningFile);
+            return new NewItemFileViewModel(FileStatus.WarningFile, file.FileName, AsUserFriendlyPostAction);
         }
 
         private string GetIcon()
@@ -120,5 +126,7 @@ namespace Microsoft.Templates.UI.V2ViewModels.Common
                     return UIStylesService.Instance.ListItemText;
             }
         }
+
+        private static string AsUserFriendlyPostAction(string arg) => arg.AsUserFriendlyPostAction();
     }
 }
