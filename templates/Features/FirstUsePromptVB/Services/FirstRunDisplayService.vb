094291ca8e4824027152f69f83330f83a29689e5
sibille <sibille@ilitia.com>
2018-01-24 21:51:29 +0100

    Fix vb tests

--- templates/Features/FirstUsePromptVB/Services/FirstRunDisplayService.vb
+++ templates/Features/FirstUsePromptVB/Services/FirstRunDisplayService.vb
@@ -3,12 +3,13 @@ Imports Microsoft.Toolkit.Uwp.Helpers
 
 Namespace Services
     Public NotInheritable Class FirstRunDisplayService
-        Shared shown As Boolean = false
+        Shared shown As Boolean = False
+
         Private Sub New()
         End Sub
 
         Friend Shared Async Function ShowIfAppropriateAsync() As Task
-            If SystemInformation.IsFirstRun And Not shown Then
+            If SystemInformation.IsFirstRun AndAlso Not shown Then
                 shown = true
                 Dim dialog = New FirstRunDialog()
                 Await dialog.ShowAsync()
