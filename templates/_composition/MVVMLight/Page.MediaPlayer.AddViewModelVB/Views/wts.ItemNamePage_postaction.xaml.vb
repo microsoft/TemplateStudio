5c0caf63c8066ec30c7005ed0d6cdcc3e51001f5
Matt Lacey <matt@mrlacey.co.uk>
2018-02-02 12:14:32 +0000

    Added Fix for VB projects
    For #1757

---
+++ templates/_composition/MVVMLight/Page.MediaPlayer.AddViewModelVB/Views/wts.ItemNamePage_postaction.xaml.vb
@@ -0,0 +1,16 @@
+﻿'{[{
+Imports Param_ItemNamespace.ViewModels
+'}]}
+Namespace Views
+    Public NotInheritable Partial Class wts.ItemNamePage
+        Inherits Page
+'{[{
+
+        Private ReadOnly Property ViewModel As wts.ItemNameViewModel
+            Get
+                Return TryCast(DataContext, wts.ItemNameViewModel)
+            End Get
+        End Property
+'}]}
+    End Class
+End Namespace
