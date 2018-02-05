19bd8ad7000be33047f7c764f1753221759fb5c3
Matt Lacey <matt@mrlacey.co.uk>
2018-02-05 20:39:59 +0000

    Include VB projects in RightClickLegacy tests
    For #1325

--- code/test/Templates.Test/BuildRightClickWithLegacy/BuildRightClickWithLegacyFixture.cs
+++ code/test/Templates.Test/BuildRightClickWithLegacy/BuildRightClickWithLegacyFixture.cs
@@ -4,16 +4,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.Templates.Core;
 using Microsoft.Templates.Core.Gen;
 using Microsoft.Templates.Core.Locations;
@@ -36,14 +30,11 @@ namespace Microsoft.Templates.Test
         {
             List<object[]> result = new List<object[]>();
 
-            // TODO: X-Ref https://github.com/Microsoft/WindowsTemplateStudio/issues/1325
-            // Re-enable for all languages
-            ////foreach (var language in ProgrammingLanguages.GetAllLanguages())
+            foreach (var language in ProgrammingLanguages.GetAllLanguages())
             {
-                const string language = ProgrammingLanguages.CSharp;
                 Configuration.Current.CdnUrl = "https://wtsrepository.blob.core.windows.net/pro/";
 
-                await InitializeTemplatesAsync(new LegacyTemplatesSourceV2());
+                await InitializeTemplatesAsync(new LegacyTemplatesSourceV2(), language);
 
                 var projectTypes = GenContext.ToolBox.Repo.GetAll().Where(t => t.GetTemplateType() == TemplateType.Project
                                                           && t.GetLanguage() == language).SelectMany(p => p.GetProjectTypeList()).Distinct();
@@ -62,11 +53,11 @@ namespace Microsoft.Templates.Test
             return result;
         }
 
-        private static async Task InitializeTemplatesAsync(TemplatesSource source)
+        private static async Task InitializeTemplatesAsync(TemplatesSource source, string language)
         {
             Configuration.Current.CdnUrl = "https://wtsrepository.blob.core.windows.net/pro/";
 
-            GenContext.Bootstrap(source, new FakeGenShell(ProgrammingLanguages.CSharp), new Version("1.6"), ProgrammingLanguages.CSharp);
+            GenContext.Bootstrap(source, new FakeGenShell(language), new Version("1.7"), language);
             if (!syncExecuted)
             {
                 await GenContext.ToolBox.Repo.SynchronizeAsync(true);
@@ -75,18 +66,19 @@ namespace Microsoft.Templates.Test
             }
         }
 
-        public async Task ChangeTemplatesSourceAsync(TemplatesSource source)
+        public async Task ChangeTemplatesSourceAsync(TemplatesSource source, string language)
         {
-            GenContext.Bootstrap(source, new FakeGenShell(ProgrammingLanguages.CSharp), ProgrammingLanguages.CSharp);
+            GenContext.Bootstrap(source, new FakeGenShell(language), language);
             await GenContext.ToolBox.Repo.SynchronizeAsync(true);
         }
 
-        public override async Task InitializeFixtureAsync(IContextProvider contextProvider, string framework = "")
+        // Renamed second parameter as this fixture needs the language while others don't
+        public override async Task InitializeFixtureAsync(IContextProvider contextProvider, string language = "")
         {
             GenContext.Current = contextProvider;
             Configuration.Current.Environment = "Pro";
             Configuration.Current.CdnUrl = "https://wtsrepository.blob.core.windows.net/pro/";
-            await InitializeTemplatesAsync(Source);
+            await InitializeTemplatesAsync(Source, language);
         }
     }
 }
