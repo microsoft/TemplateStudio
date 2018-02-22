d3e0ad53529ee7eda3a59f80a063aa1296c52f71
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-02-21 21:52:19 -0700

    Creates separate sample data feature for Prism, eliminating the need for a composition template to switch the default service into a concrete implementation with interface.

---
+++ templates/Features/SampleData.Prism/Services/ISampleDataService.cs
@@ -0,0 +1,14 @@
+using Param_ItemNamespace.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Param_ItemNamespace.Services
+{
+    // This class holds sample data used by some generated pages to show how they can be used.
+    // TODO WTS: Delete this file once your app is using real data.
+    public interface ISampleDataService
+    {
+
+    }
+}
