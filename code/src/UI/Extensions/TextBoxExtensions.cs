f35a8cc52042a98ccf7ec1b5353322483b9bb6d7
Martin Vega <mvegaca@outlook.com>
2018-02-22 09:29:09 +0100

    Remove LostKeyboardFocusCommand from Textbox extensions

--- code/src/UI/Extensions/TextBoxExtensions.cs
+++ code/src/UI/Extensions/TextBoxExtensions.cs
@@ -2,29 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using Microsoft.Templates.UI.Services;
 
 namespace Microsoft.Templates.UI.Extensions
 {
     public class TextBoxExtensions
     {
-        private static string _currentFocusTemplateName;
-
         public static readonly DependencyProperty IsTextSelectedProperty = DependencyProperty.RegisterAttached(
         "IsTextSelected",
         typeof(bool),
         typeof(TextBoxExtensions),
         new UIPropertyMetadata(false, OnIsTextSelectedPropertyChanged));
-
-        public static readonly DependencyProperty LostKeyboardFocusCommandProperty = DependencyProperty.RegisterAttached(
-          "LostKeyboardFocusCommand",
-          typeof(ICommand),
-          typeof(TextBoxExtensions),
-          new PropertyMetadata(null, OnLostKeyboardFocusCommandPropertyChanged));
 
         public static bool GetIsTextSelected(DependencyObject obj)
         {
@@ -36,16 +25,6 @@ namespace Microsoft.Templates.UI.Extensions
             obj.SetValue(IsTextSelectedProperty, value);
         }
 
-        public static void SetLostKeyboardFocusCommand(UIElement element, ICommand value)
-        {
-            element.SetValue(LostKeyboardFocusCommandProperty, value);
-        }
-
-        public static ICommand GetLostKeyboardFocusCommand(UIElement element)
-        {
-            return (ICommand)element.GetValue(LostKeyboardFocusCommandProperty);
-        }
-
         private static void OnIsTextSelectedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var textBox = d as TextBox;
@@ -54,18 +33,6 @@ namespace Microsoft.Templates.UI.Extensions
             {
                 textBox?.Focus();
                 textBox.Select(0, textBox.Text.Length);
-            }
-        }
-
-        private static void OnLostKeyboardFocusCommandPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var textBox = d as TextBox;
-            if (textBox != null)
-            {
-                textBox.LostKeyboardFocus += (sender, args) =>
-                {
-                    GetLostKeyboardFocusCommand(textBox).Execute(args);
-                };
             }
         }
     }
