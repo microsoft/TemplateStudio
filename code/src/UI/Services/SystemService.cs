23c4b24e88aaa93abaecc366ad5789f7f3fff4e6
Martin Vega <mvegaca@outlook.com>
2018-03-05 16:52:35 +0100

    Layout fixes

--- code/src/UI/Services/SystemService.cs
+++ code/src/UI/Services/SystemService.cs
@@ -61,7 +61,7 @@ namespace Microsoft.Templates.UI.Services
 
         public(double width, double height) GetMainWindowSize()
         {
-            double width = 1110; // 1277;
+            double width = 1120; // 1277;
             double height = 733; // 727
             double dpi = 0;
             if (Application.Current != null)
