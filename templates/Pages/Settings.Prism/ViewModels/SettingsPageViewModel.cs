8fa1ea671098b07700a3ca297afaa2f09eb4a322
David Gómez <v-davgom@microsoft.com>
2018-03-05 12:27:06 +0100

    Modify settings page to show app name correctly

--- templates/Pages/Settings.Prism/ViewModels/SettingsPageViewModel.cs
+++ templates/Pages/Settings.Prism/ViewModels/SettingsPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Input;
 using Prism.Commands;
+using Param_RootNamespace.Helpers;
 using Param_RootNamespace.Services;
 using Windows.ApplicationModel;
 using Windows.UI.Xaml;
@@ -59,11 +60,12 @@ namespace Param_ItemNamespace.ViewModels
 
         private string GetVersionDescription()
         {
+            var appName = "AppDisplayName".GetLocalized();
             var package = Package.Current;
             var packageId = package.Id;
             var version = packageId.Version;
 
-            return $"{package.DisplayName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
         }
     }
 }
