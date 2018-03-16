2369d2d62a2e7614706609b03570f3538f3edffd
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-03-16 06:36:18 -0700

    Updates comment to make it specific to the interface.

--- templates/Features/SampleData.Prism/Services/ISampleDataService.cs
+++ templates/Features/SampleData.Prism/Services/ISampleDataService.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Param_ItemNamespace.Services
 {
-    // This class holds sample data used by some generated pages to show how they can be used.
+    // This interface specifies methods used by some generated pages to show how they can be used.
     // TODO WTS: Delete this file once your app is using real data.
     public interface ISampleDataService
     {
