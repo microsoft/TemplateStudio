7ab7de66a5d77446e1c62a00322571978b78f048
Raúl Alarcón <ralarcon@microsoft.com>
2018-03-23 11:11:58 +0100

    Script to include files

---
+++ _build/CodeSign_IncludeSignedFiles.ps1
@@ -0,0 +1,41 @@
+﻿param
+(
+	[Parameter(Mandatory=$true)]
+	[string]$vsixFilePath,
+    [Parameter(Mandatory=$true)]
+	[string]$inputPath
+)
+
+
+Add-Type -Assembly System.IO.Compression
+
+
+
+$files= Get-ChildItem $inputPath -rec | where {!$_.PSIsContainer}
+
+$vsix = [IO.Compression.ZipFile]::Open($vsixFilePath, [System.IO.Compression.ZipArchiveMode]::Update)
+
+foreach ($file in $files)
+{
+    Write-Host "Processing file "$file.FullName
+    $newFileName = $file.FullName.Replace("$inputPath\", '').Replace("\", "/")
+
+    $entry = $vsix.Entries | where {$_.FullName -eq $newFileName}
+
+    if ($entry)
+    {
+        Write-Host Deleted file $entry.FullName
+        $entry.Delete()
+    }
+    else
+    {
+        Write-Host $newFileName not in zip
+    }
+
+    Write-Host Adding file $newFileName 
+    Write-Host
+    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($vsix, $file.FullName, $newFileName) > $null 
+}
+
+#free object
+$vsix.Dispose()
