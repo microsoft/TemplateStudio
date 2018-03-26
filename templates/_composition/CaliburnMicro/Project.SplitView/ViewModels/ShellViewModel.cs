8280d0df561cc012eae553c5178aaa6a4a96f433
Matt Lacey <matt@mrlacey.co.uk>
2018-03-26 13:03:28 +0100

    Changing NavigationViewItemExtensions naming for Caliburn.Micro

--- templates/_composition/CaliburnMicro/Project.SplitView/ViewModels/ShellViewModel.cs
+++ templates/_composition/CaliburnMicro/Project.SplitView/ViewModels/ShellViewModel.cs
@@ -45,7 +45,7 @@ namespace wts.ItemName.ViewModels
             var item = _navigationView.MenuItems
                             .OfType<NavigationViewItem>()
                             .First(menuItem => (string)menuItem.Content == (string)args.InvokedItem);
-            var pageType = item.GetValue(NavigationViewItemExtensions.PageTypeProperty) as Type;
+            var pageType = item.GetValue(NavHelper.NavigateToProperty) as Type;
             var viewModelType = ViewModelLocator.LocateTypeForViewType(pageType, false);
             _navigationService.NavigateToViewModel(viewModelType);
         }
@@ -60,7 +60,7 @@ namespace wts.ItemName.ViewModels
         private bool IsNavigationViewItemFromPageType(NavigationViewItem menuItem, Type sourcePageType)
         {
             var sourceViewModelType = ViewModelLocator.LocateTypeForViewType(sourcePageType, false);
-            var pageType = menuItem.GetValue(NavigationViewItemExtensions.PageTypeProperty) as Type;
+            var pageType = menuItem.GetValue(NavHelper.NavigateToProperty) as Type;
             var viewModelType = ViewModelLocator.LocateTypeForViewType(pageType, false);
             return viewModelType == sourceViewModelType;
         }
