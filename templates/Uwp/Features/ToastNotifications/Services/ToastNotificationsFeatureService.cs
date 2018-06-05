47fa44f350a4fe0fc19c65761098402fd877bcb3
Martin Vega <mvegaca@outlook.com>
2018-06-05 12:15:41 +0200

    Fix doc links in generated code

--- templates/Uwp/Features/ToastNotifications/Services/ToastNotificationsFeatureService.cs
+++ templates/Uwp/Features/ToastNotifications/Services/ToastNotificationsFeatureService.cs
@@ -18,7 +18,7 @@ namespace Param_RootNamespace.Services
         protected override async Task HandleInternalAsync(ToastNotificationActivatedEventArgs args)
         {
             //// TODO WTS: Handle activation from toast notification
-            //// More details at https://blogs.msdn.microsoft.com/tiles_and_toasts/2015/07/08/quickstart-sending-a-local-toast-notification-and-handling-activations-from-it-windows-10/
+            //// More details at https://docs.microsoft.com/windows/uwp/design/shell/tiles-and-notifications/send-local-toast
 
             await Task.CompletedTask;
         }
