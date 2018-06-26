072cc8b264eea138d96b1fa522150e9cf0959748
Martin Vega <mvegaca@outlook.com>
2018-06-26 14:14:14 +0200

    Test fixes

--- templates/Uwp/_composition/CaliburnMicro/Page.ImageGallery.LoadData_TabbedPivot/Views/wts.ItemNamePage_postaction.xaml.cs
+++ templates/Uwp/_composition/CaliburnMicro/Page.ImageGallery.LoadData_TabbedPivot/Views/wts.ItemNamePage_postaction.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace Param_ItemNamespace.Views
 {
-    public sealed partial class wts.ItemNamePage : Page, IImageGalleryPage
+    public sealed partial class wts.ItemNamePage : Page, Iwts.ItemNamePage
     {
         public wts.ItemNamePage()
         {
