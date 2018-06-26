276b44e64b338ffbc367b15be721d070edb00cd6
Martin Vega <mvegaca@outlook.com>
2018-06-26 12:43:09 +0200

    Fix ImageGallery in PivotItem in C#

---
+++ templates/Uwp/_composition/CaliburnMicro/Page.ImageGallery.LoadData_Blank_SplitView/Views/wts.ItemNamePage_postaction.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Param_ItemNamespace.Views
+{
+    public sealed partial class wts.ItemNamePage : Page, Iwts.ItemNamePage
+    {
+        public wts.ItemNamePage()
+        {
+            InitializeComponent();
+        }
+
+        public GridView GetGridView() => gridView;
+
+        //{[{
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                await ViewModel.LoadAnimationAsync();
+            }
+        }
+        //}]}
+    }
+}
---
+++ templates/Uwp/_composition/CaliburnMicro/Page.ImageGallery.LoadData_TabbedPivot/Views/wts.ItemNamePage_postaction.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Param_ItemNamespace.Views
+{
+    public sealed partial class wts.ItemNamePage : Page, IImageGalleryPage
+    {
+        public wts.ItemNamePage()
+        {
+            //{[{
+            Loaded += wts.ItemNamePage_Loaded;
+            //}]}
+        }
+
+        public GridView GetGridView() => gridView;
+
+        //{[{
+        private async void wts.ItemNamePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.LoadAnimationAsync();
+        }
+        //}]}
+    }
+}
