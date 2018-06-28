250f44525eafb606783e05cd8b6e545047b17edf
Martin Vega <mvegaca@outlook.com>
2018-06-28 16:19:40 +0200

    Add load logic in Camera and MediaPlayer in PivotBehavior

--- templates/Uwp/Pages/Camera.Prism/Views/CameraViewPage.xaml.cs
+++ templates/Uwp/Pages/Camera.Prism/Views/CameraViewPage.xaml.cs
@@ -7,6 +7,7 @@ namespace Param_ItemNamespace.Views
         public CameraViewPage()
         {
             InitializeComponent();
+            ViewModel.Initialize(cameraControl);
         }
     }
 }
