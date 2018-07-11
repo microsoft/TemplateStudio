a15defa01d82c73e5b22ffb0b8409792b382c97a
Matt Lacey <matt@mrlacey.co.uk>
2018-07-11 01:26:31 +0100

    Page.Ink.AddTransformService.VB
    For #2450

---
+++ templates/Uwp/_composition/_shared/Page.Ink.AddTransformServiceVB/Services/Ink/InkTransformResult.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Collections.Generic
+Imports Windows.UI.Input.Inking
+Imports Windows.UI.Xaml
+Imports Windows.UI.Xaml.Controls
+
+Namespace Services.Ink
+    Public Class InkTransformResult
+        Public Property Strokes As List(Of InkStroke) = New List(Of InkStroke)()
+        Public Property TextAndShapes As List(Of UIElement) = New List(Of UIElement)()
+        Public Property DrawingCanvas As Canvas
+
+        Public Sub New(drawingCanvas As Canvas)
+            DrawingCanvas = drawingCanvas
+        End Sub
+    End Class
+End Namespace
