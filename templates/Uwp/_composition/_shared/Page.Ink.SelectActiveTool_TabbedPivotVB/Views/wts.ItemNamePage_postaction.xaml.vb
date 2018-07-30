307bc4f84d3ee4ab0d32c44a140dfb2c87d427d7
sibille <sibille@users.noreply.github.com>
2018-07-30 15:50:03 +0200

    Update wts.ItemNamePage_postaction.xaml.vb

--- templates/Uwp/_composition/_shared/Page.Ink.SelectActiveTool_TabbedPivotVB/Views/wts.ItemNamePage_postaction.xaml.vb
+++ templates/Uwp/_composition/_shared/Page.Ink.SelectActiveTool_TabbedPivotVB/Views/wts.ItemNamePage_postaction.xaml.vb
@@ -2,7 +2,7 @@
                                     '^^
                                     '{[{
 
-                                    'in tabbedpivot projects the ballpoint pen is not selected by default, so we set it explicitly
+                                    ' In tabbedpivot projects the ballpoint pen is not selected by default, so we set it explicitly
                                     toolbar.ActiveTool = toolbar.GetToolButton(InkToolbarTool.BallpointPen)
                                     toolbar.ActiveTool.IsChecked = true
                                     '}]}
