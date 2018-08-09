2605af8ee05976a170e978d2b95f2ba4e16f96ef
Martin Vega <mvegaca@outlook.com>
2018-08-09 13:25:16 +0200

    Add Theme Initialization from composition templates c#

---
+++ templates/Uwp/_composition/Prism/Feature.ThemeSelection.Initialize/App$CleanUpInitializeAsync_gpostaction.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Param_RootNamespace
+{
+    public sealed partial class App : PrismUnityApplication
+    {
+        private async Task LaunchApplicationAsync(string page, object launchParam)
+        {
+//{--{
+            await Task.CompletedTask;
+//}--}
+        }
+    }
+}
---
+++ templates/Uwp/_composition/_shared/Feature.ThemeSelection.Initialize/Services/ActivationService$CleanUpInitializeAsync_gpostaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Param_ItemNamespace.Services
+{
+    internal class ActivationService
+    {
+        private async Task InitializeAsync()
+        {
+            //{--{
+            await Task.CompletedTask;
+            //}--}
+        }
+
+        private async Task StartupAsync()
+        {
+            //{--{
+            await Task.CompletedTask;
+            //}--}
+        }
+    }
+}
