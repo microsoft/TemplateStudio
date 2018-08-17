8aec63c4eef78874fa9d21ed1e7d9c0a2eb5ac2d
Martin Vega <mvegaca@outlook.com>
2018-08-17 09:52:04 +0200

    Add UserActivity Templates

---
+++ templates/Uwp/Features/UserActivity.Prism/App_postaction.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Param_RootNamespace
+{
+    public sealed partial class App : PrismUnityApplication
+    {
+        private async Task LaunchApplicationAsync(string page, object launchParam)
+        {
+            Window.Current.Activate();
+//{[{
+
+            // TODO WTS: This is a sample to demonstrate how to add a UserActivity. Please adapt and move this method call to where you consider convenient in your app.
+            await UserActivityService.AddSampleUserActivity();
+//}]}
+            await Task.CompletedTask;
+        }
+    }
+}
