848b85472b3a51381562e1ff5f017966bf06d843
sibille <sibille@users.noreply.github.com>
2018-09-07 11:13:01 +0200

    Fixing stylecop issues

--- templates/Uwp/_composition/Prism/Feature.MultiView/App.xaml_postaction.cs
+++ templates/Uwp/_composition/Prism/Feature.MultiView/App.xaml_postaction.cs
@@ -5,10 +5,11 @@
 protected override async Task OnInitializeAsync(IActivatedEventArgs args)
 {
     //{[{
-    await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
-    () =>
-    {
-        WindowManagerService.Current.Initialize();
-    });
+    await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+        Windows.UI.Core.CoreDispatcherPriority.Normal,
+        () =>
+        {
+            WindowManagerService.Current.Initialize();
+        });
     //}]}
 }
