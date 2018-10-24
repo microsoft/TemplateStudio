ebae043c2b83308210e5d0039e156c9ca1b2b442
sibille <sibille@ilitia.com>
2018-10-24 23:25:35 +0200

    Add vb templates

--- templates/Uwp/Features/SuspendAndResumeVB/Services/SuspendAndResumeService.vb
+++ templates/Uwp/Features/SuspendAndResumeVB/Services/SuspendAndResumeService.vb
@@ -5,15 +5,20 @@ Imports Param_RootNamespace.Helpers
 
 Namespace Services
 
-    ' More details regarding the application lifecycle and how to handle suspend and resume at https://docs.microsoft.com/windows/uwp/launch-resume/app-lifecycle
+    ' The SuspendAndResumeService allows you to the save App data before the app is being suspended (or enters in background state).
+    ' In case the App is terminated during suspension the data is restored during app launch using this ActivationHandler.
+    ' In case the App is resumed without being terminated a resume event is fired that allows you to refresh App data that might
+    ' be outdated (e.g data from online feed)
+    ' Documentation:
+    '     * How to implement and test: https://github.com/Microsoft/WindowsTemplateStudio/blob/dev/docs/features/suspend-and-resume.md
+    '     * Application Lifecycle: https://docs.microsoft.com/windows/uwp/launch-resume/app-lifecycle
     Friend Class SuspendAndResumeService
         Inherits ActivationHandler(Of LaunchActivatedEventArgs)
 
         Private Const StateFilename As String = "SuspendAndResumeState"
 
-        ' TODO WTS: Subscribe to this event if you want to save the current state. It is fired just before the app enters the background.
-        Public Event OnBackgroundEntering As EventHandler(Of OnBackgroundEnteringEventArgs)
-
+        ' This method saves the application state before entering background. It fires the event OnBackgroundEntering to collect
+        ' state data from the current subscriber and saves it the local storage.
         Public Async Function SaveStateAsync() As Task
             Dim suspensionState = New SuspensionState() With {
                 .SuspensionDate = DateTime.Now
@@ -32,16 +37,22 @@ Namespace Services
             Await ApplicationData.Current.LocalFolder.SaveAsync(StateFilename, onBackgroundEnteringArgs)
         End Function
 
+        ' This method allows subscribers to refesh data that might be outdated after App is resumed from suspension
+        ' If the App was terminated during suspension this event will not fire, data restore is handled by method HandleInternalAsync
+        Public Sub ResumeApp()
+            RaiseEvent OnResuming(Me, EventArgs.Empty)         
+        End Sub
+
+
+       ' This method restores state when the App is launched after termination, it navigates to the stored Page passing the recovered state data
         Protected Overrides Async Function HandleInternalAsync(args As LaunchActivatedEventArgs) As Task
-            Await RestoreStateAsync()
+            Dim saveState = Await ApplicationData.Current.LocalFolder.ReadAsync(Of OnBackgroundEnteringEventArgs)(StateFilename)
         End Function
 
         Protected Overrides Function CanHandleInternal(args As LaunchActivatedEventArgs) As Boolean
+            ' State has only to be restored if the App was terminated during suspension, no App data should be lost in this case
             Return args.PreviousExecutionState = ApplicationExecutionState.Terminated
         End Function
 
-        Private Async Function RestoreStateAsync() As Task
-            Dim saveState = Await ApplicationData.Current.LocalFolder.ReadAsync(Of OnBackgroundEnteringEventArgs)(StateFilename)
-        End Function
     End Class
 End Namespace
