c58c1b0a2d5fa25becf4ff93dc8ed6146b98adc4
Martin Vega <mvegaca@outlook.com>
2018-11-29 18:06:26 +0100

    stylecop fixes

--- templates/Uwp/_composition/MVVMLight/Page.ContentGridVB/ViewModels/wts.ItemNameViewModel_postaction.vb
+++ templates/Uwp/_composition/MVVMLight/Page.ContentGridVB/ViewModels/wts.ItemNameViewModel_postaction.vb
@@ -6,12 +6,12 @@ Namespace ViewModels
     Public Class wts.ItemNameViewModel
         Inherits ViewModelBase
 '{[{
+
         Public ReadOnly Property NavigationService As NavigationServiceEx
             Get
                 Return CommonServiceLocator.ServiceLocator.Current.GetInstance(Of NavigationServiceEx)()
             End Get
         End Property
-
 '}]}
 
         Private Sub OnsItemSelected(args As ItemClickEventArgs)
