fcde54690ef0b1845f4af56d91234518db570e28
Martin Vega <mvegaca@outlook.com>
2018-11-29 13:09:05 +0100

    Hotfix commit for Issue #2774

--- templates/Uwp/_composition/CaliburnMicro/Project.SplitView/Views/ShellPage.xaml.cs
+++ templates/Uwp/_composition/CaliburnMicro/Project.SplitView/Views/ShellPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using wts.ItemName.ViewModels;
 using Caliburn.Micro;
 using Microsoft.UI.Xaml.Controls;
+using WinUI = Microsoft.UI.Xaml.Controls;
 
 namespace wts.ItemName.Views
 {
@@ -24,6 +25,13 @@ namespace wts.ItemName.Views
         public NavigationView GetNavigationView()
         {
             return navigationView;
+        }
+
+        private void OnItemInvoked(WinUI.NavigationView sender, WinUI.NavigationViewItemInvokedEventArgs args)
+        {
+            // Workaround for Issue https://github.com/Microsoft/WindowsTemplateStudio/issues/2774
+            // Using EventTriggerBehavior does not work on WinUI NavigationView ItemInvoked event in Release mode.
+            ViewModel.OnItemInvoked(args);
         }
     }
 }
--- templates/Uwp/_composition/MVVMLight/Project.SplitView/Views/ShellPage.xaml.cs
+++ templates/Uwp/_composition/MVVMLight/Project.SplitView/Views/ShellPage.xaml.cs
@@ -3,6 +3,7 @@ using wts.ItemName.Services;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using wts.ItemName.ViewModels;
+using WinUI = Microsoft.UI.Xaml.Controls;
 
 namespace wts.ItemName.Views
 {
@@ -19,6 +20,13 @@ namespace wts.ItemName.Views
             InitializeComponent();
             DataContext = ViewModel;
             ViewModel.Initialize(shellFrame, navigationView, KeyboardAccelerators);
+        }
+
+        private void OnItemInvoked(WinUI.NavigationView sender, WinUI.NavigationViewItemInvokedEventArgs args)
+        {
+            // Workaround for Issue https://github.com/Microsoft/WindowsTemplateStudio/issues/2774
+            // Using EventTriggerBehavior does not work on WinUI NavigationView ItemInvoked event in Release mode.
+            ViewModel.ItemInvokedCommand.Execute(args);
         }
     }
 }
