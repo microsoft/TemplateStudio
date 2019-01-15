e9d6c802d7829e5848d9e9b5265c511e793a3463
David Gómez <v-davgom@microsoft.com>
2019-01-15 18:11:08 +0100

    Add help messages resources file to localization tool

--- code/tools/Localization/Program.cs
+++ code/tools/Localization/Program.cs
@@ -6,14 +6,12 @@ using System;
 using System.Linq;
 using CommandLine;
 using Localization.Options;
+using Localization.Resources;
 
 namespace Localization
 {
     public class Program
     {
-        private const string Separator = "**********************************************************************";
-        private const string ArgumentNewLine = "\r\n\t\t\t\t   ";
-
         [STAThread]
         public static void Main(string[] args)
         {
@@ -84,73 +82,31 @@ namespace Localization
 
         private static void PrintHelp(string verb)
         {
+            Console.WriteLine();
+
             if (string.IsNullOrEmpty(verb))
             {
-                Console.WriteLine("For more information on a specific command, type HELP command-name");
-                Console.WriteLine("EXT\tExtract localizable items for different cultures.");
-                Console.WriteLine("GEN\tGenerates Project Templates for different cultures.");
-                Console.WriteLine("VERIFY\tVerify if exist localizable items for different cultures.");
-                Console.WriteLine("HELP\tProvides Help information for Windows Template Studio Localization Tool.");
-                Console.WriteLine();
+                Console.WriteLine(HelpMessages.Info);
+                return;
             }
-            else
+
+            switch (verb.ToUpperInvariant())
             {
-                switch (verb.ToUpperInvariant())
-                {
-                    case "EXT":
-                        Console.WriteLine("Extract localizable items for different cultures.");
-                        Console.WriteLine();
-                        Console.WriteLine("Localization ext -o \"original_WTS_folder\" -a \"actual_WTS_folder\" -d \"destinationDirectory\"");
-                        Console.WriteLine();
-                        Console.WriteLine($"\toriginal_WTS_folder\t - path to the folder that contains{ArgumentNewLine}old version of WTS to compare");
-                        Console.WriteLine();
-                        Console.WriteLine($"\tactual_WTS_folder\t - path to the folder that contains{ArgumentNewLine}actual version of WTS to compare");
-                        Console.WriteLine();
-                        Console.WriteLine($"\tdestinationDirectory\t - path to the folder in which will be{ArgumentNewLine}saved all extracted items.");
-                        Console.WriteLine();
-                        Console.WriteLine("Example:");
-                        Console.WriteLine();
-                        Console.WriteLine("\tLocalization ext -o \"C:\\Projects\\wts_old\" - a \"C:\\Projects\\wts\" - d \"C:\\MyFolder\\Extracted\"");
-                        Console.WriteLine();
-                        break;
-                    case "GEN":
-                        Console.WriteLine("Generates Project Templates for different cultures.");
-                        Console.WriteLine();
-                        Console.WriteLine("Localization gen -s \"sourceDirectory\" -d \"destinationDirectory\"");
-                        Console.WriteLine();
-                        Console.WriteLine($"\tsourceDirectory\t\t - path to the folder that contains{ArgumentNewLine}source files for Project Templates{ArgumentNewLine}(it's root project folder).");
-                        Console.WriteLine();
-                        Console.WriteLine($"\tdestinationDirectory\t - path to the folder in which will be{ArgumentNewLine}saved all localized Project{ArgumentNewLine}Templates (parent for CSharp.UWP.{ArgumentNewLine}2017.Solution directory).");
-                        Console.WriteLine();
-                        Console.WriteLine("Example:");
-                        Console.WriteLine();
-                        Console.WriteLine("\tLocalization gen -s \"C:\\MyFolder\\wts\" -d \"C:\\MyFolder\\Generated\\ProjectTemplates\"");
-                        Console.WriteLine();
-                        break;
-                    case "VERIFY":
-                        Console.WriteLine("Verify if exist localizable items for different cultures.");
-                        Console.WriteLine();
-                        Console.WriteLine("Localization verify -s \"sourceDirectory\"");
-                        Console.WriteLine();
-                        Console.WriteLine($"\tsourceDirectory\t\t - path to the folder that contains{ArgumentNewLine}source files for verify{ArgumentNewLine}(it's root project folder).");
-                        Console.WriteLine();
-                        Console.WriteLine("Example:");
-                        Console.WriteLine();
-                        Console.WriteLine("\tLocalization verify -s \"C:\\MyFolder\\wts\"");
-                        Console.WriteLine();
-                        break;
-                    case "HELP":
-                        Console.WriteLine("Provides Help information for Windows Template Studio Localization Tool.");
-                        Console.WriteLine();
-                        Console.WriteLine("Localization help [command]");
-                        Console.WriteLine();
-                        Console.WriteLine("\tcommand - displays help information on that command.");
-                        Console.WriteLine();
-                        break;
-                    default:
-                        Console.WriteLine("Command unknown.");
-                        break;
-                }
+                case "EXT":
+                    Console.WriteLine(HelpMessages.ExtCommand);
+                    break;
+                case "GEN":
+                    Console.WriteLine(HelpMessages.GenCommand);
+                    break;
+                case "VERIFY":
+                    Console.WriteLine(HelpMessages.VerifyCommand);
+                    break;
+                case "HELP":
+                    Console.WriteLine(HelpMessages.HelpCommand);
+                    break;
+                default:
+                    Console.WriteLine(HelpMessages.UnknownCommand);
+                    break;
             }
         }
 
