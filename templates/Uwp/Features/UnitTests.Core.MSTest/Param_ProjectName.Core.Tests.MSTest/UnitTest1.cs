0034f60af59993c9a9c6d95da495d6926e88d19a
Matt Lacey <matt@mrlacey.co.uk>
2019-01-15 15:21:56 +0000

    Add MSTest project for Core library
    For #299

---
+++ templates/Uwp/Features/UnitTests.Core.MSTest/Param_ProjectName.Core.Tests.MSTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Param_ProjectName.Core.Tests.MSTest
+{
+    // TODO WTS: Add appropriate unit tests 
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
