49f339979a085066efb9f06992155701fc30154f
Matt Lacey <matt@mrlacey.co.uk>
2019-01-29 09:54:16 +0000

    Add basic template for testing app with xUnit
    For #299

---
+++ templates/Uwp/Features/UnitTests.App.xUnit/Param_ProjectName.Tests.xUnit/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Xunit;
+
+namespace Param_ProjectName.Tests.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
