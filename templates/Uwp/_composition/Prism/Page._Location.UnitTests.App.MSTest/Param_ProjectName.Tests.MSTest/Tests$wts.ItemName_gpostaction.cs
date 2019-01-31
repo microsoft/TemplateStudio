283dabad19c0e4f9567cf9c45fa6ac45f5047113
Matt Lacey <matt@mrlacey.co.uk>
2019-01-31 16:10:44 +0000

    Add Prism ViewModel tests

--- templates/Uwp/_composition/Prism/Page._Location.UnitTests.App.MSTest/Param_ProjectName.Tests.MSTest/Tests$wts.ItemName_gpostaction.cs
+++ templates/Uwp/_composition/Prism/Page._Location.UnitTests.App.MSTest/Param_ProjectName.Tests.MSTest/Tests$wts.ItemName_gpostaction.cs
@@ -4,7 +4,7 @@ using Param_RootNamespace.Core.Services;
 using Moq;
 //}]}
 
-namespace Param_RootNamespace.Tests.XUnit
+namespace Param_RootNamespace.Tests.MSTest
 {
     public class Tests
     {
@@ -12,13 +12,13 @@ namespace Param_RootNamespace.Tests.XUnit
         //{[{
 
         // TODO WTS: Add tests for functionality you add to wts.ItemNameViewModel.
-        [Fact]
+        [TestMethod]
         public void Testwts.ItemNameViewModelCreation()
         {
             // This test is trivial. Add your own tests for the logic you add to the ViewModel.
-            var mockDataService = new Mock<ISampleDataService>();
-            var vm = new ChartViewModel(mockDataService.Object);
-            Assert.NotNull(vm);
+            var mockLocationService = new Mock<ILocationService>();
+            var vm = new wts.ItemNameViewModel(mockLocationService.Object);
+            Assert.IsNotNull(vm);
         }
         //}]}
     }
---
+++ templates/Uwp/_composition/Prism/Page._NoParams.UnitTests.App.xUnit/Param_ProjectName.Tests.xUnit/Tests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,24 @@
+﻿//{[{
+using Param_RootNamespace.ViewModels;
+using Param_RootNamespace.Core.Services;
+using Moq;
+//}]}
+
+namespace Param_RootNamespace.Tests.XUnit
+{
+    public class Tests
+    {
+        //^^
+        //{[{
+
+        // TODO WTS: Add tests for functionality you add to wts.ItemNameViewModel.
+        [Fact]
+        public void Testwts.ItemNameViewModelCreation()
+        {
+            // This test is trivial. Add your own tests for the logic you add to the ViewModel.
+            var vm = new wts.ItemNameViewModel();
+            Assert.NotNull(vm);
+        }
+        //}]}
+    }
+}
