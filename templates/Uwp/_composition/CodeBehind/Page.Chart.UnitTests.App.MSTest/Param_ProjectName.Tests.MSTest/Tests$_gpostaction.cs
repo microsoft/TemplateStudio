8fe922c60bab63bb10670cd24db3f94e19201802
Matt Lacey <matt@mrlacey.co.uk>
2019-01-31 01:24:31 +0000

    Add C# codebehind tests for chart page

---
+++ templates/Uwp/_composition/CodeBehind/Page.Chart.UnitTests.App.MSTest/Param_ProjectName.Tests.MSTest/Tests$_gpostaction.cs
@@ -0,0 +1,23 @@
+﻿//{[{
+using Param_RootNamespace.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+//}]}
+
+namespace Param_RootNamespace.Tests.MSTest
+{
+    public class Tests
+    {
+        //^^
+        //{[{
+
+        // TODO WTS: Add tests for functionality you add to wts.ItemNamePage.
+        [UITestMethod]
+        public void Testwts.ItemNamePageCreation()
+        {
+            // This test is trivial. Add your own tests for the logic you add to the code-behind file for the page.
+            var view = new wts.ItemNamePage();
+            Assert.IsNotNull(view);
+        }
+        //}]}
+    }
+}
---
+++ templates/Uwp/_composition/CodeBehind/Page.Chart.UnitTests.App.xUnit/Param_ProjectName.Tests.xUnit/Tests$_gpostaction.cs
@@ -0,0 +1,29 @@
+﻿//{[{
+using System.Threading.Tasks;
+using Param_RootNamespace.Views;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+//}]}
+
+namespace Param_RootNamespace.Tests.XUnit
+{
+    public class Tests
+    {
+        //^^
+        //{[{
+
+        // TODO WTS: Add tests for functionality you add to wts.ItemNamePage.
+        [Fact]
+        public async Task Testwts.ItemNamePageCreation()
+        {
+            // Creating pages requires the UI thread so execute the test there.
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // This test is trivial. Add your own tests for the logic you add to the code-behind file for the page.
+                var view = new wts.ItemNamePage();
+                Assert.NotNull(view);
+            });
+        }
+        //}]}
+    }
+}
