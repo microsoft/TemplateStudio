c9eb757edc342ca48c0068792e64505386172609
Matt Lacey <matt@mrlacey.co.uk>
2019-02-02 12:25:15 +0000

    Add VB versions of App ViewModel tests

---
+++ templates/Uwp/_composition/_shared/Page.UnitTests.App.xUnitVB/Param_ProjectName.Tests.xUnit/Tests$wts.ItemName_gpostaction.vb
@@ -0,0 +1,17 @@
+﻿'{[{
+Imports Param_RootNamespace.ViewModels
+'}]}
+
+Public Class Tests
+    '^^
+    '{[{
+
+    ' TODO WTS: Add tests for functionality you add to wts.ItemNameViewModel.
+    <Fact>
+    Public Sub Testwts.ItemNameViewModelCreation()
+        ' This test is trivial. Add your own tests for the logic you add to the ViewModel.
+        Dim vm = new wts.ItemNameViewModel()
+        Assert.NotNull(vm)
+    End Sub
+    '}]}
+End Class
