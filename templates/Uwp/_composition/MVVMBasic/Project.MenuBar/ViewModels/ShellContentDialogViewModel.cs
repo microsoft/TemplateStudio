1a8e30e9d2d440218711f5a407ac7aff19d206d8
Martin Vega <mvegaca@outlook.com>
2019-02-20 16:13:44 +0100

    MVVM Basic templates for menu bar

---
+++ templates/Uwp/_composition/MVVMBasic/Project.MenuBar/ViewModels/ShellContentDialogViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using Param_RootNamespace.Helpers;
+
+namespace Param_RootNamespace.ViewModels
+{
+    public class ShellContentDialogViewModel : Observable
+    {
+        public ShellContentDialogViewModel()
+        {
+        }
+    }
+}
