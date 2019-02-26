f5225ddf6fbb2211f2c9c2db7dcc11b21c3059f2
sibille <sibille@ilitia.com>
2019-02-26 21:43:22 +0100

    back to 2.4.1

--- _build/Execute-LocalTestsInParallel.ps1
+++ _build/Execute-LocalTestsInParallel.ps1
@@ -2,7 +2,7 @@
 $rootPath = (Split-Path $PSScriptRoot)
 
 $scriptPath = Join-Path $rootPath \_build\ParallelTestExecution.ps1
-$testrunnerPath = Join-Path $rootPath \Code\packages\xunit.runner.console.2.2.0\tools\xunit.console.exe
+$testrunnerPath = Join-Path $env:userprofile \.nuget\packages\xunit.runner.console\2.4.1\tools\net47\xunit.console.exe
 $templateTestLibraryPath = Join-Path $rootPath \Code\test\Templates.Test\bin\Analyze\Microsoft.Templates.Test.dll
 $coreTestLibraryPath = Join-Path $rootPath \Code\test\Core.Test\bin\Analyze\Microsoft.Templates.Core.Test.dll 
 $uiTestLibraryPath = Join-Path $rootPath \Code\test\UI.Test\bin\Analyze\Microsoft.UI.Test.dll 
