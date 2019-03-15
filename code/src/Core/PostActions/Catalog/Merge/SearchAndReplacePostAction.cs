f72b2be8a184b4442dfde8dc19c03d4878816d21
sibille <sibille@ilitia.com>
2019-03-15 14:06:27 +0100

    Remove using

--- code/src/Core/PostActions/Catalog/Merge/SearchAndReplacePostAction.cs
+++ code/src/Core/PostActions/Catalog/Merge/SearchAndReplacePostAction.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 using Microsoft.Templates.Core.Extensions;
