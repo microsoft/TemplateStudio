dae9a659162065cd14da5b503ff10c27c63b6d03
Martin Vega <mvegaca@outlook.com>
2019-03-21 09:01:51 +0100

    Fix identity composition template

--- templates/Uwp/Features/IdentityForcedLogin/ViewModels/Param_SettingsPageNameViewModel_postaction.cs
+++ templates/Uwp/Features/IdentityForcedLogin/ViewModels/Param_SettingsPageNameViewModel_postaction.cs
@@ -55,6 +55,5 @@ namespace Param_RootNamespace.ViewModels
             await IdentityService.LogoutAsync();
         }
 //}]}
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
