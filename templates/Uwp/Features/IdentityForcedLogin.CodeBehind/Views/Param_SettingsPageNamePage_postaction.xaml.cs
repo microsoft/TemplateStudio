e4b2ab72eb7877cc3932ac43a2afab4af806bb84
Martin Vega <mvegaca@outlook.com>
2019-03-21 09:09:51 +0100

    Forced Login for CodeBehind Blank projects

---
+++ templates/Uwp/Features/IdentityForcedLogin.CodeBehind/Views/Param_SettingsPageNamePage_postaction.xaml.cs
@@ -0,0 +1,54 @@
+﻿//{[{
+using Param_RootNamespace.Core.Helpers;
+using Param_RootNamespace.Core.Services;
+using Param_RootNamespace.Models;
+//}]}
+namespace Param_RootNamespace.Views
+{
+    public sealed partial class Param_SettingsPageNamePage : Page, INotifyPropertyChanged
+    {
+//{[{
+        private UserDataService UserDataService => Singleton<UserDataService>.Instance;
+
+        private IdentityService IdentityService => Singleton<IdentityService>.Instance;
+//}]}
+        private ElementTheme _elementTheme = ThemeSelectorService.Theme;
+//{[{
+        private UserData _user;
+//}]}
+//^^
+//{[{
+        public UserData User
+        {
+            get { return _user; }
+            set { Set(ref _user, value); }
+        }
+//}]}
+
+        public Param_SettingsPageNamePage()
+        {
+        }
+
+        private async Task InitializeAsync()
+        {
+//^^
+//{[{
+            User = await UserDataService.GetUserFromCacheAsync();
+            User = await UserDataService.GetUserFromGraphApiAsync();
+            if (User == null)
+            {
+                User = UserDataService.GetDefaultUserData();
+            }
+//}]}
+            await Task.CompletedTask;
+        }
+//^^
+//{[{
+        private async void OnLogout(object sender, RoutedEventArgs e)
+        {
+            await IdentityService.LogoutAsync();
+        }
+//}]}
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
