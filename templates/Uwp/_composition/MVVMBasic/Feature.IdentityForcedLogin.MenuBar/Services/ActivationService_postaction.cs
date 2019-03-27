832881287bc39e9c48c9b000c36fcd2d1db78326
Martin Vega <mvegaca@outlook.com>
2019-03-27 12:30:03 +0100

    Fix ForcedLogin MenuBar MVVMBasic

---
+++ templates/Uwp/_composition/MVVMBasic/Feature.IdentityForcedLogin.MenuBar/Services/ActivationService_postaction.cs
@@ -0,0 +1,14 @@
+﻿namespace Param_RootNamespace.Services
+{
+    internal class ActivationService
+    {
+//^^
+//{[{
+
+        public void SetShell(Lazy<UIElement> shell)
+        {
+            _shell = shell;
+        }
+//}]}
+    }
+}
