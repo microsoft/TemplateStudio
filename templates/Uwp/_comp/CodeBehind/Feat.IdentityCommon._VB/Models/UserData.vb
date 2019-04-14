03f97ac71fe0cb39e38a825f8efafbe6d07625cc
Matt Lacey <matt@mrlacey.co.uk>
2019-04-14 16:45:41 +0100

    CodeBehind.Feature.IdentityCommon.VB

---
+++ templates/Uwp/_comp/CodeBehind/Feat.IdentityCommon._VB/Models/UserData.vb
@@ -0,0 +1,9 @@
+﻿Imports Windows.UI.Xaml.Media.Imaging
+
+Namespace Models
+    Public Class UserData
+        Public Property Name As String
+        Public Property UserPrincipalName As String
+        Public Property Photo As BitmapImage
+    End Class
+End Namespace
