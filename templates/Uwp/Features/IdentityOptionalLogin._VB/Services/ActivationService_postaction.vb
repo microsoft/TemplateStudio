bd866d4c307214df64a7c833b8b2ec390b6a923a
Matt Lacey <matt@mrlacey.co.uk>
2019-04-14 16:36:00 +0100

    IdentityOptionalLogin

---
+++ templates/Uwp/Features/IdentityOptionalLogin._VB/Services/ActivationService_postaction.vb
@@ -0,0 +1,35 @@
+﻿'{[{
+Imports Param_RootNamespace.Core.Helpers
+Imports Param_RootNamespace.Core.Services
+'}]}
+
+Namespace Services
+    Friend Class ActivationService
+        Private _lastActivationArgs As Object
+'{[{
+        Private ReadOnly Property IdentityService As IdentityService
+            Get
+                Return Singleton(Of IdentityService).Instance
+            End Get
+        End Property
+
+        Private ReadOnly Property UserDataService As UserDataService
+            Get
+                Return Singleton(Of UserDataService).Instance
+            End Get
+        End Property
+'}]}
+
+        Public Async Function ActivateAsync(activationArgs As Object) As Task
+            If IsInteractive(activationArgs) Then
+                Await InitializeAsync()
+'{[{
+                UserDataService.Initialize()
+                IdentityService.InitializeWithAadAndPersonalMsAccounts()
+                Await IdentityService.AcquireTokenSilentAsync()
+'}]}
+            End If
+
+        End Function
+    End Class
+End Namespace
---
+++ templates/Uwp/Features/IdentityOptionalLogin._VB/ViewModels/SettingsViewModel$CleanInitializeAsync_gpostaction.vb
@@ -0,0 +1,11 @@
+﻿Namespace ViewModels
+    Public Class SettingsViewModel
+        Implements System.ComponentModel.INotifyPropertyChanged
+
+        Private Async Function InitializeAsync() As Task
+'{--{
+            Await Task.CompletedTask
+'}--}
+        End Function
+    End Class
+End Namespace
