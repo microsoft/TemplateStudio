3ec4b1ab8874975b91d7598fecf1b2df11a1452c
Matt Lacey <matt@mrlacey.co.uk>
2019-04-15 17:16:39 +0100

    MVVMLight.Feature.IdentityForcedLogin.VB

---
+++ templates/Uwp/_comp/MVVMLight/Feat.IdentityForcedLogin._VB/ViewModels/LogInViewModel_postaction.vb
@@ -0,0 +1,21 @@
+﻿'{[{
+Imports GalaSoft.MvvmLight
+Imports GalaSoft.MvvmLight.Command
+'}]}
+Namespace ViewModels
+    Public Class LogInViewModel
+        Inherits ViewModelBase
+
+        Public Property IsBusy As Boolean
+            Get
+                Return _isBusy
+            End Get
+            Set(value As Boolean)
+                [Set](_isBusy, value)
+'{[{
+                LoginCommand.RaiseCanExecuteChanged()
+'}]}
+            End Set
+        End Property
+    End Class
+End Namespace
