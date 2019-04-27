9d4eb3ae0484cf05a79107a36ca568f2f88c2fe4
Matt Lacey <matt@mrlacey.co.uk>
2019-04-27 15:41:07 +0100

    test fixes

--- templates/Uwp/_comp/Prism/Feat.UnitTests.Core.SampleData.ImageGallery.xUnit/Param_ProjectName.Core.Tests.xUnit/Tests_postaction.cs
+++ templates/Uwp/_comp/Prism/Feat.UnitTests.Core.SampleData.ImageGallery.xUnit/Param_ProjectName.Core.Tests.xUnit/Tests_postaction.cs
@@ -12,10 +12,10 @@ namespace Param_RootNamespace.Core.Tests.XUnit
         // TODO WTS: Remove or update this once your app is using real data and not the SampleDataService.
         // This test serves only as a demonstration of testing functionality in the Core library.
         [Fact]
-        public void EnsureSampleDataServiceReturnsGalleryData()
+        public async void EnsureSampleDataServiceReturnsGalleryDataAsync()
         {
             var dataService = new SampleDataService();
-            var actual = dataService.GetGallerySampleDataAsync();
+            var actual = await dataService.GetGallerySampleDataAsync();
 
             Assert.NotEmpty(actual);
         }
