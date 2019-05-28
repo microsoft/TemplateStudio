4e21fbaed7abb2dd1ad4cb79442ba52ef389ca88
Matt Lacey <matt@mrlacey.co.uk>
2019-05-28 12:38:03 +0100

    fix encoding issue of £ sign

--- code/test/Templates.Test/BaseGenAndBuildTests.cs
+++ code/test/Templates.Test/BaseGenAndBuildTests.cs
@@ -43,7 +43,7 @@ namespace Microsoft.Templates.Test
             // $ is technically valid in a project name but cannot be used with WTS as it is used as an identifier in global post action file names.
             // ^ is technically valid in project names but Visual Studio cannot open files with this in the path
             // ' is technically valid in project names but breaks test projects if used in the name so don't test for it
-            return " -_.,@! (�)+=";
+            return " -_.,@! (£)+=";
         }
 
         protected static string ShortProjectType(string projectType)
