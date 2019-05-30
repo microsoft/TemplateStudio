2454094d1444c89db0fddf00090c81e92fdc34f2
Martin Vega <mvegaca@outlook.com>
2019-05-30 14:48:57 +0200

    VB remove Exit Function from ActivationSercice GetActivationHandlers

--- templates/Uwp/_comp/_shared/Feat.ShareTarget._VB/Services/ActivationService_postaction.vb
+++ templates/Uwp/_comp/_shared/Feat.ShareTarget._VB/Services/ActivationService_postaction.vb
@@ -5,22 +5,19 @@ Imports Param_RootNamespace.Core.Helpers
 Namespace Services
     Friend Class ActivationService
         Private Iterator Function GetActivationHandlers() As IEnumerable(Of ActivationHandler)
-            '{[{
+'{[{
             yield Singleton(Of wts.ItemNameActivationHandler).Instance
-            '}]}
-'{--{
-            Exit Function
-'}--}
+'}]}
         End Function
 
-        '^^
-        '{[{
+'^^
+'{[{
         Friend Async Function ActivateFromShareTargetAsync(activationArgs As ShareTargetActivatedEventArgs) As Task
             Dim shareTargetHandler = GetActivationHandlers().FirstOrDefault(Function(h) h.CanHandle(activationArgs))
             If shareTargetHandler IsNot Nothing Then
                 Await shareTargetHandler.HandleAsync(activationArgs)
             End If
         End Function
-        '}]}
+'}]}
     End Class
 End Namespace
