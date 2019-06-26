018483a16cc9eafd6e9f2a2e2ddd11e70a77765c
Martin Vega <mvegaca@outlook.com>
2019-06-26 15:11:49 +0200

    Add UnitTest poractions for TreeView

---
+++ templates/Uwp/_comp/_shared/Test.UnitTests.Core.SampleData.TreeView.xUnit/Param_ProjectName.Core.Tests.xUnit/Tests_postaction.cs
@@ -0,0 +1,24 @@
+﻿//{[{
+using System.Threading.Tasks;
+using Param_RootNamespace.Core.Services;
+//}]}
+
+namespace Param_RootNamespace.Core.Tests.XUnit
+{
+    public class Tests
+    {
+        //^^
+        //{[{
+
+        // TODO WTS: Remove or update this once your app is using real data and not the SampleDataService.
+        // This test serves only as a demonstration of testing functionality in the Core library.
+        [Fact]
+        public async void EnsureSampleDataServiceReturnsTreeViewDataAsync()
+        {
+            var actual = await SampleDataService.GetTreeViewDataAsync();
+
+            Assert.NotEmpty(actual);
+        }
+        //}]}
+    }
+}
