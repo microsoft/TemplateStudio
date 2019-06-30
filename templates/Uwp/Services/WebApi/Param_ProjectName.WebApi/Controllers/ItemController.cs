fdfa60f8eddb300ca834f2eed7bd340a8c3f71ad
Matt Lacey <matt@mrlacey.co.uk>
2019-06-30 19:09:20 +0100

    Have Controller use SampleOrders

--- templates/Uwp/Services/WebApi/Param_ProjectName.WebApi/Controllers/ItemController.cs
+++ templates/Uwp/Services/WebApi/Param_ProjectName.WebApi/Controllers/ItemController.cs
@@ -21,7 +21,7 @@ namespace Param_RootNamespace.WebApi.Controllers
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<Item>> List()
+        public ActionResult<IEnumerable<SampleOrder>> List()
         {
             return _itemRepository.GetAll().ToList();
         }
@@ -29,9 +29,9 @@ namespace Param_RootNamespace.WebApi.Controllers
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Item> GetItem(string id)
+        public ActionResult<SampleOrder> GetItem(long id)
         {
-            Item item = _itemRepository.Get(id);
+            var item = _itemRepository.Get(id);
 
             if (item == null)
             {
@@ -44,16 +44,16 @@ namespace Param_RootNamespace.WebApi.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<Item> Create([FromBody] Item item)
+        public ActionResult<SampleOrder> Create([FromBody] SampleOrder item)
         {
             _itemRepository.Add(item);
-            return CreatedAtAction(nameof(GetItem), new { item.Id }, item);
+            return CreatedAtAction(nameof(GetItem), new { item.OrderId }, item);
         }
 
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult Edit([FromBody] Item item)
+        public ActionResult Edit([FromBody] SampleOrder item)
         {
             try
             {
@@ -70,9 +70,9 @@ namespace Param_RootNamespace.WebApi.Controllers
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult Delete(string id)
+        public ActionResult Delete(long id)
         {
-            Item item = _itemRepository.Remove(id);
+            var item = _itemRepository.Remove(id);
 
             if (item == null)
             {
