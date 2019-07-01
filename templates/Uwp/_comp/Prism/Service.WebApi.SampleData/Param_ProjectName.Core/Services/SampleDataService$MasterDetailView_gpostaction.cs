891b6a33f1b33e210dd0a7191ef4dc8e53715213
Matt Lacey <matt@mrlacey.co.uk>
2019-07-01 13:33:34 +0100

    Fix Prism support with WebAPI

---
+++ templates/Uwp/_comp/Prism/Service.WebApi.SampleData/Param_ProjectName.Core/Services/SampleDataService$MasterDetailView_gpostaction.cs
@@ -0,0 +1,21 @@
+﻿//{[{
+using System.Threading.Tasks;
+//}]}
+
+namespace Param_RootNamespace.Core.Services
+{
+    public class SampleDataService : ISampleDataService
+    {
+//^^
+//{[{
+
+        // TODO WTS: Remove this once your Web API is returning real data.
+        public async Task<ObservableCollection<SampleOrder>> GetWebApiSampleDataAsync()
+        {
+            await Task.CompletedTask;
+
+            return new ObservableCollection<SampleOrder>(AllOrders());
+        }
+//}]}
+    }
+}
---
+++ templates/Uwp/_comp/Prism/Service.WebApi.SampleData/Param_ProjectName.WebApi/Models/ItemRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Param_RootNamespace.Core.Models;
+using Param_RootNamespace.Core.Services;
+
+namespace Param_RootNamespace.WebApi.Models
+{
+    // TODO WTS: Replace or update this class when no longer using sample data.
+    public class ItemRepository : IItemRepository
+    {
+        private static Dictionary<long, SampleOrder> items =
+            new Dictionary<long, SampleOrder>();
+
+        public ItemRepository()
+        {
+            Task.Run(async () =>
+            {
+                var dataService = new SampleDataService();
+
+                foreach (var order in await dataService.GetWebApiSampleDataAsync())
+                {
+                    items.TryAdd(order.OrderId, order);
+                }
+            });
+        }
+
+        public IEnumerable<SampleOrder> GetAll()
+        {
+            return items.Values;
+        }
+
+        public void Add(SampleOrder item)
+        {
+            items[item.OrderId] = item;
+        }
+
+        public SampleOrder Get(long id)
+        {
+            items.TryGetValue(id, out SampleOrder item);
+
+            return item;
+        }
+
+        public SampleOrder Remove(long id)
+        {
+            items.Remove(id, out SampleOrder item);
+
+            return item;
+        }
+
+        public void Update(SampleOrder item)
+        {
+            items[item.OrderId] = item;
+        }
+    }
+}
