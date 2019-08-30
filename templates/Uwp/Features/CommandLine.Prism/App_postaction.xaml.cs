cd034f5597cae8d1b4c11f65dbf62401738acfd5
Matt Lacey <matt@mrlacey.co.uk>
2019-08-30 12:08:20 +0100

    Work around the way Prism handles launching
    Work around the way Prism handles launching and the UWP app model limiting access to cmdLine info

--- templates/Uwp/Features/CommandLine.Prism/App_postaction.xaml.cs
+++ templates/Uwp/Features/CommandLine.Prism/App_postaction.xaml.cs
@@ -8,22 +8,42 @@ namespace Param_RootNamespace
 {
     public sealed partial class App : PrismUnityApplication
     {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+//{[{
+        // Used to store details between when UWP makes them available and when Prism convention wants to use them.
+        private (string activationPath, string arguments) cmdLineDetails;
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+
+            // Details of the command line launch can only be accessed during App.OnActivated.
+            // Storing the details for use when needed in `OnActivateApplicationAsync`.
+            if (args.Kind == ActivationKind.CommandLineLaunch)
+            {
+                var operation = ((CommandLineActivatedEventArgs)args).Operation;
+                cmdLineDetails = (operation.CurrentDirectoryPath, operation.Arguments);
+            }
+        }
+//}]}
+
         protected override async Task OnActivateApplicationAsync(IActivatedEventArgs args)
         {
 //^^
 //{[{
             // Only handle a commandline launch if arguments are passed.
-            if (args.Kind == ActivationKind.CommandLineLaunch && (((CommandLineActivatedEventArgs)args)?.Operation.Arguments.Any() ?? false) && args.PreviousExecutionState != ApplicationExecutionState.Running)
+            if (args.Kind == ActivationKind.CommandLineLaunch && !string.IsNullOrWhiteSpace(cmdLineDetails.arguments) && args.PreviousExecutionState != ApplicationExecutionState.Running)
             {
-                CommandLineActivatedEventArgs cmdLineArgs = (CommandLineActivatedEventArgs)args;
-                CommandLineActivationOperation operation = cmdLineArgs.Operation;
-
                 // Because these are supplied by the caller, they should be treated as untrustworthy.
-                var cmdLineString = operation.Arguments;
+                var cmdLineString = cmdLineDetails.arguments;
 
                 // The directory where the command-line activation request was made.
                 // This is typically not the install location of the app itself, but could be any arbitrary path.
-                var activationPath = operation.CurrentDirectoryPath;
+                var activationPath = cmdLineDetails.activationPath;
 
                 //// TODO WTS: parse the cmdLineString to determine what to do.
                 //// If the arguments warrant showing a different view on launch, that can be done here.
