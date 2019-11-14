973beab72cce020d097e7a3dc1093c08cb56947d
sibille <sibille@ilitia.com>
2019-11-14 15:23:43 +0100

    Report project related telemetry by category

--- code/tools/WtsTelemetry/WtsTelemetry/Services/QueryService.cs
+++ code/tools/WtsTelemetry/WtsTelemetry/Services/QueryService.cs
@@ -16,6 +16,21 @@ queryTable
 | extend percentage = todouble(items)/toscalar(queryTable | count)*100
 | order by percentage desc
 ";
+        private readonly string DataByCategoryQuery = @"
+let startDatetime = startofmonth(datetime({0}-{1}-01));
+let endDatetime = endofmonth(datetime({0}-{1}-01));
+let queryTable = customEvents
+| where timestamp between(startDatetime .. endDatetime)
+| extend eventName = iif(itemType == 'customEvent',name,'')
+| where eventName == '{2}'
+| where customDimensions.WtsCategory == '{4}';
+queryTable
+| extend itemName = tostring(customDimensions['{3}'])
+| summarize items = sum(itemCount) by itemName
+| extend total = toscalar(queryTable | count)
+| extend percentage = todouble(items)/toscalar(queryTable | count)*100
+| order by percentage desc
+";
 
         private readonly int year;
         private readonly int month;
@@ -26,20 +41,20 @@ queryTable
             this.month = month;
         }
 
-        public string Projects => string.Format(DataQuery, year, month, "WtsProjectGen", "WtsProjectType");
+        public string Projects(string platform) => string.Format(DataByCategoryQuery, year, month, "WtsProjectGen", "WtsProjectType", platform);
 
-        public string Frameworks => string.Format(DataQuery, year, month, "WtsProjectGen", "WtsFramework");
+        public string Frameworks(string platform) => string.Format(DataByCategoryQuery, year, month, "WtsProjectGen", "WtsFramework", platform);
 
-        public string Languages => string.Format(DataQuery, year, month, "WtsProjectGen", "WtsLanguage");
+        public string Pages(string platform) => string.Format(DataByCategoryQuery, year, month, "WtsPageGen", "WtsTemplateName", platform);
 
-        public string Pages => string.Format(DataQuery, year, month, "WtsPageGen", "WtsTemplateName");
+        public string Features(string platform) => string.Format(DataByCategoryQuery, year, month, "WtsFeatureGen", "WtsTemplateName", platform);
 
-        public string Features => string.Format(DataQuery, year, month, "WtsFeatureGen", "WtsTemplateName");
+        public string Services(string platform) => string.Format(DataByCategoryQuery, year, month, "WtsServiceGen", "WtsTemplateName", platform);
 
-        public string Services => string.Format(DataQuery, year, month, "WtsServiceGen", "WtsTemplateName");
-
-        public string Testing => string.Format(DataQuery, year, month, "WtsTestingGen", "WtsTemplateName");
+        public string Testing(string platform) => string.Format(DataByCategoryQuery, year, month, "WtsTestingGen", "WtsTemplateName", platform);
 
         public string EntryPoints => string.Format(DataQuery, year, month, "WtsWizard", "WtsWizardType");
+
+        public string Languages => string.Format(DataQuery, year, month, "WtsProjectGen", "WtsLanguage");
     }
 }
--- code/tools/WtsTelemetry/WtsTelemetry/WtsTelemetry.cs
+++ code/tools/WtsTelemetry/WtsTelemetry/WtsTelemetry.cs
@@ -12,6 +12,10 @@ namespace WtsTelemetry
 {
     public static class WtsTelemetry
     {
+
+        private const string Uwp = "Uwp";
+        private const string Wpf = "Wpf";
+
         // Every minute: 0 * * * * *
         // Every 5 minutes: 0 */5 * * * *
         // Every day: 0 0 0 * * *
@@ -31,26 +35,38 @@ namespace WtsTelemetry
             var stringDate = $"{year}.{month.ToString("D2")}";
 
             log.Info($"WTS: Get Application Insight data from {stringDate}");
-            var projectData = DataService.GetData(queries.Projects);
-            var frameworksData = DataService.GetData(queries.Frameworks);
-            var pagesData = DataService.GetData(queries.Pages);
-            var featuresData = DataService.GetData(queries.Features);
-            var servicesData = DataService.GetData(queries.Services);
-            var testingData = DataService.GetData(queries.Testing);
+            var projectDataUwp = DataService.GetData(queries.Projects(Uwp));
+            var projectDataWpf = DataService.GetData(queries.Projects(Wpf));
+            var frameworksDataUwp = DataService.GetData(queries.Frameworks(Uwp));
+            var frameworksDataWpf = DataService.GetData(queries.Frameworks(Wpf));
+            var pagesDataUwp = DataService.GetData(queries.Pages(Uwp));
+            var pagesDataWpf = DataService.GetData(queries.Pages(Wpf));
+            var featuresDataUwp = DataService.GetData(queries.Features(Uwp));
+            var featuresDataWpf = DataService.GetData(queries.Features(Wpf));
+            var servicesDataUwp = DataService.GetData(queries.Services(Uwp));
+            var servicesDataWpf = DataService.GetData(queries.Services(Wpf));
+            var testingDataUwp = DataService.GetData(queries.Testing(Uwp));
+            var testingDataWpf = DataService.GetData(queries.Testing(Wpf));
             var entryPointData = DataService.GetData(queries.EntryPoints);
             var languageData = DataService.GetData(queries.Languages);
 
             log.Info($"WTS: Create Md File");
             var mdText = new MarkdownBuilder()
                         .AddHeader(year, month)
-                        .AddTable("Project Type", "Project", projectData)
-                        .AddTable("Framework", "Framework Type", frameworksData)
-                        .AddTable("Pages", "Pages", pagesData)
-                        .AddTable("Features", "Features", featuresData)
-                        .AddTable("Services", "Services", servicesData)
-                        .AddTable("Testing", "Testing", testingData)
-                        .AddTable("Windows Template Studio entry point", "Entry point", entryPointData)
-                        .AddTable("Programming languages", "Languages", languageData)
+                        .AddTable("Project Type (Uwp)", "Project", projectDataUwp)
+                        .AddTable("Project Type (Wpf)", "Project", projectDataWpf)
+                        .AddTable("Framework (Uwp)", "Framework Type", frameworksDataUwp)
+                        .AddTable("Framework (Wpf)", "Framework Type", frameworksDataWpf)
+                        .AddTable("Pages (Uwp)", "Pages", pagesDataUwp)
+                        .AddTable("Pages (Wpf)", "Pages", pagesDataWpf)
+                        .AddTable("Features (Uwp)", "Features", featuresDataUwp)
+                        .AddTable("Features (Wpf)", "Features", featuresDataWpf)
+                        .AddTable("Services (Uwp)", "Services", servicesDataUwp)
+                        .AddTable("Services (Wpf)", "Services", servicesDataWpf)
+                        .AddTable("Testing (Uwp)", "Testing", testingDataUwp)
+                        .AddTable("Testing (Wpf)", "Services", testingDataWpf)
+                        .AddTable("Windows Template Studio entry point (Common)", "Entry point", entryPointData)
+                        .AddTable("Programming languages (Common)", "Languages", languageData)
                         .GetText();
 
 
