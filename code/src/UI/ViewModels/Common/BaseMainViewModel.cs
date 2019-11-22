75b731949d10334371c020cb33c8dd99af79fbf4
Martin Vega <mvegaca@outlook.com>
2019-11-22 10:34:11 +0100

    fix using statements order

--- code/src/UI/ViewModels/Common/BaseMainViewModel.cs
+++ code/src/UI/ViewModels/Common/BaseMainViewModel.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Templates.Core;
