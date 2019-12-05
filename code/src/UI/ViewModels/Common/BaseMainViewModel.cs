0b4c7c7752075fc056f4b984d37dc467c6c021c5
sibille <sibille@ilitia.com>
2019-12-05 09:42:46 +0100

    Set Versions once templates are available

--- code/src/UI/ViewModels/Common/BaseMainViewModel.cs
+++ code/src/UI/ViewModels/Common/BaseMainViewModel.cs
@@ -96,8 +96,6 @@ namespace Microsoft.Templates.UI.ViewModels.Common
 
         private async void OnSyncStatusChanged(object sender, SyncStatusEventArgs args)
         {
-            WizardStatus.SetVersions();
-
             var notification = args.GetNotification();
             if (notification?.Category == Category.TemplatesSync)
             {
@@ -115,6 +113,8 @@ namespace Microsoft.Templates.UI.ViewModels.Common
 
             if (args.Status == SyncStatus.Updated || args.Status == SyncStatus.Ready)
             {
+                WizardStatus.SetVersions();
+
                 await OnTemplatesAvailableAsync();
             }
         }
