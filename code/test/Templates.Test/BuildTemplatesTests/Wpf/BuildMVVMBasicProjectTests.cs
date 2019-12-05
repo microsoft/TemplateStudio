36f85429f0ac0063cae279654988fd42f4758aca
sibille <sibille@ilitia.com>
2019-12-05 15:18:41 +0100

    Add msix tests

--- code/test/Templates.Test/BuildTemplatesTests/Wpf/BuildMVVMBasicProjectTests.cs
+++ code/test/Templates.Test/BuildTemplatesTests/Wpf/BuildMVVMBasicProjectTests.cs
@@ -81,6 +81,27 @@ namespace Microsoft.Templates.Test.Wpf
         }
 
         [Theory]
+        [MemberData(nameof(BaseGenAndBuildTests.GetProjectTemplatesForBuild), "MVVMBasic", ProgrammingLanguages.CSharp, Platforms.Wpf)]
+        [Trait("ExecutionSet", "BuildMVVMBasicWpf")]
+        [Trait("ExecutionSet", "_Full")]
+        public async Task Build_AllWithMsix_Wpf(string projectType, string framework, string platform, string language)
+        {
+            Func<ITemplateInfo, bool> templateSelector =
+                t => t.GetTemplateType().IsItemTemplate()
+                && (t.GetProjectTypeList().Contains(projectType) || t.GetProjectTypeList().Contains(All))
+                && (t.GetFrontEndFrameworkList().Contains(framework) || t.GetFrontEndFrameworkList().Contains(All))
+                && t.GetPlatform() == platform
+                && !t.GetIsHidden()
+                || t.Identity == "wts.Wpf.Feat.StyleCop";
+
+            var projectName = $"{projectType}{framework}AllMsix";
+
+            var projectPath = await AssertGenerateProjectAsync(projectName, projectType, framework, platform, language, templateSelector, BaseGenAndBuildFixture.GetDefaultName);
+
+            AssertBuildProjectWpfWithMsixAsync(projectPath, projectName, platform);
+        }
+
+        [Theory]
         [MemberData(nameof(BaseGenAndBuildTests.GetPageAndFeatureTemplatesForBuild), "MVVMBasic", ProgrammingLanguages.CSharp, Platforms.Wpf, "wts.Wpf.Feat.MSIXPackaging")]
         [Trait("ExecutionSet", "BuildOneByOneMVVMBasicWpf")]
         [Trait("ExecutionSet", "_OneByOne")]
