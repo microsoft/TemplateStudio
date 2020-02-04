8c0020ce687acade1e9060fa6055253f7910d661
Martin Vega <mvegaca@outlook.com>
2020-02-04 17:44:52 +0100

    Add MenuItemsEnabledBehavior in SplitView Optional Identity projects

--- templates/Wpf/_comp/_shared/Service.IdentityCommon/Param_ProjectName.Core/Services/IdentityService.cs
+++ templates/Wpf/_comp/_shared/Service.IdentityCommon/Param_ProjectName.Core/Services/IdentityService.cs
@@ -15,7 +15,7 @@ namespace Param_RootNamespace.Core.Services
     public class IdentityService : IIdentityService
     {
         //// For more information about using Identity, see
-        //// https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/UWP/features/identity.md
+        //// https://github.com/microsoft/WindowsTemplateStudio/blob/master/docs/WPF/services/identity.md
         ////
         //// Read more about Microsoft Identity Client here
         //// https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki
--- templates/Wpf/_comp/_shared/Service.IdentityOptionalLogin.MenuBar/Helpers/Restricted.cs
+++ templates/Wpf/_comp/_shared/Service.IdentityOptionalLogin.MenuBar/Helpers/Restricted.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Param_RootNamespace.Helpers
 {
+    // TODO WTS: Add this attribute to a ViewModel class to restrict the Page to authorized users
     internal class Restricted : Attribute
     {
     }
