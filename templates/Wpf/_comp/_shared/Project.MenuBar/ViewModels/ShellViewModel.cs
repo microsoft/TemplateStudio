9a475f8adb2e8d89390795cef0050c1f79974585
sibille <sibille@ilitia.com>
2020-02-11 12:04:40 +0100

    Fix stylecop

--- templates/Wpf/_comp/_shared/Project.MenuBar/ViewModels/ShellViewModel.cs
+++ templates/Wpf/_comp/_shared/Project.MenuBar/ViewModels/ShellViewModel.cs
@@ -6,7 +6,7 @@ using Param_RootNamespace.Strings;
 
 namespace Param_RootNamespace.ViewModels
 {
-    // You can show pages in different ways (update main view, navigate, right pane, new windows or dialog) 
+    // You can show pages in different ways (update main view, navigate, right pane, new windows or dialog)
     // using the NavigationService, RightPaneService and WindowManagerService.
     // Read more about MenuBar project type here:
     // https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/WPF/projectTypes/menubar.md
