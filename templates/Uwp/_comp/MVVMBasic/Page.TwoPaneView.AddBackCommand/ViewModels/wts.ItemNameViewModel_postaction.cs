58ffb8f42325f334a69acd61ba3a3630ef24c698
Martin Vega <mvegaca@outlook.com>
2020-03-18 15:37:45 +0100

    Add TwoPaneView for MVVMBasic WIP

---
+++ templates/Uwp/_comp/MVVMBasic/Page.TwoPaneView.AddBackCommand/ViewModels/wts.ItemNameViewModel_postaction.cs
@@ -0,0 +1,77 @@
+﻿//{[{
+using System.Windows.Input;
+using Param_RootNamespace.Helpers;
+using Param_RootNamespace.Services;
+//}]}
+
+namespace Param_RootNamespace.ViewModels
+{
+    public class wts.ItemNameViewModel : Observable
+    {
+//^^
+//{[{
+        private bool _isGoBackButtonVisible;
+        private ICommand _goBackCommand;
+//}]}
+        private SampleOrder _selected;
+//^^
+//{[{
+        public bool IsGoBackButtonVisible
+        {
+            get { return _isGoBackButtonVisible; }
+            set { Set(ref _isGoBackButtonVisible, value); }
+        }
+//}]}
+
+        public ObservableCollection<SampleOrder> SampleItems { get; private set; } = new ObservableCollection<SampleOrder>();
+//^^
+//{[{
+        public ICommand GoBackCommand => _goBackCommand ?? (_goBackCommand = new RelayCommand(OnGoBack));
+//}]}
+
+        public wts.ItemNameViewModel()
+        {
+        }
+
+        public bool TryCloseDetail()
+        {
+            if (TwoPanePriority == WinUI.TwoPaneViewPriority.Pane2)
+            {
+//^^
+//{[{
+                RefreshIsGoBackButtonVisible();
+//}]}
+                return true;
+            }
+        }
+
+        private void OnItemClick()
+        {
+            if (_twoPaneView.Mode == WinUI.TwoPaneViewMode.SinglePane)
+            {
+//^^
+//{[{
+                RefreshIsGoBackButtonVisible();
+//}]}
+            }
+        }
+
+        private void OnModeChanged(WinUI.TwoPaneView twoPaneView)
+        {
+//^^
+//{[{
+
+            RefreshIsGoBackButtonVisible();
+//}]}
+        }
+
+//^^
+//{[{
+        private void OnGoBack()
+            => TryCloseDetail();
+
+        private void RefreshIsGoBackButtonVisible()
+            => IsGoBackButtonVisible = _twoPaneView.Mode == WinUI.TwoPaneViewMode.SinglePane && TwoPanePriority == WinUI.TwoPaneViewPriority.Pane2;
+//}]}
+    }
+}
