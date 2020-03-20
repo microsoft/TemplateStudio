835b4992aff03af90af3f48b38052973bc7036d7
Martin Vega <mvegaca@outlook.com>
2020-03-20 16:15:25 +0100

    Add TwoPaneView page emplates UWP

---
+++ templates/Uwp/Pages/TwoPaneView.Prism/Param_ProjectName/Views/wts.ItemNamePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Param_RootNamespace.Views
+{
+    public sealed partial class wts.ItemNamePage : Page
+    {
+        public wts.ItemNamePage()
+        {
+            InitializeComponent();
+            ViewModel.Initialize(twoPaneView);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await ViewModel.LoadDataAsync();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            if (e.NavigationMode == NavigationMode.Back && ViewModel.TryCloseDetail())
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
