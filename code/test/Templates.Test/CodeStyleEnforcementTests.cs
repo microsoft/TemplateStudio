d934e2a57462b9dabd3aed3f1b218ff514217c7c
Matt Lacey <matt@mrlacey.co.uk>
2020-06-03 21:37:41 +0100

    improve testing to find porting issues faster

--- code/test/Templates.Test/CodeStyleEnforcementTests.cs
+++ code/test/Templates.Test/CodeStyleEnforcementTests.cs
@@ -166,6 +166,8 @@ namespace Microsoft.Templates.Test
             CheckStringNotIncluded(" -= AddressOf"); // Use RemoveHandler instead
             CheckStringNotIncluded("Param_Setter("); // ParamSetter should be in square brackets
             CheckStringNotIncluded("CSharpImpl"); // Output by converter
+            CheckStringNotIncluded("//"); // C# comment
+            CheckStringNotIncluded("?.Invoke"); //use RaiseEvent instead
 
             IfLineIncludes(" As Task", itMustAlsoInclude: " Async ", unlessItContains: new[] { " MustOverride ", "Function RunAsync(", "Function RunAsyncInternal(", " FireAndForget(", "OnPivotSelectedAsync", "OnPivotUnselectedAsync", "OnPivotActivatedAsync", "TaskCanceledException" });
 
