2cbbe2ca3802b060c849688185aa21decd1fa4e1
sibille <sibille@ilitia.com>
2020-06-22 13:18:13 +0200

    Change master to release in docs and templates

--- templates/Uwp/Pages/InkDraw._VB/Views/InkDrawViewPage.xaml.vb
+++ templates/Uwp/Pages/InkDraw._VB/Views/InkDrawViewPage.xaml.vb
@@ -3,7 +3,7 @@ Imports Windows.UI.Xaml.Controls
 
 Namespace Views
 
-    ' For more information regarding Windows Ink documentation and samples see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/UWP/pages/ink.md
+    ' For more information regarding Windows Ink documentation and samples see https://github.com/Microsoft/WindowsTemplateStudio/blob/release/docs/UWP/pages/ink.md
     Public NotInheritable Partial Class InkDrawViewPage
         Inherits Page
 
--- templates/Wpf/_comp/_shared/Project.MenuBar/ViewModels/ShellViewModel.cs
+++ templates/Wpf/_comp/_shared/Project.MenuBar/ViewModels/ShellViewModel.cs
@@ -9,7 +9,7 @@ namespace Param_RootNamespace.ViewModels
     // You can show pages in different ways (update main view, navigate, right pane, new windows or dialog)
     // using the NavigationService, RightPaneService and WindowManagerService.
     // Read more about MenuBar project type here:
-    // https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/WPF/projectTypes/menubar.md
+    // https://github.com/Microsoft/WindowsTemplateStudio/blob/release/docs/WPF/projectTypes/menubar.md
     public class ShellViewModel : System.ComponentModel.INotifyPropertyChanged
     {
         private readonly INavigationService _navigationService;
