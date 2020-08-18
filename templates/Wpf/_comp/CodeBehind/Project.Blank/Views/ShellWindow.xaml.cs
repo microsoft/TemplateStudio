502cea5a4a346437e25298b31968187053dafe06
Martin Vega <mvegaca@outlook.com>
2020-08-18 17:21:40 +0200

    Add CodeBehind framework with Blank project type WIP

---
+++ templates/Wpf/_comp/CodeBehind/Project.Blank/Views/ShellWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using Param_RootNamespace.Contracts.Services;
+using Param_RootNamespace.Contracts.Views;
+using MahApps.Metro.Controls;
+
+namespace Param_RootNamespace.Views
+{
+    public partial class ShellWindow : MetroWindow, IShellWindow, INotifyPropertyChanged
+    {
+        private readonly INavigationService _navigationService;
+        private bool _canGoBack;
+
+        public bool CanGoBack
+        {
+            get { return _canGoBack; }
+            set { Set(ref _canGoBack, value); }
+        }
+
+        public ShellWindow(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            InitializeComponent();
+        }
+
+        public Frame GetNavigationFrame()
+            => shellFrame;
+
+        public void ShowWindow()
+            => Show();
+
+        public void CloseWindow()
+            => Close();
+
+        private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _navigationService.Navigated += OnNavigated;
+        }
+
+        private void OnUnloaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _navigationService.Navigated -= OnNavigated;
+        }
+
+        private void OnNavigated(object sender, Type pageType)
+        {
+            CanGoBack = _navigationService.CanGoBack;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
