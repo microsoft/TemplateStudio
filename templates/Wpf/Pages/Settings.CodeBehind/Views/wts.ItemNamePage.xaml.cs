4a8488927240226db689ec7374870a1e15ed3599
Martin Vega <mvegaca@outlook.com>
2020-08-20 12:17:09 +0200

    Add settings code behind page

---
+++ templates/Wpf/Pages/Settings.CodeBehind/Views/wts.ItemNamePage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Controls;
+using Microsoft.Extensions.Options;
+using Param_RootNamespace.Contracts.Services;
+using Param_RootNamespace.Contracts.Views;
+using Param_RootNamespace.Models;
+
+namespace Param_RootNamespace.Views
+{
+    public partial class wts.ItemNamePage : Page, INotifyPropertyChanged, INavigationAware
+    {
+        private readonly AppConfig _appConfig;
+        private readonly IThemeSelectorService _themeSelectorService;
+        private readonly ISystemService _systemService;
+        private readonly IApplicationInfoService _applicationInfoService;
+        private bool _isInitialized;
+        private AppTheme _theme;
+        private string _versionDescription;
+
+        public AppTheme Theme
+        {
+            get { return _theme; }
+            set { Set(ref _theme, value); }
+        }
+
+        public string VersionDescription
+        {
+            get { return _versionDescription; }
+            set { Set(ref _versionDescription, value); }
+        }
+
+        public wts.ItemNamePage(IOptions<AppConfig> appConfig, IThemeSelectorService themeSelectorService, ISystemService systemService, IApplicationInfoService applicationInfoService)
+        {
+            _appConfig = appConfig.Value;
+            _themeSelectorService = themeSelectorService;
+            _systemService = systemService;
+            _applicationInfoService = applicationInfoService;
+            InitializeComponent();
+        }
+
+        public void OnNavigatedTo(object parameter)
+        {
+            VersionDescription = $"Param_ProjectName - {_applicationInfoService.GetVersion()}";
+            Theme = _themeSelectorService.GetCurrentTheme();
+            _isInitialized = true;
+        }
+
+        public void OnNavigatedFrom()
+        {
+        }
+
+        private void OnLightChecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_isInitialized)
+            {
+                _themeSelectorService.SetTheme(AppTheme.Light);
+            }
+        }
+
+        private void OnDarkChecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_isInitialized)
+            {
+                _themeSelectorService.SetTheme(AppTheme.Dark);
+            }
+        }
+
+        private void OnPrivacyStatementClick(object sender, System.Windows.RoutedEventArgs e)
+            => _systemService.OpenInWebBrowser(_appConfig.PrivacyStatement);
+    }
+}
