a12d5eccba3da539b33f9c5ddba46e7e3bd6f03d
Martin Vega <mvegaca@outlook.com>
2020-08-21 11:56:25 +0200

    Add ForcedLogin for WPF Code Behind

---
+++ templates/Wpf/Services/IdentityForcedLogin.CodeBehind/Contracts/Views/ILogInWindow.cs
@@ -0,0 +1,9 @@
+﻿namespace Param_RootNamespace.Contracts.Views
+{
+    public interface ILogInWindow
+    {
+        void ShowWindow();
+
+        void CloseWindow();
+    }
+}
---
+++ templates/Wpf/_comp/CodeBehind/Service.IdentityCommon/Contracts/Services/IUserDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Param_RootNamespace.Models;
+
+namespace Param_RootNamespace.Contracts.Services
+{
+    public interface IUserDataService
+    {
+        event EventHandler<UserData> UserDataUpdated;
+
+        void Initialize();
+
+        UserData GetUser();
+    }
+}
