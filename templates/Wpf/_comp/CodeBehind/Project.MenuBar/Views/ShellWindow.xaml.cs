edd85374a8bdc8cf1f28507144c91ab9af53ab28
Martin Vega <mvegaca@outlook.com>
2020-08-26 17:01:17 +0200

    Add wpf code behind menubar support

---
+++ templates/Wpf/_comp/CodeBehind/Project.MenuBar/Views/ShellWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using Param_RootNamespace.Contracts.Services;
+using Param_RootNamespace.Contracts.Views;
+using MahApps.Metro.Controls;
+
+namespace Param_RootNamespace.Views
+{
+    public partial class ShellWindow : MetroWindow, IShellWindow, INotifyPropertyChanged
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IRightPaneService _rightPaneService;
+        private bool _canGoBack;
+
+        public bool CanGoBack
+        {
+            get { return _canGoBack; }
+            set { Set(ref _canGoBack, value); }
+        }
+
+        public ShellWindow(INavigationService navigationService, IRightPaneService rightPaneService)
+        {
+            _navigationService = navigationService;
+            _rightPaneService = rightPaneService;
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public Frame GetNavigationFrame()
+            => shellFrame;
+
+        public Frame GetRightPaneFrame()
+            => rightPaneFrame;
+
+        public void ShowWindow()
+            => Show();
+
+        public void CloseWindow()
+            => Close();
+
+        public SplitView GetSplitView()
+            => splitView;
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigated += OnNavigated;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _navigationService.Navigated -= OnNavigated;
+        }
+
+        private void OnGoBack(object sender, RoutedEventArgs e)
+        {
+            _navigationService.GoBack();
+        }
+
+        private void OnNavigated(object sender, Type pageType)
+        {
+            CanGoBack = _navigationService.CanGoBack;
+        }
+
+        private void OnMenuFileExit(object sender, RoutedEventArgs e)
+            => Application.Current.Shutdown();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ templates/Wpf/_comp/CodeBehind/Service.IdentityOptionalLogin.MenuBar/Views/ShellWindow_postaction.xaml.cs
@@ -0,0 +1,71 @@
+﻿//{[{
+using System.Windows.Controls;
+using Param_RootNamespace.Helpers;
+using Param_RootNamespace.Models;
+using Param_RootNamespace.Core.Contracts.Services;
+using Param_RootNamespace.Core.Helpers;
+//}]}
+namespace Param_RootNamespace.Views
+{
+    public partial class ShellWindow : MetroWindow, IShellWindow, INotifyPropertyChanged
+    {
+        private readonly IRightPaneService _rightPaneService;
+//{[{
+        private readonly IIdentityService _identityService;
+
+        private bool _isLoggedIn;
+        private bool _isAuthorized;
+//}]}
+//^^
+//{[{
+
+        public bool IsLoggedIn
+        {
+            get { return _isLoggedIn; }
+            set { Set(ref _isLoggedIn, value); }
+        }
+
+        public bool IsAuthorized
+        {
+            get { return _isAuthorized; }
+            set { Set(ref _isAuthorized, value); }
+        }
+
+//}]}
+        // TODO WTS: Change the icons and titles for all HamburgerMenuItems here.
+        public ShellWindow(/*{[{*/IIdentityService identityService/*}]}*/)
+        {
+//^^
+//{[{
+            _identityService = identityService;
+            _identityService.LoggedIn += OnLoggedIn;
+            _identityService.LoggedOut += OnLoggedOut;
+//}]}
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+//^^
+//{[{
+            IsLoggedIn = _identityService.IsLoggedIn();
+            IsAuthorized = IsLoggedIn && _identityService.IsAuthorized();
+//}]}
+        }
+//{[{
+
+        private void OnLoggedIn(object sender, EventArgs e)
+        {
+            IsLoggedIn = true;
+            IsAuthorized = IsLoggedIn && _identityService.IsAuthorized();
+        }
+
+        private void OnLoggedOut(object sender, EventArgs e)
+        {
+            IsLoggedIn = false;
+            IsAuthorized = false;
+            _navigationService.CleanNavigation();
+            _navigationService.NavigateTo(typeof(Param_HomeNamePage), null, true);
+        }
+//}]}
+    }
+}
