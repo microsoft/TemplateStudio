02622a61155b4b038db51ad5edf4daa90dcea35e
Martin Vega <mvegaca@outlook.com>
2020-08-27 17:44:49 +0200

    Add WPF WebView Page for Code Behind

---
+++ templates/Wpf/Pages/WebView.CodeBehind/Properties/Resources.Designer_postaction.cs
@@ -0,0 +1,59 @@
+﻿public class Resources {
+//^^
+//{[{
+
+    /// <summary>
+    ///   Looks up a localized string similar to wts.ItemNamePageBackButtonTooltip
+    /// </summary>
+    public static string wts.ItemNamePageBackButtonTooltip {
+        get {
+            return ResourceManager.GetString("wts.ItemNamePageBackButtonTooltip", resourceCulture);
+        }
+    }
+
+    /// <summary>
+    ///   Looks up a localized string similar to wts.ItemNamePageForwardButtonTooltip
+    /// </summary>
+    public static string wts.ItemNamePageForwardButtonTooltip {
+        get {
+            return ResourceManager.GetString("wts.ItemNamePageForwardButtonTooltip", resourceCulture);
+        }
+    }
+
+    /// <summary>
+    ///   Looks up a localized string similar to wts.ItemNamePageLoadingText
+    /// </summary>
+    public static string wts.ItemNamePageLoadingText {
+        get {
+            return ResourceManager.GetString("wts.ItemNamePageLoadingText", resourceCulture);
+        }
+    }
+
+    /// <summary>
+    ///   Looks up a localized string similar to wts.ItemNamePageFaildedMessage
+    /// </summary>
+    public static string wts.ItemNamePageFaildedMessage {
+        get {
+            return ResourceManager.GetString("wts.ItemNamePageFaildedMessage", resourceCulture);
+        }
+    }
+
+    /// <summary>
+    ///   Looks up a localized string similar to wts.ItemNamePageRefreshButtonTooltip
+    /// </summary>
+    public static string wts.ItemNamePageRefreshButtonTooltip {
+        get {
+            return ResourceManager.GetString("wts.ItemNamePageRefreshButtonTooltip", resourceCulture);
+        }
+    }
+
+    /// <summary>
+    ///   Looks up a localized string similar to wts.ItemNamePageOpenInBrowserButtonTooltip
+    /// </summary>
+    public static string wts.ItemNamePageOpenInBrowserButtonTooltip {
+        get {
+            return ResourceManager.GetString("wts.ItemNamePageOpenInBrowserButtonTooltip", resourceCulture);
+        }
+    }
+//}]}
+}
