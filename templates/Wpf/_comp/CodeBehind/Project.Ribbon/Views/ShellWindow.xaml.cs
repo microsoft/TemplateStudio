29059fdbf5a6d0ba8cb85bc49d4df30a150e4fe8
Martin Vega <mvegaca@outlook.com>
2020-08-27 10:12:27 +0200

    Add WPF CodeBehind Ribbon templates

--- templates/Wpf/_comp/CodeBehind/Project.Ribbon/Views/ShellWindow.xaml.cs
+++ templates/Wpf/_comp/CodeBehind/Project.Ribbon/Views/ShellWindow.xaml.cs
@@ -1,41 +1,49 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using Fluent;
+using MahApps.Metro.Controls;
+using Param_RootNamespace.Behaviors;
 using Param_RootNamespace.Contracts.Services;
 using Param_RootNamespace.Contracts.Views;
-using MahApps.Metro.Controls;
 
 namespace Param_RootNamespace.Views
 {
-    public partial class ShellWindow : MetroWindow, IShellWindow, INotifyPropertyChanged
+    public partial class ShellWindow : MetroWindow, IShellWindow, IRibbonWindow, INotifyPropertyChanged
     {
-        private readonly INavigationService _navigationService;
         private readonly IRightPaneService _rightPaneService;
-        private bool _canGoBack;
 
-        public bool CanGoBack
+        public RibbonTitleBar TitleBar
         {
-            get { return _canGoBack; }
-            set { Set(ref _canGoBack, value); }
+            get => (RibbonTitleBar)GetValue(TitleBarProperty);
+            private set => SetValue(TitleBarPropertyKey, value);
         }
 
-        public ShellWindow(INavigationService navigationService, IRightPaneService rightPaneService)
+        private static readonly DependencyPropertyKey TitleBarPropertyKey = DependencyProperty.RegisterReadOnly(nameof(TitleBar), typeof(RibbonTitleBar), typeof(ShellWindow), new PropertyMetadata());
+
+        public static readonly DependencyProperty TitleBarProperty = TitleBarPropertyKey.DependencyProperty;
+
+        public ShellWindow(IServiceProvider serviceProvider, IRightPaneService rightPaneService)
         {
-            _navigationService = navigationService;
             _rightPaneService = rightPaneService;
             InitializeComponent();
+            navigationBehavior.Initialize(serviceProvider);
             DataContext = this;
         }
 
         public Frame GetNavigationFrame()
             => shellFrame;
 
+        public RibbonTabsBehavior GetRibbonTabsBehavior()
+            => tabsBehavior;
+
         public Frame GetRightPaneFrame()
             => rightPaneFrame;
+
+        public SplitView GetSplitView()
+            => splitView;
 
         public void ShowWindow()
             => Show();
@@ -43,30 +51,18 @@ namespace Param_RootNamespace.Views
         public void CloseWindow()
             => Close();
 
-        public SplitView GetSplitView()
-            => splitView;
-
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            var window = sender as MetroWindow;
+            TitleBar = window.FindChild<RibbonTitleBar>("RibbonTitleBar");
+            TitleBar.InvalidateArrange();
+            TitleBar.UpdateLayout();
         }
 
         private void OnUnloaded(object sender, RoutedEventArgs e)
         {
-            _rightPaneService.CleanUp();
+            tabsBehavior.Unsubscribe();
         }
-
-        private void OnGoBack(object sender, RoutedEventArgs e)
-        {
-            _navigationService.GoBack();
-        }
-
-        private void OnNavigated(object sender, Type pageType)
-        {
-            CanGoBack = _navigationService.CanGoBack;
-        }
-
-        private void OnMenuFileExit(object sender, RoutedEventArgs e)
-            => Application.Current.Shutdown();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
