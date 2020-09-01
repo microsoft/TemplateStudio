438d838c27570f91479277d4503074d6256564f2
Martin Vega <mvegaca@outlook.com>
2020-09-01 08:29:04 +0200

    Remove msix dependency

--- templates/Wpf/_comp/MVVMBasic/Feature.ToastNotifications/App_postaction.xaml.cs
+++ templates/Wpf/_comp/MVVMBasic/Feature.ToastNotifications/App_postaction.xaml.cs
@@ -10,21 +10,19 @@ namespace Param_RootNamespace
 {
     public partial class App : Application
     {
-//^^
-//{[{
-        public async Task StartAsync()
-            => await _host.StartAsync();
-//}]}
-
         private async void OnStartup(object sender, StartupEventArgs e)
         {
 //{[{
-            // Read more about sending local toast notifications from desktop C# apps
-            // https://docs.microsoft.com/windows/uwp/design/shell/tiles-and-notifications/send-local-toast-desktop
-            //
-            // Register AUMID, COM server, and activator
-            DesktopNotificationManagerCompat.RegisterAumidAndComServer<ToastNotificationActivator>("Param_ProjectName");
-            DesktopNotificationManagerCompat.RegisterActivator<ToastNotificationActivator>();
+            // https://docs.microsoft.com/windows/uwp/design/shell/tiles-and-notifications/send-local-toast
+            DesktopNotificationManagerCompat.OnActivated += (toastArgs) =>
+            {
+                Current.Dispatcher.Invoke(async () =>
+                {
+                    var config = GetService<IConfiguration>();
+                    config[ToastNotificationActivationHandler.ActivationArguments] = toastArgs.Argument;
+                    await _host.StartAsync();
+                });
+            };
 
             // TODO: Register arguments you want to use on App initialization
             var activationArgs = new Dictionary<string, string>
@@ -44,7 +42,7 @@ namespace Param_RootNamespace
                     .Build();
 //^^
 //{[{
-            if (e.Args.Contains(DesktopNotificationManagerCompat.ToastActivatedLaunchArg))
+            if (DesktopNotificationManagerCompat.WasCurrentProcessToastActivated())
             {
                 // ToastNotificationActivator code will run after this completes and will show a window if necessary.
                 return;
