d8ad22c4a22d7b449ef8da49082b22cfb003ad13
Martin Vega <mvegaca@outlook.com>
2020-09-04 11:18:30 +0200

    Move Exception message from localized resource to code in Settings Page

--- templates/Uwp/Pages/Settings.CodeBehind._VB/Helpers/EnumToBooleanConverter.vb
+++ templates/Uwp/Pages/Settings.CodeBehind._VB/Helpers/EnumToBooleanConverter.vb
@@ -7,7 +7,7 @@
         Public Function Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
             If TypeOf parameter Is String Then
                 If Not [Enum].IsDefined(EnumType, value) Then
-                    Throw New ArgumentException("ExceptionEnumToBooleanConverterValueMustBeAnEnum".GetLocalized())
+                    Throw New ArgumentException("value must be an Enum!")
                 End If
 
                 Dim enumValue = [Enum].Parse(EnumType, parameter.ToString())
@@ -15,7 +15,7 @@
                 Return enumValue.Equals(value)
             End If
 
-            Throw New ArgumentException("ExceptionEnumToBooleanConverterParameterMustBeAnEnumName".GetLocalized())
+            Throw New ArgumentException("parameter must be an Enum name!")
         End Function
 
         Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
@@ -24,7 +24,7 @@
                 Return [Enum].Parse(EnumType, enumString)
             End If
 
-            Throw New ArgumentException("ExceptionEnumToBooleanConverterParameterMustBeAnEnumName".GetLocalized())
+            Throw New ArgumentException("parameter must be an Enum name!")
         End Function
     End Class
 End Namespace
