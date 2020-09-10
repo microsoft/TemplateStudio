e13c828cbd4019fa19704d9d9ed5ba812193def5
Martin Vega <mvegaca@outlook.com>
2020-09-10 16:24:31 +0200

    Working on settings page WIP

---
+++ templates/WinUI/Pages/Settings/ViewModels/wts.ItemNameViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows.Input;
+using Microsoft.UI.Xaml;
+using Windows.ApplicationModel;
+using Param_RootNamespace.Contracts.Services;
+
+namespace Param_RootNamespace.ViewModels
+{
+    public class wts.ItemNameViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        private readonly IThemeSelectorService _themeSelectorService;
+        private ElementTheme _elementTheme;
+
+        public ElementTheme ElementTheme
+        {
+            get { return _elementTheme; }
+
+            set { SetProperty(ref _elementTheme, value); }
+        }
+
+        private string _versionDescription;
+
+        public string VersionDescription
+        {
+            get { return _versionDescription; }
+
+            set { SetProperty(ref _versionDescription, value); }
+        }
+
+        private ICommand _switchThemeCommand;
+
+        public ICommand SwitchThemeCommand
+        {
+            get
+            {
+                if (_switchThemeCommand == null)
+                {
+                    _switchThemeCommand = new RelayCommand<ElementTheme>(
+                        async (param) =>
+                        {
+                            ElementTheme = param;
+                            await _themeSelectorService.SetThemeAsync(param);
+                        });
+                }
+
+                return _switchThemeCommand;
+            }
+        }
+
+        public wts.ItemNameViewModel(IThemeSelectorService themeSelectorService)
+        {
+            _themeSelectorService = themeSelectorService;
+            _elementTheme = _themeSelectorService.Theme;
+            VersionDescription = GetVersionDescription();
+        }
+
+        private string GetVersionDescription()
+        {
+            var appName = "AppDisplayName";
+            var package = Package.Current;
+            var packageId = package.Id;
+            var version = packageId.Version;
+
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+    }
+}
