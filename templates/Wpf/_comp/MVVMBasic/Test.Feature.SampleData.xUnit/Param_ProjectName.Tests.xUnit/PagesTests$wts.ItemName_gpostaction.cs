8ab0e52a6f10bc37c06429bcb8cd5ae52301568d
vmadurga <vmadurga@gmail.com>
2020-10-15 17:53:56 +0200

    MVVMBasic xUnit test pages

---
+++ templates/Wpf/_comp/MVVMBasic/Test.Feature.SampleData.xUnit/Param_ProjectName.Tests.xUnit/PagesTests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,17 @@
+﻿//{[{
+using Param_RootNamespace.Core.Contracts.Services;
+using Param_RootNamespace.Core.Services;
+//}]}
+namespace Param_RootNamespace.Tests.xUnit
+{
+    public class PagesTests
+    {
+        private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            // Services
+//{[{
+            services.AddSingleton<ISampleDataService, SampleDataService>();
+//}]}
+        }
+    }
+}
