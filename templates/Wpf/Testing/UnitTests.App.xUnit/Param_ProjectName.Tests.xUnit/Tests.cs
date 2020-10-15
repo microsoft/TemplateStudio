807186c840a8cc5db78cd6f60d4ce3bc988e0486
vmadurga <vmadurga@gmail.com>
2020-10-15 09:51:26 +0200

    Add App basic tests Mstest and xUnit

---
+++ templates/Wpf/Testing/UnitTests.App.xUnit/Param_ProjectName.Tests.xUnit/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Param_RootNamespace.Tests.xUnit
+{
+    // TODO WTS: Add appropriate unit tests.
+    public class Tests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
