dc197630500a034a1bce3ca0f68698843d775b53
vmadurga <vmadurga@gmail.com>
2020-10-19 14:09:02 +0200

    Add pages test templates NUnit

---
+++ templates/Wpf/_comp/Prism/Test.Feature.SystemService.NUnit/Param_ProjectName.Tests.NUnit/PagesTests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,18 @@
+﻿//{[{
+using Param_RootNamespace.Services;
+using Param_RootNamespace.Contracts.Services;
+//}]}
+namespace Param_RootNamespace.Tests.NUnit
+{
+    public class PagesTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // App Services
+//{[{
+            _container.RegisterType<ISystemService, SystemService>();
+//}]}
+        }
+    }
+}
