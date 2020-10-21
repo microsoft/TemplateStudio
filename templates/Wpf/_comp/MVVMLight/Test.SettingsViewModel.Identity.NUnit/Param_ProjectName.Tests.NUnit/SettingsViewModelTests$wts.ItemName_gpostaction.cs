a48070b9f9d116b8438576bee0890170277d962c
vmadurga <vmadurga@gmail.com>
2020-10-21 14:14:57 +0200

    Fix SettingsViewModel IdentityService

---
+++ templates/Wpf/_comp/MVVMLight/Test.SettingsViewModel.Identity.NUnit/Param_ProjectName.Tests.NUnit/SettingsViewModelTests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,68 @@
+﻿//{[{
+using Param_RootNamespace.Core.Contracts.Services;
+//}]}
+namespace Param_RootNamespace.Tests.NUnit
+{
+    public class SettingsViewModelTests
+    {
+        public SettingsViewModelTests()
+        {
+
+        }
+
+        [Test]
+        public void TestSettingsViewModel_SetCurrentTheme()
+        {
+            var mockThemeSelectorService = new Mock<IThemeSelectorService>();
+            mockThemeSelectorService.Setup(mock => mock.GetCurrentTheme()).Returns(AppTheme.Light);
+            var mockAppConfig = new Mock<AppConfig>();
+            var mockSystemService = new Mock<ISystemService>();
+            var mockApplicationInfoService = new Mock<IApplicationInfoService>();
+//{[{
+            var mockUserDataService = new Mock<IUserDataService>();
+            var mockIdentityService = new Mock<IIdentityService>();
+//}]}
+            var settingsVm = new SettingsViewModel(mockAppConfig.Object, mockThemeSelectorService.Object, mockSystemService.Object, mockApplicationInfoService.Object/*{[{*/, mockUserDataService.Object, mockIdentityService.Object/*}]}*/);
+            settingsVm.OnNavigatedTo(null);
+
+            Assert.AreEqual(AppTheme.Light, settingsVm.Theme);
+        }
+
+        [Test]
+        public void TestSettingsViewModel_SetCurrentVersion()
+        {
+            var mockThemeSelectorService = new Mock<IThemeSelectorService>();
+            var mockAppConfig = new Mock<AppConfig>();
+            var mockSystemService = new Mock<ISystemService>();
+            var mockApplicationInfoService = new Mock<IApplicationInfoService>();
+//{[{
+            var mockUserDataService = new Mock<IUserDataService>();
+            var mockIdentityService = new Mock<IIdentityService>();
+//}]}
+            var testVersion = new Version(1, 2, 3, 4);
+            mockApplicationInfoService.Setup(mock => mock.GetVersion()).Returns(testVersion);
+
+            var settingsVm = new SettingsViewModel(mockAppConfig.Object, mockThemeSelectorService.Object, mockSystemService.Object, mockApplicationInfoService.Object/*{[{*/, mockUserDataService.Object, mockIdentityService.Object/*}]}*/);
+            settingsVm.OnNavigatedTo(null);
+
+            Assert.AreEqual($"Param_RootNamespace - {testVersion}", settingsVm.VersionDescription);
+        }
+
+        [Test]
+        public void TestSettingsViewModel_SetThemeCommand()
+        {
+            var mockThemeSelectorService = new Mock<IThemeSelectorService>();
+            var mockAppConfig = new Mock<AppConfig>();
+            var mockSystemService = new Mock<ISystemService>();
+            var mockApplicationInfoService = new Mock<IApplicationInfoService>();
+//{[{
+            var mockUserDataService = new Mock<IUserDataService>();
+            var mockIdentityService = new Mock<IIdentityService>();
+//}]}
+            var settingsVm = new SettingsViewModel(mockAppConfig.Object, mockThemeSelectorService.Object, mockSystemService.Object, mockApplicationInfoService.Object/*{[{*/, mockUserDataService.Object, mockIdentityService.Object/*}]}*/);
+            settingsVm.SetThemeCommand.Execute(AppTheme.Light.ToString());
+
+            mockThemeSelectorService.Verify(mock => mock.SetTheme(AppTheme.Light));
+        }
+    }
+}
---
+++ templates/Wpf/_comp/Prism/Test.Service.Identity.AppInfoService.xUnit/Param_ProjectName.Tests.xUnit/PagesTests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,37 @@
+﻿//{[{
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Param_RootNamespace.Core.Contracts.Services;
+//}]}
+namespace Param_RootNamespace.Tests.xUnit
+{
+    public class PagesTests
+    {
+        public PagesTests()
+        {
+            // Core Services
+//{[{
+            _container.RegisterType<IIdentityService, IdentityService>();
+            _container.RegisterType<IMicrosoftGraphService, MicrosoftGraphService>();
+//}]}
+            // App Services
+//{[{
+            _container.RegisterType<IUserDataService, UserDataService>();
+            _container.RegisterType<IIdentityCacheService, IdentityCacheService>();
+            _container.RegisterFactory<IHttpClientFactory>(container => GetHttpClientFactory());
+//}]}
+        }
+//{[{
+        private IHttpClientFactory GetHttpClientFactory()
+        {
+            var services = new ServiceCollection();
+            services.AddHttpClient("msgraph", client =>
+            {
+                client.BaseAddress = new System.Uri("https://graph.microsoft.com/v1.0/");
+            });
+
+            return services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>();
+        }
+//}]}
+    }
+}
