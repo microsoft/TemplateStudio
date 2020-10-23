c80766159c7306ac634748714439508dbc93dac0
vmadurga <vmadurga@gmail.com>
2020-10-23 12:01:51 +0200

    Override Expander AutomationControlType

---
+++ code/src/UI/AutomationPeers/ExpanderButtonAutomationPeer.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace Microsoft.Templates.UI.AutomationPeers
+{
+    public class ExpanderButtonAutomationPeer : ExpanderAutomationPeer
+    {
+        public ExpanderButtonAutomationPeer(Expander owner) : base(owner)
+        {
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Button;
+        }
+    }
+}
---
+++ code/src/UI/Controls/ExpanderButtonAutomationPeerControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using Microsoft.Templates.UI.AutomationPeers;
+
+namespace Microsoft.Templates.UI.Controls
+{
+    public class ExpanderButtonAutomationPeerControl : Expander
+    {
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new ExpanderButtonAutomationPeer(this);
+        }
+    }
+}
