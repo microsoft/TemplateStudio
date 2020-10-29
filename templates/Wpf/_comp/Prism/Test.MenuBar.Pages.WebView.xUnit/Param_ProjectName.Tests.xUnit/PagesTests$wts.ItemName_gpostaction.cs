b8289c8a8b61ac602e21c7d50ec99378496dfb92
vmadurga <vmadurga@gmail.com>
2020-10-29 11:38:41 +0100

    Add Prism Menu bar WebView test composition

---
+++ templates/Wpf/_comp/Prism/Test.MenuBar.Pages.WebView.xUnit/Param_ProjectName.Tests.xUnit/PagesTests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,23 @@
+﻿namespace Param_RootNamespace.Tests.xUnit
+{
+    public class PagesTests
+    {
+        public PagesTests()
+        {
+            // App Services
+//{[{
+            _container.RegisterType<IRightPaneService, RightPaneService>();
+//}]}
+        }
+//^^
+//{[{
+        // TODO WTS: Add tests for functionality you add to wts.ItemNameViewModel.
+        [Fact]
+        public void Testwts.ItemNameViewModelCreation()
+        {
+            var vm = _container.Resolve<wts.ItemNameViewModel>();
+            Assert.NotNull(vm);
+        }
+//}]}
+    }
+}
