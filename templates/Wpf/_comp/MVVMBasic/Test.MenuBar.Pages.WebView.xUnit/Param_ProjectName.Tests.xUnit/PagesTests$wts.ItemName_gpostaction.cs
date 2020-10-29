be2d0a597c23cf182a347a1fa728474db6e96d5c
vmadurga <vmadurga@gmail.com>
2020-10-29 09:56:43 +0100

    Add MenuBar MVVMBasic test compositions

---
+++ templates/Wpf/_comp/MVVMBasic/Test.MenuBar.Pages.WebView.xUnit/Param_ProjectName.Tests.xUnit/PagesTests$wts.ItemName_gpostaction.cs
@@ -0,0 +1,41 @@
+﻿namespace Param_RootNamespace.Tests.xUnit
+{
+    public class PagesTests
+    {
+        private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            // Services
+//{[{
+            services.AddSingleton<IRightPaneService, RightPaneService>();
+//}]}
+            // ViewModels
+//{[{
+            services.AddTransient<wts.ItemNameViewModel>();
+//}]}
+        }
+
+//{[{
+        // TODO WTS: Add tests for functionality you add to wts.ItemNameViewModel.
+        [Fact]
+        public void Testwts.ItemNameViewModelCreation()
+        {
+            var vm = _host.Services.GetService(typeof(wts.ItemNameViewModel));
+            Assert.NotNull(vm);
+        }
+
+        [Fact]
+        public void TestGetwts.ItemNamePageType()
+        {
+            if (_host.Services.GetService(typeof(IPageService)) is IPageService pageService)
+            {
+                var pageType = pageService.GetPageType(typeof(wts.ItemNameViewModel).FullName);
+                Assert.Equal(typeof(wts.ItemNamePage), pageType);
+            }
+            else
+            {
+                Assert.True(false, $"Can't resolve {nameof(IPageService)}");
+            }
+        }
+//}]}
+    }
+}
