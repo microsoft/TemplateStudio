57156fe6172e3efc74b60b7aa987feb050b38583
Martin Vega <mvegaca@outlook.com>
2020-12-01 17:16:00 +0100

    Update Fluent Ribbon nuget package

--- templates/Wpf/_comp/_shared/Project.Ribbon.ThemeSelection/Services/ThemeSelectorService_postaction.cs
+++
@@ -1,8 +0,0 @@
-﻿public void SetTheme(AppTheme theme)
-{
-//^^
-//{[{
-    Fluent.ThemeManager.ChangeTheme(Application.Current, $"{theme}.Blue");
-//}]}
-    App.Current.Properties["Theme"] = theme.ToString();
-}
