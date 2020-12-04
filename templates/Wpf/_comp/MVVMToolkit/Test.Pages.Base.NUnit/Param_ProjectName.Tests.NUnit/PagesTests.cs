bd204f75511fdb278debaba7f069d73ee7c55b1c
Martin Vega <mvegaca@outlook.com>
2020-12-04 13:59:11 +0100

    Add Test composition missing templates for WPF MVVM Tookit framework

---
+++ templates/Wpf/_comp/MVVMToolkit/Test.Pages.Base.NUnit/Param_ProjectName.Tests.NUnit/PagesTests.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Reflection;
+using Param_RootNamespace.Contracts.Services;
+using Param_RootNamespace.Models;
+using Param_RootNamespace.Services;
+using Param_RootNamespace.ViewModels;
+using Param_RootNamespace.Views;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+
+namespace Param_RootNamespace.Tests.NUnit
+{
+    public class PagesTests
+    {
+        private IHost _host;
+
+        [SetUp]
+        public void Setup()
+        {
+            var appLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(c => c.SetBasePath(appLocation))
+                .ConfigureServices(ConfigureServices)
+                .Build();
+        }
+
+        private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            // Core Services
+
+            // Services
+            services.AddSingleton<IPageService, PageService>();
+            services.AddSingleton<INavigationService, NavigationService>();
+
+            // ViewModels
+
+            // Configuration
+            services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
+        }
+    }
+}
