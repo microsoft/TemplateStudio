5c52a19aa55cbaab94629a5c6d7d644ee8ddc567
sibille <sibille@users.noreply.github.com>
2020-12-10 17:22:49 +0100

    Exclude testing projects from WPF WACK tests

--- code/test/Templates.Test/WACK/Wpf/WindowsAppCertKitTests.cs
+++ code/test/Templates.Test/WACK/Wpf/WindowsAppCertKitTests.cs
@@ -72,6 +72,8 @@ namespace Microsoft.Templates.Test.Wack.Wpf
                 && (t.GetProjectTypeList().Contains(projectType) || t.GetProjectTypeList().Contains(All))
                 && (t.GetFrontEndFrameworkList().Contains(framework) || t.GetFrontEndFrameworkList().Contains(All))
                 && !t.GroupIdentity.StartsWith("wts.Wpf.Service.Identity")
+                && !t.GroupIdentity.Contains("Test")
+                && !t.GroupIdentity.Contains("WinAppDriver")
                 && t.GetPlatform() == platform
                 && !t.GetIsHidden();
 
