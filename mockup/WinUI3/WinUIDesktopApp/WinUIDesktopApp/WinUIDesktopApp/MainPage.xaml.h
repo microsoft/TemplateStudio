cc4f88353db7514a89432a71b126ad09e85718b7
sibille <sibille@users.noreply.github.com>
2021-01-18 12:23:48 +0100

    Add C++ mockup

---
+++ mockup/WinUI3/WinUIDesktopApp/WinUIDesktopApp/WinUIDesktopApp/MainPage.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::WinUIDesktopApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::WinUIDesktopApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
