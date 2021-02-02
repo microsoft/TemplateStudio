bca658b1200a256edacec987eb820262c0470330
Martin Vega <mvegaca@outlook.com>
2021-02-02 13:01:55 +0100

    Add project template

---
+++ templates/WinUI/Projects/Default.UWP/wts.ProjectName/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Param_RootNamespace
+{
+    public sealed partial class App : Application
+    {
+        public static Window MainWindow => Window.Current;
+
+        public App()
+        {
+            InitializeComponent();
+            UnhandledException += App_UnhandledException;
+        }
+
+        private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            base.OnLaunched(e);
+        }
+
+        protected override async void OnActivated(Windows.ApplicationModel.Activation.IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+        }
+    }
+}
