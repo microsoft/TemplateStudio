9b2e425f78622b8f7fdbba6c784f52c206ed963b
sibille <sibille@users.noreply.github.com>
2021-03-09 18:29:24 +0100

    Run tests using VS 2019

--- code/test/Templates.Test/bat/WinUI/RestoreAndBuild.bat
+++ code/test/Templates.Test/bat/WinUI/RestoreAndBuild.bat
@@ -1,5 +1,14 @@
 REM #Setup MsBuild context by brute force, restore packages and build the solution
 
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community" (
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
+)
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional" (
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsMSBuildCmd.bat"
+)
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" (
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+)
 IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview" (
 	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\Common7\Tools\VsMSBuildCmd.bat"
 )
