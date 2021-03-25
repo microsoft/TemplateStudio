a82839ca1ecccafedd7008fc32861f2f03b0aebb
vmadurga <vmadurga@gmail.com>
2021-03-25 18:27:08 +0100

    Fis usings views uwp

--- templates/WinUI/Projects/Default.UWP/wts.ProjectName/App.xaml.cs
+++ templates/WinUI/Projects/Default.UWP/wts.ProjectName/App.xaml.cs
@@ -14,6 +14,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Param_RootNamespace.Views;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
