40c7a4d8684810bf46fd9fd7954f8d46e2635344
sibille <sibille@users.noreply.github.com>
2021-03-30 12:52:27 +0200

    Update templates for WinUI 3 Desktop (C++)

--- templates/WinUI/Projects/Default.Desktop._Cpp/wts.ProjectName/App.xaml.cpp
+++ templates/WinUI/Projects/Default.Desktop._Cpp/wts.ProjectName/App.xaml.cpp
@@ -21,7 +21,6 @@ using namespace Param_RootNamespace::implementation;
 App::App()
 {
     InitializeComponent();
-    Suspending({ this, &App::OnSuspending });
 
 //-:cnd:noEmit
 #if defined _DEBUG && !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
@@ -46,16 +45,4 @@ void App::OnLaunched(LaunchActivatedEventArgs const&)
 {
     window = make<MainWindow>();
     window.Activate();
-}
-
-/// <summary>
-/// Invoked when application execution is being suspended.  Application state is saved
-/// without knowing whether the application will be terminated or resumed with the contents
-/// of memory still intact.
-/// </summary>
-/// <param name="sender">The source of the suspend request.</param>
-/// <param name="e">Details about the suspend request.</param>
-void App::OnSuspending([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] Windows::ApplicationModel::SuspendingEventArgs const& e)
-{
-    // Save application state and stop any background activity
 }
