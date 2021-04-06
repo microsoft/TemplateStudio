d6d87a9b993133fb7167910971369602e14fd249
vmadurga <vmadurga@gmail.com>
2021-04-06 18:29:48 +0200

    Add ToastNotification feature to MVVMToolkit

---
+++ templates/Wpf/_comp/MVVMToolkit/Feature.ToastNotifications/Services/ApplicationHostService_postaction.cs
@@ -0,0 +1,26 @@
+﻿namespace Param_RootNamespace.Services
+{
+    public class ApplicationHostService : IHostedService
+    {
+        private readonly INavigationService _navigationService;
+//{[{
+        private readonly IToastNotificationsService _toastNotificationsService;
+//}]}
+        public ApplicationHostService(/*{[{*/IToastNotificationsService toastNotificationsService/*}]}*/)
+        {
+//^^
+//{[{
+            _toastNotificationsService = toastNotificationsService;
+//}]}
+        }
+
+        private async Task StartupAsync()
+        {
+//^^
+//{[{
+            _toastNotificationsService.ShowToastNotificationSample();
+//}]}
+            await Task.CompletedTask;
+        }
+    }
+}
