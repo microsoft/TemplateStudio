7df58a22f2d9909f1affab0555ecf92614ab045d
vmadurga <vmadurga@gmail.com>
2021-04-09 13:00:48 +0200

    Fix docs link info

--- templates/Wpf/_comp/MVVMLight/Feature.ToastNotifications/Activation/ToastNotificationActivationHandler.cs
+++ templates/Wpf/_comp/MVVMLight/Feature.ToastNotifications/Activation/ToastNotificationActivationHandler.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Param_RootNamespace.Activation
 {
-    // For more information about send a local toast notification from C# apps, see
+    // For more information about sending a local toast notification from C# apps, see
     // https://docs.microsoft.com/windows/uwp/design/shell/tiles-and-notifications/send-local-toast?tabs=desktop
     // and https://github.com/microsoft/WindowsTemplateStudio/blob/release/docs/WPF/features/toast-notifications.md
 
