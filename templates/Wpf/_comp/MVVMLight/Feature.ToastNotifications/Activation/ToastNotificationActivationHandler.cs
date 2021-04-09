9e26c04c97e899d28797d4ce467c7b57b84ce13d
vmadurga <vmadurga@gmail.com>
2021-04-09 13:42:31 +0200

    Fix StyleCop ToastNotificationActivationHandler

--- templates/Wpf/_comp/MVVMLight/Feature.ToastNotifications/Activation/ToastNotificationActivationHandler.cs
+++ templates/Wpf/_comp/MVVMLight/Feature.ToastNotifications/Activation/ToastNotificationActivationHandler.cs
@@ -12,7 +12,6 @@ namespace Param_RootNamespace.Activation
     // For more information about sending a local toast notification from C# apps, see
     // https://docs.microsoft.com/windows/uwp/design/shell/tiles-and-notifications/send-local-toast?tabs=desktop
     // and https://github.com/microsoft/WindowsTemplateStudio/blob/release/docs/WPF/features/toast-notifications.md
-
     public class ToastNotificationActivationHandler : IActivationHandler
     {
         public const string ActivationArguments = "ToastNotificationActivationArguments";
