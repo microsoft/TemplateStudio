24e574ed54b7e7ff1f2fe65ac0be082b22c193b4
Martin Vega <mvegaca@outlook.com>
2021-05-06 16:37:00 +0200

    Add navigation keyboardAccelerators to WinUI Templates

--- templates/WinUI/_comp/MVVMToolkit/Project.SplitView/Views/ShellPage.xaml.cs
+++ templates/WinUI/_comp/MVVMToolkit/Project.SplitView/Views/ShellPage.xaml.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Param_RootNamespace.Contracts.Services;
 using Param_RootNamespace.ViewModels;
+using Windows.System;
 
 namespace Param_RootNamespace.Views
 {
     // TODO WTS: Change the icons and titles for all NavigationViewItems in ShellPage.xaml.
     public sealed partial class ShellPage : Page
     {
+        private readonly KeyboardAccelerator _altLeftKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu);
+        private readonly KeyboardAccelerator _backKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.GoBack);
+
         public ShellViewModel ViewModel { get; }
 
         public ShellPage(ShellViewModel viewModel)
@@ -14,6 +21,33 @@ namespace Param_RootNamespace.Views
             InitializeComponent();
             ViewModel.NavigationService.Frame = shellFrame;
             ViewModel.NavigationViewService.Initialize(navigationView);
+        }
+
+        private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            // Keyboard accelerators are added here to avoid showing 'Alt + left' tooltip on the page.
+            // More info on tracking issue https://github.com/Microsoft/microsoft-ui-xaml/issues/8
+            KeyboardAccelerators.Add(_altLeftKeyboardAccelerator);
+            KeyboardAccelerators.Add(_backKeyboardAccelerator);
+        }
+
+        private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
+        {
+            var keyboardAccelerator = new KeyboardAccelerator() { Key = key };
+            if (modifiers.HasValue)
+            {
+                keyboardAccelerator.Modifiers = modifiers.Value;
+            }
+
+            keyboardAccelerator.Invoked += OnKeyboardAcceleratorInvoked;
+            return keyboardAccelerator;
+        }
+
+        private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+        {
+            var navigationService = Ioc.Default.GetService<INavigationService>();
+            var result = navigationService.GoBack();
+            args.Handled = result;
         }
     }
 }
