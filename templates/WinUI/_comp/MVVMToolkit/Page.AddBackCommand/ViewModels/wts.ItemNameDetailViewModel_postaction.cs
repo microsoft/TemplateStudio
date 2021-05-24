9cc62dae48164a0d75fcabb20dbf376528c50916
Martin Vega <mvegaca@outlook.com>
2021-05-24 18:32:26 +0200

    Add back button in content grid page for blank advanced projects

---
+++ templates/WinUI/_comp/MVVMToolkit/Page.AddBackCommand/ViewModels/wts.ItemNameDetailViewModel_postaction.cs
@@ -0,0 +1,40 @@
+﻿//{[{
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Param_RootNamespace.Contracts.Services;
+//}]}
+
+namespace Param_RootNamespace.ViewModels
+{
+    public class wts.ItemNameDetailViewModel : ObservableRecipient, INavigationAware
+    {
+//{[{
+        private readonly INavigationService _navigationService;
+//}]}
+//^^
+//{[{
+        private ICommand _goBackCommand;
+
+        public ICommand GoBackCommand => _goBackCommand ?? (_goBackCommand = new RelayCommand(OnGoBack));
+//}]}
+
+        public wts.ItemNameDetailViewModel(/*{[{*/INavigationService navigationService/*}]}*/)
+        {
+//{[{
+            _navigationService = navigationService;
+//}]}
+        }
+
+//^^
+//{[{
+        private void OnGoBack()
+        {
+            if (_navigationService.CanGoBack)
+            {
+                _navigationService.GoBack();
+            }
+        }
+//}]}
+    }
+}
