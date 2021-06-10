78ca6cc9be093c66796f431eb982b9fcfc41309b
vmadurga <vmadurga@gmail.com>
2021-06-10 18:13:48 +0200

    Add base project templates

---
+++ templates/WinUI/_comp/MVVMToolkit/Project.MenuBar/Services/RightPaneService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using MahApps.Metro.Controls;
+using Param_RootNamespace.Contracts.Services;
+using Param_RootNamespace.Contracts.ViewModels;
+
+namespace Param_RootNamespace.Services
+{
+    public class RightPaneService : IRightPaneService
+    {
+        private readonly IPageService _pageService;
+
+        private SplitView _splitView;
+        private Frame _frame;
+        private object _lastParamUsed;
+
+        public RightPaneService(IPageService pageService)
+        {
+            _pageService = pageService;
+        }
+
+        public void Initialize(Frame rightPaneFrame, SplitView splitView)
+        {
+            _splitView = splitView;
+            _frame = rightPaneFrame;
+            _frame.Navigated += OnNavigated;
+        }
+
+        public void OpenInRightPane(string pageKey, object parameter = null)
+        {
+            // Don't open the same page multiple times
+            if (_frame.GetPageViewModel() == null || _frame.GetPageViewModel().GetType().FullName != pageKey || (parameter != null && !parameter.Equals(_lastParamUsed)))
+            {
+                var pageType = _pageService.GetPageType(pageKey);
+                var vmBeforeNavigation = _frame.GetPageViewModel();
+                var navigationResult = _frame.Navigate(pageType, parameter);
+                if (navigationResult)
+                {
+                    _lastParamUsed = parameter;
+                    if (vmBeforeNavigation is INavigationAware navigationAware)
+                    {
+                        navigationAware.OnNavigatedFrom();
+                    }
+                }
+            }
+
+            _splitView.IsPaneOpen = true;
+        }
+
+        public void CleanUp()
+        {
+            _frame.Navigated -= OnNavigated;
+        }
+
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            if (sender is Frame frame)
+            {
+                frame.BackStack.Clear();
+                if (frame.GetPageViewModel() is INavigationAware navigationAware)
+                {
+                    navigationAware.OnNavigatedTo(e.Parameter);
+                }
+            }
+        }
+    }
+}
---
+++ templates/WinUI/_comp/MVVMToolkit/Project.MenuBar/ViewModels/ShellViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Param_RootNamespace.Contracts.Services;
+using Param_RootNamespace.Properties;
+
+namespace Param_RootNamespace.ViewModels
+{
+    // You can show pages in different ways (update main view, navigate, right pane, new windows or dialog)
+    // using the NavigationService, RightPaneService and WindowManagerService.
+    // Read more about MenuBar project type here:
+    // https://github.com/Microsoft/WindowsTemplateStudio/blob/release/docs/WinUI/projectTypes/menubar.md
+    public class ShellViewModel : ObservableRecipient
+    {
+        private bool _isBackEnabled;
+        private object _selected;
+        private ICommand _menuFileExitCommand;
+
+        public ICommand MenuFileExitCommand => _menuFileExitCommand ?? (_menuFileExitCommand = new RelayCommand(OnMenuFileExit));
+
+        public INavigationService NavigationService { get; }
+
+        public IRightPaneService RightPaneService { get; }
+
+        public bool IsBackEnabled
+        {
+            get { return _isBackEnabled; }
+            set { SetProperty(ref _isBackEnabled, value); }
+        }
+
+        public object Selected
+        {
+            get { return _selected; }
+            set { SetProperty(ref _selected, value); }
+        }
+
+        public ShellViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+            NavigationService.Navigated += OnNavigated;
+        }
+
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            IsBackEnabled = NavigationService.CanGoBack;
+        }
+
+        private void OnMenuViewsMain() => NavigationService.NavigateTo(typeof(MainViewModel).FullName, null, true);
+
+        private void OnMenuFileExit() => Application.Current.Exit();
+    }
+}
