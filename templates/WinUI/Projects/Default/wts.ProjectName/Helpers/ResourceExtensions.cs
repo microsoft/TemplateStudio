7ad386a3f722cbaef2d9ec81cc63faf7073ed531
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-02-02 19:36:59 -0800

    Additional cleanup.

--- templates/WinUI/Projects/Default/wts.ProjectName/Helpers/ResourceExtensions.cs
+++ templates/WinUI/Projects/Default/wts.ProjectName/Helpers/ResourceExtensions.cs
@@ -4,11 +4,11 @@ namespace Param_RootNamespace.Helpers
 {
     internal static class ResourceExtensions
     {
-        private static ResourceLoader _resLoader = new ResourceLoader();
+        private static ResourceLoader _resourceLoader = new ResourceLoader();
 
         public static string GetLocalized(this string resourceKey)
         {
-            return _resLoader.GetString(resourceKey);
+            return _resourceLoader.GetString(resourceKey);
         }
     }
 }
