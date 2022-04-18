c80871bde22936ea6328fb3e9e35dacac1a51658
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-04-18 01:47:19 -0700

    Updated failing tests and removed additional BlankAdvanced references.

--- code/test/SharedFunctionality.Tests/TemplateValidator/TemplateJsonVerifier.cs
+++ code/test/SharedFunctionality.Tests/TemplateValidator/TemplateJsonVerifier.cs
@@ -517,7 +517,7 @@ namespace TemplateValidator
             // This tag may contain a single value or multiple ones separated by the pipe character
             foreach (var projectType in tag.Value.Split('|'))
             {
-                if (!new[] { "Blank", "BlankAdvanced", "SplitView", "TabbedNav", "MenuBar", "all" }.Contains(projectType))
+                if (!new[] { "Blank", "SplitView", "TabbedNav", "MenuBar", "all" }.Contains(projectType))
                 {
                     results.Add($"Invalid value '{tag.Value}' specified in the ts.projecttype tag.");
                 }
