37bde9e93316ada2e7f3dad528f6d4e914d974bf
Sophia Morin <t-smorin@microsoft.com>
2022-05-16 17:11:49 -0700

    shell page postactions

---
+++ code/TemplateStudioForWinUICs/Templates/_comp/MT/Project.MenuBar.AddNavViewItem.Settings/Views/ShellPage_postaction.xaml.cs
@@ -0,0 +1,17 @@
+﻿                </MenuBar>
+<!--{[{-->
+                <AppBarButton Grid.Column="1" x:Name="SettingsButton" AnimatedIcon.State="Normal"
+                                                  PointerEntered="SettingsButton_PointerEntered"
+                                                  PointerExited="SettingsButton_PointerExited"
+                                                  Command="{x:Bind ViewModel.MenuSettingsCommand}">
+                    <muxc:AnimatedIcon x:Name='AnimatedIcon'>
+                        <muxc:AnimatedIcon.Source>
+                            <animatedvisuals:AnimatedSettingsVisualSource/>
+                        </muxc:AnimatedIcon.Source>
+                        <muxc:AnimatedIcon.FallbackIconSource>
+            <muxc:FontIconSource FontFamily="{StaticResource SymbolThemeFontFamily}" Glyph="&#xE713;"/>
+                        </muxc:AnimatedIcon.FallbackIconSource>
+                    </muxc:AnimatedIcon>
+                </AppBarButton>
+<!--}]}-->
+
