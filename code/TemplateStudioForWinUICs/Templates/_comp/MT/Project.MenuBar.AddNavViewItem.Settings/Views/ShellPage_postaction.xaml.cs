bce7d07d15a1b6f640e3d819beccc4915c3828ce
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-05-19 14:27:27 -0700

    Fixed accessibility issue with SettingsButton.

--- code/TemplateStudioForWinUICs/Templates/_comp/MT/Project.MenuBar.AddNavViewItem.Settings/Views/ShellPage_postaction.xaml.cs
+++ code/TemplateStudioForWinUICs/Templates/_comp/MT/Project.MenuBar.AddNavViewItem.Settings/Views/ShellPage_postaction.xaml.cs
@@ -3,8 +3,8 @@
 //^^
 //{[{
 
-            SettingsButton.AddHandler(UIElement.PointerPressedEvent, new PointerEventHandler(SettingsButton_PointerPressed), true);
-            SettingsButton.AddHandler(UIElement.PointerReleasedEvent, new PointerEventHandler(SettingsButton_PointerReleased), true);
+            ShellMenuBarSettingsButton.AddHandler(UIElement.PointerPressedEvent, new PointerEventHandler(ShellMenuBarSettingsButton_PointerPressed), true);
+            ShellMenuBarSettingsButton.AddHandler(UIElement.PointerReleasedEvent, new PointerEventHandler(ShellMenuBarSettingsButton_PointerReleased), true);
 //}]}
         }
 
@@ -13,29 +13,29 @@
 //^^
 //{[{
 
-            SettingsButton.RemoveHandler(UIElement.PointerPressedEvent, (PointerEventHandler)SettingsButton_PointerPressed);
-            SettingsButton.RemoveHandler(UIElement.PointerReleasedEvent, (PointerEventHandler)SettingsButton_PointerReleased);
+            ShellMenuBarSettingsButton.RemoveHandler(UIElement.PointerPressedEvent, (PointerEventHandler)ShellMenuBarSettingsButton_PointerPressed);
+            ShellMenuBarSettingsButton.RemoveHandler(UIElement.PointerReleasedEvent, (PointerEventHandler)ShellMenuBarSettingsButton_PointerReleased);
 //}]}
         }
 
 //^^
 //{[{
-        private void SettingsButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+        private void ShellMenuBarSettingsButton_PointerEntered(object sender, PointerRoutedEventArgs e)
         {
             AnimatedIcon.SetState((UIElement)sender, "PointerOver");
         }
 
-        private void SettingsButton_PointerPressed(object sender, PointerRoutedEventArgs e)
+        private void ShellMenuBarSettingsButton_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             AnimatedIcon.SetState((UIElement)sender, "Pressed");
         }
 
-        private void SettingsButton_PointerReleased(object sender, PointerRoutedEventArgs e)
+        private void ShellMenuBarSettingsButton_PointerReleased(object sender, PointerRoutedEventArgs e)
         {
             AnimatedIcon.SetState((UIElement)sender, "Normal");
         }
 
-        private void SettingsButton_PointerExited(object sender, PointerRoutedEventArgs e)
+        private void ShellMenuBarSettingsButton_PointerExited(object sender, PointerRoutedEventArgs e)
         {
             AnimatedIcon.SetState((UIElement)sender, "Normal");
         }
