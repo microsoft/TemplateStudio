80bf2087cec777ca542b613780e4ae64a3ca7885
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-05-23 18:46:08 -0700

    Removed stale comment.

--- code/TemplateStudioForWinUICs/Templates/_comp/MT/Project.MenuBar/ViewModels/ShellViewModel.cs
+++ code/TemplateStudioForWinUICs/Templates/_comp/MT/Project.MenuBar/ViewModels/ShellViewModel.cs
@@ -8,10 +8,6 @@ using Param_RootNamespace.Contracts.Services;
 
 namespace Param_RootNamespace.ViewModels
 {
-    // You can show pages in different ways (update main view, navigate or right pane)
-    // using the NavigationService and RightPaneService.
-    // Read more about MenuBar project type here:
-    // https://github.com/microsoft/TemplateStudio/blob/main/docs/WinUI/projectTypes/menubar.md
     public class ShellViewModel : ObservableRecipient
     {
         private bool _isBackEnabled;
