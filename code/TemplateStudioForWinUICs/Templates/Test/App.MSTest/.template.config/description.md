b506fe85fa8ae0a8c9169fb629a6e38deeb5c4b9
Sophia Morin <t-smorin@microsoft.com>
2022-06-02 10:04:56 -0700

    new test branch

---
+++ code/TemplateStudioForWinUICs/Templates/Test/App.MSTest/.template.config/description.md
@@ -0,0 +1,1 @@
+﻿Including this feature adds a new project to the solution that is set up for hosting Unit tests of the code in the main app.
---
+++ code/TemplateStudioForWinUICs/Templates/Test/App.MSTest/Param_ProjectName.Tests.MSTest/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Param_RootNamespace.Tests.MSTest
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
