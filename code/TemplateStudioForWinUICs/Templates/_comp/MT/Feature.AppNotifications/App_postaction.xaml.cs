3802fa498d51fa0fb7d61b19190bf25bf112bfc8
Sophia Morin <t-smorin@microsoft.com>
2022-06-17 11:37:41 -0700

    post action (not working)

---
+++ code/TemplateStudioForWinUICs/Templates/_comp/MT/Feature.AppNotifications/App_postaction.xaml.cs
@@ -0,0 +1,7 @@
+﻿        .ConfigureServices((context, services) =>
+        {
+            // Services
+//{[{
+            services.AddSingleton<INotificationService, NotificationService>();
+        }
+//}]}
