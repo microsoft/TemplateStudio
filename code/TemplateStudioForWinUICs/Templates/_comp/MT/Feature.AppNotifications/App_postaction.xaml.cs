0c6a83e67af4be35e89891d6135a9c2a8f703032
Sophia Morin <t-smorin@microsoft.com>
2022-06-20 09:11:45 -0700

    post action working, but "interface not registered"

--- code/TemplateStudioForWinUICs/Templates/_comp/MT/Feature.AppNotifications/App_postaction.xaml.cs
+++ code/TemplateStudioForWinUICs/Templates/_comp/MT/Feature.AppNotifications/App_postaction.xaml.cs
@@ -1,7 +1,17 @@
-﻿        .ConfigureServices((context, services) =>
+﻿//{[{
+using Param_RootNamespace.Notifications;
+//}]}
+        .ConfigureServices((context, services) =>
         {
+            // Other Activation Handlers
+//{[{
+            services.AddTransient<IActivationHandler, NotificationActivationHandler>();
+//}]}
             // Services
 //{[{
             services.AddSingleton<INotificationService, NotificationService>();
-        }
+//}]}
+        base.OnLaunched(args);
+//{[{
+        App.GetService<INotificationService>().Initialize();
 //}]}
