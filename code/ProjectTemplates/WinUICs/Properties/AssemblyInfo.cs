728cc66c92aabfba56efbe3bc0bfdbe90613f480
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-06-29 17:57:32 -0700

    Updated project metadata.

--- code/ProjectTemplates/WinUICs/Properties/AssemblyInfo.cs
+++ code/ProjectTemplates/WinUICs/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinUICs")]
+[assembly: AssemblyTitle("TemplateStudioForWinUICs.ProjectTemplates")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WinUICs")]
+[assembly: AssemblyProduct("TemplateStudioForWinUICs.ProjectTemplates")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
