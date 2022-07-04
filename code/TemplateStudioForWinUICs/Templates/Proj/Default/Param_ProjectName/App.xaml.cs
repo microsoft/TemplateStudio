620a1f38188992b96e79c26a42ed5baca1d1bbc0
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-07-04 12:50:37 -0700

    Use WindowEx for App.MainWindow.

--- code/TemplateStudioForWinUICs/Templates/Proj/Default/Param_ProjectName/App.xaml.cs
+++ code/TemplateStudioForWinUICs/Templates/Proj/Default/Param_ProjectName/App.xaml.cs
@@ -1,23 +1,25 @@
 ﻿using Microsoft.UI.Xaml;
 using Param_RootNamespace.Helpers;
+using WinUIEx;
 
 // To learn more about WinUI3, see: https://docs.microsoft.com/windows/apps/winui/winui3/.
 namespace Param_RootNamespace;
 
 public partial class App : Application
 {
-    public static Window MainWindow { get; set; } = new Window() { Title = "AppDisplayName".GetLocalized() };
+    public static WindowEx MainWindow { get; } = new() { Backdrop = new MicaSystemBackdrop(), Content = null, PersistenceId = "DA786591-C56F-426F-8ECA-CA016C49F8A2", Title = "AppDisplayName".GetLocalized() };
 
     public App()
     {
         InitializeComponent();
+
         UnhandledException += App_UnhandledException;
     }
 
     private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
     {
         // TODO: Log and handle exceptions as appropriate.
-        // For more details, see https://docs.microsoft.com/windows/winui/api/microsoft.ui.xaml.unhandledexceptioneventargs.
+        // https://docs.microsoft.com/windows/windows-app-sdk/api/winrt/microsoft.ui.xaml.application.unhandledexception.
     }
 
     protected async override void OnLaunched(LaunchActivatedEventArgs args)
