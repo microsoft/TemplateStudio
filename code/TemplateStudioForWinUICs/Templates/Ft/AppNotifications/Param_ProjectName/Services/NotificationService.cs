62a6fb2b9d814c71e4219f00abe2ab8248ef30be
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-07-05 22:35:23 -0700

    Code cleanup and use Settings page for example rather than ContentGrid.

--- code/TemplateStudioForWinUICs/Templates/Ft/AppNotifications/Param_ProjectName/Services/NotificationService.cs
+++ code/TemplateStudioForWinUICs/Templates/Ft/AppNotifications/Param_ProjectName/Services/NotificationService.cs
@@ -1,12 +1,10 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using Param_ProjectName.Contracts.Services;
+﻿using Param_ProjectName.Contracts.Services;
 using Param_ProjectName.ViewModels;
 using Microsoft.Windows.AppNotifications;
 using System.Web;
 
 namespace Param_RootNamespace.Notifications;
+
 public class NotificationService : INotificationService
 {
     private readonly INavigationService _navigationService;
@@ -24,52 +22,41 @@ public class NotificationService : INotificationService
     public void Initialize()
     {
         AppNotificationManager.Default.NotificationInvoked += OnNotificationInvoked;
+
         AppNotificationManager.Default.Register();
     }
 
     public void OnNotificationInvoked(AppNotificationManager sender, AppNotificationActivatedEventArgs args)
     {
-        // TODO: Handle notification invocations when your app is in the foreground.
+        // TODO: Handle notification invocations when your app is already running.
 
-        //// Extract arguments from the notification payload.
-        //var action = ParseArguments(args.Argument, "action");
-
-        //// Take action based on the arguments. Navigating to a specific page is a common scenario.
-        //if (action == "ContentGridPage")
+        //// Navigate to a specific page based on the notification arguments.
+        //if (ParseArguments(args.Argument, "action") == "Settings")
         //{
-        //    // Make sure you're on the UI thread before navigating.
-        //    if (App.MainWindow.DispatcherQueue.HasThreadAccess)
+        //    App.MainWindow.DispatcherQueue.TryEnqueue(() =>
         //    {
-        //        // Navigate to a specific page based on the payload in the notification.
-        //        _navigationService.NavigateTo(typeof(ContentGridViewModel).FullName!);
-        //    }
-        //    else
-        //    {
-        //        App.MainWindow.DispatcherQueue.TryEnqueue(() =>
-        //        {
-        //             // Navigate to a specific page based on the payload in the notification.
-        //            _navigationService.NavigateTo(typeof(ContentGridViewModel).FullName!);
-        //        });
-        //    }
+        //        _navigationService.NavigateTo(typeof(SettingsViewModel).FullName);
+        //    });
         //}
     }
 
     public bool Show(string payload)
     {
         var appNotification = new AppNotification(payload);
+
         AppNotificationManager.Default.Show(appNotification);
+
         return appNotification.Id != 0;
     }
 
-    public string? ParseArguments(string args, string parameter)
+    public string? ParseArguments(string arguments, string parameter)
     {
-        return HttpUtility.ParseQueryString(args)[parameter];
+        return HttpUtility.ParseQueryString(arguments)[parameter];
     }
 
     public void Unregister()
     {
         AppNotificationManager.Default.Unregister();
     }
-
 }
 
