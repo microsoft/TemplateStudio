1f3d2fee2c4ddd5f767c08da1b54c78ce02e298d
Sophia Morin <t-smorin@microsoft.com>
2022-07-18 10:37:01 -0700

    template fix

---
+++ code/TemplateStudioForWinUICs/Templates/_comp/MT/Page.Settings.GetVersionDescription/ViewModels/Param_ItemNameViewModel_postaction.cs
@@ -0,0 +1,33 @@
+﻿//{[{
+using System.Reflection;
+using Windows.ApplicationModel;
+using Windows.Storage;
+//}]}
+
+private static string GetVersionDescription()
+    {
+        //{[{
+        private readonly bool _isPackaged;
+        var appName = "AppDisplayName".GetLocalized();
+        try
+        {
+            var package = Package.Current;
+            _isPackaged = true;
+        }
+        catch (InvalidOperationException)
+        {
+            _isPackaged = false;
+        }
+
+        if (_isPackaged)
+        {
+            var version = Package.Current.Id.Version;
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+        else
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            return $"{appName} - {version?.Major}.{version?.Minor}.{version?.Build}.{version?.Revision}";
+        }
+        //}]}
+    }
