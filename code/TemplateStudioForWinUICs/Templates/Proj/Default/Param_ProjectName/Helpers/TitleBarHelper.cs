1478988b5b34dc5c89fd9caacf89a330110cb79b
Humberto Molina <humberto_molinalopez@hotmail.com>
2023-03-06 13:07:17 -0600

    Update TitleBarHelper.cs

--- code/TemplateStudioForWinUICs/Templates/Proj/Default/Param_ProjectName/Helpers/TitleBarHelper.cs
+++ code/TemplateStudioForWinUICs/Templates/Proj/Default/Param_ProjectName/Helpers/TitleBarHelper.cs
@@ -29,17 +29,9 @@ internal class TitleBarHelper
             if (theme == ElementTheme.Default)
             {
                 var uiSettings = new UISettings();
-                var color = uiSettings.GetColorValue(UIColorType.Background);
+                var background = uiSettings.GetColorValue(UIColorType.Background);
 
-                if (color.ToString() == "#FFFFFFFF")
-                {
-                    theme = ElementTheme.Light;
-                }
-                else
-                {
-                    theme = ElementTheme.Dark;
-                }
-
+                theme = background == Colors.White ? ElementTheme.Light : ElementTheme.Dark;
             }
             
             if (theme == ElementTheme.Default)
