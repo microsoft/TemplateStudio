221e75dd7f95b7b9f7c8f15ab5667d68dd4c543b
Mysterious_Dev <mysteriousdevs@gmail.com>
2023-09-23 16:39:00 +0200

    Reordering

--- code/TemplateStudioForWinUICs/Templates/Proj/Default/Param_ProjectName/Helpers/TitleBarHelper.cs
+++ code/TemplateStudioForWinUICs/Templates/Proj/Default/Param_ProjectName/Helpers/TitleBarHelper.cs
@@ -39,13 +39,6 @@ internal class TitleBarHelper
                 theme = Application.Current.RequestedTheme == ApplicationTheme.Light ? ElementTheme.Light : ElementTheme.Dark;
             }
 
-            App.MainWindow.AppWindow.TitleBar.ButtonHoverBackgroundColor = theme switch
-            {
-                ElementTheme.Dark => Color.FromArgb(0x33, 0xFF, 0xFF, 0xFF),
-                ElementTheme.Light => Color.FromArgb(0x33, 0x00, 0x00, 0x00),
-                _ => Colors.Transparent
-            };
-
             App.MainWindow.AppWindow.TitleBar.ButtonForegroundColor = theme switch
             {
                 ElementTheme.Dark => Colors.White,
@@ -57,6 +50,13 @@ internal class TitleBarHelper
             {
                 ElementTheme.Dark => Colors.White,
                 ElementTheme.Light => Colors.Black,
+                _ => Colors.Transparent
+            };
+
+            App.MainWindow.AppWindow.TitleBar.ButtonHoverBackgroundColor = theme switch
+            {
+                ElementTheme.Dark => Color.FromArgb(0x33, 0xFF, 0xFF, 0xFF),
+                ElementTheme.Light => Color.FromArgb(0x33, 0x00, 0x00, 0x00),
                 _ => Colors.Transparent
             };
 
